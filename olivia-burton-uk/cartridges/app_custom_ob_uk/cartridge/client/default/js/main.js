1545ac7bc58d3f7e60e56796a63014bc18de63a1
Asim shahzad <asim.shahzad@innovadeltech.com>
2019-06-27 17:02:46 +0000

    Merged in feature/MSS-9-cookies-policy-bottom-popup (pull request #6)
    [MSS-9] Display Cookie Policy At Bottom Of The Page On Each New Session.
    
    Approved-by: Muhammad Haroon Rasheed <haroon.rasheed@innovadeltech.com>

---
+++ olivia-burton-uk/cartridges/app_custom_ob_uk/cartridge/client/default/js/main.js
@@ -0,0 +1,26 @@
+window.jQuery = window.$ = require('jquery');
+
+var processInclude = require('base/util');
+
+$(document).ready(function () {
+    processInclude(require('movado/components/slickCarousel'));
+    processInclude(require('movado/utilities/sticky-header'));
+    processInclude(require('movado/utilities/modal'));
+    processInclude(require('movado/components/menu'));
+    processInclude(require('./components/cookie'));
+    processInclude(require('base/components/footer'));
+    processInclude(require('movado/components/miniCart'));
+    processInclude(require('movado/components/newsLetterSubscription'));
+    processInclude(require('movado/components/collapsibleItem'));
+    processInclude(require('movado/components/search'));
+    processInclude(require('movado/productTile'));
+    processInclude(require('base/components/clientSideValidation'));
+    processInclude(require('base/components/countrySelector'));
+    processInclude(require('movado/welcomeMat/welcomeMatModal'));
+    processInclude(require('movado/components/mod_video'));
+    processInclude(require('movado/product/compare'));
+    processInclude(require('movado/utilities/gtm-events'));
+    processInclude(require('movado/product/wishlistHeart'));
+});
+
+require('base/components/spinner');
