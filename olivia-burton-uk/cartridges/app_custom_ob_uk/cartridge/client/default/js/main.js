8cb947f675635e74f98611efc43c0edbe409d35c
Anis Ur Rehman <anis.rehman@innovadeltech.com>
2019-07-17 13:51:04 +0000

    Merged in feature/MSS-88-ob-embossing-engraving (pull request #27)
    [MSS-88] OB Embossing/Engraving.
    
    * [MSS-88] added logic to set custom attributes.
    
    * [MSS-88] Implement fonts and orientation for embossing and engraving
    
    * [MSS-88] linting.
    
    * [MSS-88] styling.
    
    Approved-by: Haroon Chaudhry <haroon.chaudhry@innovadeltech.com>

--- olivia-burton-uk/cartridges/app_custom_ob_uk/cartridge/client/default/js/main.js
+++ olivia-burton-uk/cartridges/app_custom_ob_uk/cartridge/client/default/js/main.js
@@ -9,11 +9,10 @@ $(document).ready(function () {
     processInclude(require('movado/components/menu'));
     processInclude(require('./components/cookie'));
     processInclude(require('base/components/footer'));
-    processInclude(require('movado/components/miniCart'));
     processInclude(require('movado/components/newsLetterSubscription'));
     processInclude(require('movado/components/collapsibleItem'));
     processInclude(require('movado/components/search'));
-    processInclude(require('movado/productTile'));
+    processInclude(require('./productTile'));
     processInclude(require('base/components/clientSideValidation'));
     processInclude(require('base/components/countrySelector'));
     processInclude(require('movado/welcomeMat/welcomeMatModal'));
@@ -21,6 +20,8 @@ $(document).ready(function () {
     processInclude(require('movado/product/compare'));
     processInclude(require('movado/utilities/gtm-events'));
     processInclude(require('movado/product/wishlistHeart'));
+    processInclude(require('./product/base'));
+    processInclude(require('./components/miniCart'));
 });
 
 require('base/components/spinner');
---
+++ olivia-burton-uk/cartridges/app_custom_ob_uk/cartridge/controllers/Cart.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var server = require('server');
+
+var customProductOptionsHelper = require('*/cartridge/scripts/helpers/customProductOptionsHelper');
+
+server.extend(module.superModule);
+
+// Added custom code for personalization text for Engraving and Embossing
+//Added custom code for personalization text for Engraving and Embossing
+server.append('AddProduct', function (req, res, next) {
+    var BasketMgr = require('dw/order/BasketMgr');
+    var viewData = res.getViewData();
+    var addCartGtmArray;
+    if (!viewData.error) {
+        // variables for personalization message
+        var embossedMessage = req.form.EmbossedMessage; // message to be Embossed Or Engraved  //'EM\nEngraveMessage';
+        var engravedMessage = req.form.EngravedMessage;
+        var orientation = req.form.orientation;
+        var font = req.form.font;
+        var currentBasket = BasketMgr.getCurrentOrNewBasket();
+        if (embossedMessage || engravedMessage) {
+            customProductOptionsHelper.updateOptionLineItem(currentBasket, viewData.pliUUID, embossedMessage, engravedMessage, orientation, font);
+        }
+
+    }
+    next();
+});
+
+module.exports = server.exports();
