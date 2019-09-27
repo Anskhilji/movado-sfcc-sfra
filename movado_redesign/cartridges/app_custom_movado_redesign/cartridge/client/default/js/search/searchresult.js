f9b3bb547c7d09902adddfddd25177edf9754ace
Muhammad Abrar <muhammad.abrar@innovadeltech.com>
2019-09-27 14:29:50 +0000

    Merged in bugfix/MSS-254-some-initial-plp-fixes---movado- (pull request #169)
    [MSS-254][MSS-267][MSS-268] Fixed All PLP Redesign bugs reported in different tickets
    
    Approved-by: Haroon Chaudhry <haroon.chaudhry@innovadeltech.com>

--- movado_redesign/cartridges/app_custom_movado_redesign/cartridge/client/default/js/search/searchresult.js
+++ movado_redesign/cartridges/app_custom_movado_redesign/cartridge/client/default/js/search/searchresult.js
@@ -1,25 +1,43 @@
 'use strict';
+
 $(document).ready(function () {
-    $(".filter-btn").click(function(){
+    $(".search-results.plp-new-design .filter-btn").click(function(){
         $(".modal-background").addClass("d-block").show();
         $("body").addClass("no-overflow");
-        $(".refinement-bar").addClass("faster fadeInRight animated").addClass("d-block");
-        $('.selected-value').prepend("<span>Sort By</span> ");
+        $(".search-results.plp-new-design .refinement-bar").removeClass("fast fadeOutRight animated").addClass("faster fadeInRight animated").addClass("d-block");
     });
-
-    $(".custom-select__dropdown .custom-select__option").click(function(){
+    $('.search-results.plp-new-design .refinement-bar .selected-value').prepend("<span>Sort By</span> ");
+    $(".search-results.plp-new-design .custom-select__dropdown .custom-select__option").click(function(){
         $('.selected-value').prepend("<span>Sort By</span> ");
     });
 });
 
-$(document).on("click",".close-refinebar", function (e) {
+$(document).on("click",".search-results.plp-new-design  .close-refinebar", function (e) {
     e.preventDefault();
     $(".modal-background").removeClass("d-block").hide();
     $("body").removeClass("no-overflow");
-    $(".refinement-bar").removeClass("faster fadeInRight animated").removeClass("d-block");
+    $(".search-results.plp-new-design .refinement-bar").removeClass("faster fadeInRight animated").addClass("fast fadeOutRight animated");
+    setTimeout(function(){
+        $(".search-results.plp-new-design .refinement-bar").removeClass("d-block");
+    },300);
 });
+const $menu = $('.search-results.plp-new-design  .refinement-bar');
+$(document).mouseup(e => {
+    if (!$menu.is(e.target) // if the target of the click isn't the container...
+    && $menu.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        $(".modal-background").removeClass("d-block").hide();
+        $("body").removeClass("no-overflow");
+        $(".search-results.plp-new-design  .refinement-bar").removeClass("faster fadeInRight animated").removeClass("d-block").addClass("fast fadeOutRight animated");
+        setTimeout(function(){
+            $(".search-results.plp-new-design  .refinement-bar").removeClass("d-block");
+        },300);
+   
+    }
+  });
 
-$(".mobile-menu .close-button").click(function(){
+
+$(".search-results.plp-new-design  .mobile-menu .close-button").click(function(){
     $(".mobile-menu").addClass("animated fadeOut delay-point-three");
     $(".multilevel-dropdown .nav-item > a").removeClass("fadeInLeft fast animated").addClass("fadeOutLeft fast animated");
     $(".country-selector-mobile .html-slot-container, .mobile-login .nav-link").removeClass("fadeInLeft fast animated").addClass("fadeOutLeft fast animated");
@@ -28,32 +46,53 @@ $(".mobile-menu .close-button").click(function(){
     },1000);
 });
 
+
+
+$('.search-results.plp-new-design #sort-order').customSelect();
 $(window).on("load resize scroll", function(e) {
     var width= $(window).width();
+    var tileHeight=$('.search-results.plp-new-design .product-tile').height();
 
     if ($(window).width() > 768) {
-        $(document).on("mouseenter", ".search-results.plp-new-design .product-grid .product-tile", function() {
-            $(this).addClass("hovered-tile");
-            $(".modal-background").addClass("d-block fadeIn faster animated").show();
-        });
 
-        $(document).on("mouseleave", ".search-results.plp-new-design .product-grid .product-tile", function() {
-            $(this).removeClass("hovered-tile");
-            $(".modal-background").removeClass("d-block fadeIn faster animated").hide();
-        });
-    }
+        var myTimeout; 
+        $('.search-results.plp-new-design .product .product-tile').mouseenter(function () {
+            var that=this
+            var thatHeight=$(this).height();
 
-    else {
-        $(document).on("mouseenter", ".search-results.plp-new-design .product-grid .product-tile", function() {
-            $(this).removeClass("hovered-tile");
-            $(".modal-background").removeClass("d-block fadeIn faster animated").hide();
-        });
-    
-        $(document).on("mouseleave", ".search-results.plp-new-design .product-grid .product-tile", function() {
-            $(this).removeClass("hovered-tile");
-            $(".modal-background").removeClass("d-block fadeIn faster animated").hide();
-        });
+            myTimeout = setTimeout(function () {
+            $(that).addClass('hovered-tile');
+            $(".hovered-tile").css("min-height", thatHeight);
+            $(that).find(".tile-btns").addClass("delay-point-five fadeIn fast animated");
+            $(that).find(".tile-discription").addClass(" delay-point-five fadeIn fast animated");
+                }, 500);
+            }).mouseleave(function () {
+                clearTimeout(myTimeout);
+                $('.search-results.plp-new-design .product-tile').removeClass('hovered-tile');
+                $(".tile-btns").removeClass("fadeIn delay-point-five fast animated");
+                $(".tile-discription").removeClass("delay-point-five fast animated");
+                $(".hovered-tile").css("min-height", "unset");
+            });
+    }else {
+        $('.search-results.plp-new-design .product .product-tile').mouseenter(function () {
+            var that=this
+            var thatHeight=$(this).height();
+            console.log(thatHeight);
+
+            myTimeout = setTimeout(function () {
+                $('.search-results.plp-new-design .product-tile').removeClass('hovered-tile');
+                $(".tile-btns").removeClass("fadeIn delay-point-five fast animated");
+                $(".tile-discription").removeClass("delay-point-five fast animated");
+                $(".hovered-tile").css("min-height", "unset");
+                }, 500);
+            }).mouseleave(function () {
+                clearTimeout(myTimeout);
+                $('.search-results.plp-new-design .product-tile').removeClass('hovered-tile');
+                $(".tile-btns").removeClass("fadeIn delay-point-five fast animated");
+                $(".tile-discription").removeClass("delay-point-five fast animated");
+                $(".hovered-tile").css("min-height", "unset");
+            });
+
     }
 });
 
-$('.search-results.plp-new-design #sort-order').customSelect();
