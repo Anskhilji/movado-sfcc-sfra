4b9da1ea135242d01a6974e15740410c617b73d8
Asim shahzad <asim.shahzad@innovadeltech.com>
2019-10-10 21:54:03 +0000

    Merged in bugfix/MSS-305-production-issue-duplicate-products-hotfix (pull request #218)
    [MSS-305] Remove extra search.js file and fix issue of duplicate items on plp.
    
    Approved-by: Muhammad Haroon Rasheed <haroon.rasheed@innovadeltech.com>
    Approved-by: Haroon Chaudhry <haroon.chaudhry@innovadeltech.com>

--- movado_redesign/cartridges/app_custom_movado_redesign/cartridge/client/default/js/search.js
+++ movado_redesign/cartridges/app_custom_movado_redesign/cartridge/client/default/js/search.js
@@ -1,6 +1,5 @@
 'use strict';
 var processInclude = require('base/util');
-processInclude(require('movado/search'));
 $(document).ready(function () {
 	processInclude(require('./search/jquery-custom-select'));
 	processInclude(require('./search/searchresult'));
