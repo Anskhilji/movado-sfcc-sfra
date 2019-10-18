71a3adf261c02c2f6293e4b10321e407006d0c71
ma-it <ma-it@pixelmedia.com>
2019-10-18 22:16:55 +0500

    [MSS-317] Working on task(add engraving popup done)

--- movado_redesign/cartridges/app_custom_movado_redesign/cartridge/client/default/js/product/productDetailCustom.js
+++ movado_redesign/cartridges/app_custom_movado_redesign/cartridge/client/default/js/product/productDetailCustom.js
@@ -1,9 +1,32 @@
 'use strict';
 $('.carousel-nav').slick({
-	  slidesToShow: 5,
-	  slidesToScroll: 1,
-	  asNavFor: '.primary-images .main-carousel',
-	  dots: false,
-	  centerMode: true,
-	  focusOnSelect: true,
-	});
+	slidesToShow: 5,
+	slidesToScroll: 1,
+	asNavFor: '.primary-images .main-carousel',
+	dots: false,
+	centerMode: true,
+	focusOnSelect: true,
+});
+
+$(function() {
+    //----- OPEN
+    $('.pdp-v-one [pd-popup-open]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('pd-popup-open');
+        $('[pd-popup="' + targeted_popup_class + '"]').fadeIn(100);
+        $(".prices-add-to-cart-actions").css("z-index", "999999");
+        e.preventDefault();
+    });
+ 
+    //----- CLOSE
+    $('.pdp-v-one [pd-popup-close]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('pd-popup-close');
+        $('[pd-popup="' + targeted_popup_class + '"]').fadeOut(200);
+        $(".prices-add-to-cart-actions").css("z-index", "9");
+        e.preventDefault();
+    });
+    $('.pdp-v-one .popup-action-btns .save').on('click', function()  {
+        var textaraetext=$('.pdp-v-one .text-area textarea.valid').val();
+        $('.pdp-v-one .engravedtext').text(textaraetext);
+    });
+    save
+});
