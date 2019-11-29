c68671546743a2fca9628b0946a89f26e0757651
asimch <asim.shahzad@innovadeltech.com>
2019-11-29 21:55:18 +0500

    [MSS-325] Implemented logic to calculate shipping date by excluding weekends without embossing.

--- olivia-burton-uk/cartridges/app_custom_ob_uk/cartridge/models/shipping/shippingMethod.js
+++ olivia-burton-uk/cartridges/app_custom_ob_uk/cartridge/models/shipping/shippingMethod.js
@@ -2,9 +2,12 @@
 
 var ShippingMethodBase = module.superModule;
 
+var BasketMgr = require('dw/order/BasketMgr');
+var Calendar = require('dw/util/Calendar');
 var PromotionMgr = require('dw/campaign/PromotionMgr');
 var Resource = require('dw/web/Resource');
 var ShippingMgr = require('dw/order/ShippingMgr');
+var StringUtils = require('dw/util/StringUtils');
 
 var formatCurrency = require('*/cartridge/scripts/util/formatting').formatCurrency;
 
@@ -51,6 +54,52 @@ function getIsFree(shippingMethod, shipment) {
     return {isFree : isFree, freeShippingLabel : freeShippingLabel};
 }
 
+function getShippingDate(shippingMethod, shipment) {
+    var calendar = new Calendar();
+    var weekend;
+    var today = calendar.get(calendar.DAY_OF_WEEK);
+    var basket = BasketMgr.getCurrentBasket();
+    var deliverDate;
+    var deliveryDay;
+    
+//    var basketLastModifiedDate = StringUtils.formatCalendar(
+//            new Calendar(basket.getLastModified()), 'MM/dd/yyyy'
+//        )
+        
+    var basketLastModifiedTime = StringUtils.formatCalendar(
+            new Calendar(basket.getLastModified()), 'h:mm a'
+    )
+    
+    if (basketLastModifiedTime.indexOf('PM') > -1){
+        deliverDate = new Calendar(basket.getLastModified());
+        deliverDate.add(deliverDate.DAY_OF_MONTH, "2");
+        deliveryDay = deliverDate.get(deliverDate.DAY_OF_WEEK);
+    } else {
+        deliverDate = new Calendar(basket.getLastModified());
+        deliverDate.add(deliverDate.DAY_OF_MONTH, "1");
+        deliveryDay = deliverDate.get(deliverDate.DAY_OF_WEEK);
+    }
+    
+    
+    
+//    var basketLastModified = basket.getLastModified();
+//    var creationTimeHours = basketCreationTime.getHours();
+    switch (deliveryDay) {
+        case deliverDate.FRIDAY:
+            deliverDate.add(deliverDate.DAY_OF_MONTH, "4");
+            break;
+        case deliverDate.SATURDAY:
+            deliverDate.add(deliverDate.DAY_OF_MONTH, "3");
+            break;
+        case deliverDate.SUNDAY:
+            deliverDate.add(deliverDate.DAY_OF_MONTH, "2");
+            break;
+        default:
+            weekend = "Not Defined"
+    }
+    return today;
+}
+
 /**
  * Plain JS object that represents a DW Script API dw.order.ShippingMethod object
  * @param {dw.order.ShippingMethod} shippingMethod - the default shipment of the current basket
@@ -61,6 +110,7 @@ function ShippingMethodModel(shippingMethod, shipment) {
     if (shipment) {
         this.shippingCost = getShippingCost(shippingMethod, shipment);
         this.freeShippingContent = getIsFree(shippingMethod, shipment);
+        this.dayOfWeek = getShippingDate(shippingMethod, shipment);
     }
 }
 
