d8317b81df0a54ba11d1a8bdcdc4d30dc1809282
Muhammad Aurangzaib <muhammad.aurangzaib@innovadeltech.com>
2019-12-31 13:27:28 +0500

    [MSS:249] resolved review comments

--- movado_redesign/cartridges/app_custom_movado_redesign/cartridge/client/default/js/header/header.js
+++ movado_redesign/cartridges/app_custom_movado_redesign/cartridge/client/default/js/header/header.js
@@ -32,7 +32,7 @@ function remove_grayout (){
     $(".modal-background").removeClass("show-overlay").fadeOut();
     $(".modal-background").removeClass("d-block").fadeOut();
     $(".mobile-menu .nav-item > a").removeClass("fadeInLeft fast animated").addClass("fadeOutLeft fast animated");
-    $(".dropdown-menu .dropdown-item a").removeClass("animated fast fadeInLeft").addClass("animated fast fadeOutLeft");
+    $(".dropdown-menu .dropdown-item a").removeClass("fadeInLeft").addClass("animated fast fadeOutLeft");
     $(".country-selector-mobile .html-slot-container, .mobile-login .nav-link").removeClass("fadeInLeft fast animated").addClass("fadeOutLeft fast animated");
     setTimeout(function(){
         $(".mobile-menu").removeClass("in");
@@ -166,7 +166,7 @@ $(".navbar-toggler-custom").click(function(){
         $(".mobile-menu .nav-item > a").removeClass("fadeOutLeft fast animated").addClass("fadeInLeft fast animated");
         $(".country-selector-mobile .html-slot-container, .mobile-login .nav-link").removeClass("fadeOutLeft fast animated").addClass("fadeInLeft fast animated");
     },300);
-    $(".dropdown-menu .dropdown-item a").removeClass("animated fast fadeOutLeft").addClass("animated fast fadeInLeft");
+    $(".dropdown-menu .dropdown-item a").removeClass("fadeOutLeft").addClass("animated fast fadeInLeft");
     $(".modal-background").addClass("d-block").fadeIn();
     $(".mobile-menu").removeClass("animated delay-point-three fadeOut").addClass("animated");
     $('.slick-slider').slick("refresh");
