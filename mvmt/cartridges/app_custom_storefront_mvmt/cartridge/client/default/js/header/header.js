1b3b88df973a5d8bcad40e530ee07e725e7424eb
ma-it <ma-it@pixelmedia.com>
2020-01-14 21:17:23 +0500

    [MSS-381] MVMT header redesign, worked on mobile header static content

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/header/header.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/header/header.js
@@ -12,21 +12,35 @@ $(document).ready(function() {
 $(".desktop-search-icon").click(function(){
     $(this).toggleClass('active');
     $('.desktop-side-search').toggleClass('desktop-search-active');
-    $('.header-search-field').focus();
+    $('.desktop-side-search .header-search-field').focus();
 });
 
 $('.header-mobile-categories .header-mobile-category').click(function() {
-    var menuText = $(this).find('.header-mobile-category-text').attr('data-trigger-menu');
+    var menuText = $(this).find('.header-mobile-category-text').data('trigger-menu');
     $(this).toggleClass('active');
+    $('.mobile-nav .mobile-category-list').removeClass('active');
+    $('.mobile-nav, #overlay').addClass('active');
+    $('.mobile-nav').find("[data-mobile-category='" + menuText + "']").addClass('active');
 
     if ($(this).hasClass('active')) {
-        var siblingmenuText = $(this).siblings().find('.header-mobile-category-text').attr('data-trigger-menu');
+        var siblingmenuText = $(this).siblings().find('.header-mobile-category-text').data('trigger-menu');
         $(this).siblings().removeClass('active');
         $(this).siblings().find('.header-mobile-category-text').text(siblingmenuText);
         $(this).find('.header-mobile-category-text').text('+');
     } else {
         $(this).find('.header-mobile-category-text').text(menuText);
+        $('.mobile-nav .mobile-category-list').removeClass('active');
+        $('.mobile-nav, #overlay').removeClass('active');
     }
+});
+
+$('.mobile-search-button').click(function() {
+    $('.mobile-side-search').addClass('active');
+    $('.mobile-side-search .header-search-field').focus();
+});
+
+$('.mobile-search-icon').click(function() {
+    $('.mobile-side-search').removeClass('active');
 });
 
 /* $(".header-search-field").focusin(function(){
