edc14be54fcab37da63ba63befd452b5a3c64b83
asimch <asim.shahzad@innovadeltech.com>
2020-01-15 15:50:51 +0500

    MSS-253 Change order total to net price.

--- feeds/cartridges/app_custom_feed/cartridge/scripts/CheckoutRefundedOrders.js
+++ feeds/cartridges/app_custom_feed/cartridge/scripts/CheckoutRefundedOrders.js
@@ -86,7 +86,7 @@ function getOrdersDetails(order) {
     var paymentInstrument = order.paymentInstrument;
     var orderDetails = {
         orderNo : order.orderNo ? order.orderNo : '',
-        orderTotal : order.getMerchandizeTotalGrossPrice() ? formatMoney(order.getMerchandizeTotalGrossPrice()) : '',
+        orderTotal : order.getTotalNetPrice() ? formatMoney(order.getTotalNetPrice()) : '',
         creationDate : orderCreationDate ? commonUtils.getFormattedDate(orderCreationDate) : '',
         refundAmount : sapAlreadyRefundedAmount ? formatMoney(sapAlreadyRefundedAmount) : '',
         paymentMethod : paymentInstrument ? paymentInstrument.getPaymentMethod() : ''
