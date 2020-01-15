00690a2ba42672dc87fd0c3d8f6f1b9047f0a6a5
Muhammad Aurangzaib <muhammad.aurangzaib@innovadeltech.com>
2020-01-15 16:05:23 +0500

    [MSS:440] removed the extra information

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/components/newsLetterSubscription.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/components/newsLetterSubscription.js
@@ -46,16 +46,16 @@ function processSubscription(response) {
         wrapperContainer.removeClass('d-none');
         $('.submission-status div').text(response.message);
         if (!response.error) {
-            $('.submission-status div').attr('class', 'success');
-            $('.footer-more-fields').css('top', topPercentage);
-            $('#add-to-email-list').prop('checked', response.customerFound);
-            if (response.isanalyticsTrackingEnabled && response.userTracking) {
-                setAnalyticsTrackingByAJAX.userTracking = response.userTracking;
-                window.dispatchEvent(setAnalyticsTrackingByAJAX);
-            }
+            $('.news-letter-signUP-submission-status div').removeClass('error').addClass('success');
+//            $('.footer-more-fields').css('top', topPercentage);
+//            $('#add-to-email-list').prop('checked', response.customerFound);
+//            if (response.isanalyticsTrackingEnabled && response.userTracking) {
+//                setAnalyticsTrackingByAJAX.userTracking = response.userTracking;
+//                window.dispatchEvent(setAnalyticsTrackingByAJAX);
+//            }
         } else {
-            $('.submission-status div').attr('class', 'error');
-            $('.footer-more-fields').css('top', topPercentage);
+            $('.submission-status div').addClass('error');
+//            $('.footer-more-fields').css('top', topPercentage);
         }
     }
 }
@@ -77,7 +77,7 @@ $('#newsletterSubscribe').submit(function (e) {
         });
     } else {
         wrapperContainer.removeClass('d-none');
-        $('.submission-status div').text(wrapperContainer.data('errormsg')).attr('class', 'error');
+        $('.submission-status div').text(wrapperContainer.data('errormsg')).attr('class', 'ok');
         $('.footer-more-fields').css('top', topPercentage);
     }
 });
@@ -85,11 +85,11 @@ $('#newsletterSubscribe').submit(function (e) {
 $('#emailSubcriberBtn').click(function (e) {
     var topPercentage = top(false);
     $('#overlay').addClass('footer-form-overlay');
-    $('.footer-more-fields').addClass('is-active');
+    //$('.footer-more-fields').addClass('is-active');
     $('.footer-more-fields').css('top', topPercentage);
 });
 
 $('.close-footer-more, #overlay').click(function (e) {
-    $('.footer-more-fields').removeClass('is-active');
+    //$('.footer-more-fields').removeClass('is-active');
     $('#overlay').removeClass('footer-form-overlay');
 });
