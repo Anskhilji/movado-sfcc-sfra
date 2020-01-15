fc54501b7ad77d2d17e86f5a1cc90111a7ab0f6f
asimch <asim.shahzad@innovadeltech.com>
2020-01-15 17:21:34 +0500

    MSS-253 Change net price to gross price to include tax.

--- feeds/cartridges/app_custom_feed/cartridge/scripts/CheckoutRefundedOrders.js
+++ feeds/cartridges/app_custom_feed/cartridge/scripts/CheckoutRefundedOrders.js
@@ -86,7 +86,7 @@ function getOrdersDetails(order) {
     var paymentInstrument = order.paymentInstrument;
     var orderDetails = {
         orderNo : order.orderNo ? order.orderNo : '',
-        orderTotal : order.getTotalNetPrice() ? formatMoney(order.getTotalNetPrice()) : '',
+        orderTotal : order.getTotalGrossPrice() ? formatMoney(order.getTotalGrossPrice()) : '',
         creationDate : orderCreationDate ? commonUtils.getFormattedDate(orderCreationDate) : '',
         refundAmount : sapAlreadyRefundedAmount ? formatMoney(sapAlreadyRefundedAmount) : '',
         paymentMethod : paymentInstrument ? paymentInstrument.getPaymentMethod() : ''
