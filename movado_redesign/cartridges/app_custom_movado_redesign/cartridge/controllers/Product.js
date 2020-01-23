a747964de9e769ebec28dd0bef2671218344447e
Haroon Rasheed <haroon.rasheed@innovadeltech.com>
2020-01-23 17:15:51 +0000

    [MSS-234] Replace product show controll for AB test

--- movado_redesign/cartridges/app_custom_movado_redesign/cartridge/controllers/Product.js
+++ movado_redesign/cartridges/app_custom_movado_redesign/cartridge/controllers/Product.js
@@ -4,8 +4,13 @@ var server = require('server');
 var page = module.superModule;
 server.extend(page);
 
+var ABTestMgr = require('dw/campaign/ABTestMgr');
+
+var cache = require('*/cartridge/scripts/middleware/cache');
+var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
+var pageMetaData = require('*/cartridge/scripts/middleware/pageMetaData');
+
 server.replace('ShowAvailability', function (req, res, next) {
-    var ABTestMgr = require('dw/campaign/ABTestMgr');
     var template;
     var productHelper = require('*/cartridge/scripts/helpers/productHelpers');
     var showProductPageHelperResult = productHelper.showProductPage(req.querystring, req.pageMetaData);
@@ -23,5 +28,41 @@ server.replace('ShowAvailability', function (req, res, next) {
     });
     next();
 });
+
+server.replace('Show', cache.applyPromotionSensitiveCache, consentTracking.consent, function (req, res, next) {
+    var productHelper = require('*/cartridge/scripts/helpers/productHelpers');
+    var showProductPageHelperResult = productHelper.showProductPage(req.querystring, req.pageMetaData);
+    var productType = showProductPageHelperResult.product.productType;
+
+    var template;
+    if (ABTestMgr.isParticipant('MovadoRedesignPDPABTest','Control')) {
+        template = 'product/old/productDetails';
+    } else if (ABTestMgr.isParticipant('MovadoRedesignPDPABTest','render-modern-design')) {
+        template = 'product/modern/productDetails';
+    } else if (ABTestMgr.isParticipant('MovadoRedesignPDPABTest','render-traditional-design')){
+        template = 'product/traditional/productDetails';
+    } else {
+        template = 'product/old/productDetails';
+    }
+
+    if (productType === 'bundle') {
+        template = 'product/bundleDetails';
+    } else if (productType === 'set') {
+        template = 'product/setDetails';
+    }
+
+    if (!showProductPageHelperResult.product.online && productType !== 'set' && productType !== 'bundle') {
+        res.setStatusCode(404);
+        res.render('error/notFound');
+    } else {
+        res.render(template, {
+            product: showProductPageHelperResult.product,
+            addToCartUrl: showProductPageHelperResult.addToCartUrl,
+            resources: showProductPageHelperResult.resources,
+            breadcrumbs: showProductPageHelperResult.breadcrumbs
+        });
+    }
+    next();
+}, pageMetaData.computedPageMetaData);
 
 module.exports = server.exports();
