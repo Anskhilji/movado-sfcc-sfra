157eb300abb6cd93359ca18a45e4ecbedfb2d194
ma-it <ma-it@pixelmedia.com>
2020-01-28 23:35:49 +0500

    MVMT pdp product variations, product, design changes

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/productDetailCustom.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/productDetailCustom.js
@@ -7,6 +7,35 @@ $(document).ready(function() {
         focusOnSelect: true,
         infinite: false,
         dots: false,
-        arrows: true
+        arrows: true,
     });
+
+    $(function() {
+        var header = $('header').height();
+        var productdetail = $('.product-detail').height();
+        var stickybar = $('.sticky-bar');
+        console.log(productdetail)
+        $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
+
+
+            if (scroll >= productdetail) {
+                stickybar.css({
+                    top: header +'px'
+                });
+            }
+
+            else {
+                stickybar.css("top","-25px")
+            }
+            // if (scroll >= hieghtThreshold && scroll <=  hieghtThreshold_end ) {
+            //     header.addClass('dark');
+            //     stickybar.addClass('active');
+            //     stickybar.css("top",""+stickybar+"px")
+            // } else {
+            //     header.removeClass('dark');
+            //     menu.removeClass('dark');
+            // }
+        });
+    })
 });
