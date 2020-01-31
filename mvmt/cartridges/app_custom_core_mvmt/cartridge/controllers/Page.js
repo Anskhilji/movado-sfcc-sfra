82f420a553656ce2c8e248161d61e009c1a301eb
Hassan Tahir <hassan.tahir@innovadeltech.com>
2020-01-31 12:15:23 +0500

    [MSS-274] Pr comments are fixed

--- mvmt/cartridges/app_custom_core_mvmt/cartridge/controllers/Page.js
+++ mvmt/cartridges/app_custom_core_mvmt/cartridge/controllers/Page.js
@@ -7,20 +7,14 @@ var cache = require('*/cartridge/scripts/middleware/cache');
 var page = module.superModule;
 server.extend(page);
 
-server.replace(
-        'IncludeHeaderMenu',
-        server.middleware.include,
-        cache.applyPromotionSensitiveCache,
-        function (req, res, next) {
-            var catalogMgr = require('dw/catalog/CatalogMgr');
-            var Categories = require('*/cartridge/models/categories');
-            var siteRootCategory = catalogMgr.getSiteCatalog().getRoot();
-            var topLevelCategories = siteRootCategory.hasOnlineSubCategories() ?
-                    siteRootCategory.getOnlineSubCategories() : null;
-            res.render('/components/header/menu', new Categories(topLevelCategories));
-            next();
-        }
-    );
+server.replace('IncludeHeaderMenu', server.middleware.include, cache.applyPromotionSensitiveCache, function (req, res, next) {
+    var catalogMgr = require('dw/catalog/CatalogMgr');
+    var Categories = require('*/cartridge/models/categories');
+    var siteRootCategory = catalogMgr.getSiteCatalog().getRoot();
+    var topLevelCategories = siteRootCategory.hasOnlineSubCategories() ? siteRootCategory.getOnlineSubCategories() : null;
+    res.render('/components/header/menu', new Categories(topLevelCategories));
+    next();
+});
 
 server.replace(
     'IncludeHeader',
