b3b4f1fe9d8ada0b41a66bd04a3ecd5be9b8d20e
ma-it <ma-it@pixelmedia.com>
2020-02-04 18:42:35 +0500

    [MSS-381] resolve PR reviews

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/header/header.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/header/header.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
     $('.submenu-control').hover(function(event) {
         $(this).parent().find('.submenu-control').removeClass('active');
         $(this).addClass('active');
-        var currentCategory = $(this).data('category');
-        var activeSubMenu = $('.submenu').find("[data-parentcategory='" + currentCategory + "']");
-        activeSubMenu.siblings('.submenu-container').addClass('d-none');
-        activeSubMenu.removeClass('d-none');
+        var $currentCategory = $(this).data('category');
+        var $activeSubMenu = $('.submenu').find("[data-parentcategory='" + $currentCategory + "']");
+        $activeSubMenu.siblings('.submenu-container').addClass('d-none');
+        $activeSubMenu.removeClass('d-none');
     });
 });
 
@@ -18,19 +18,19 @@ $('.desktop-search-icon').click(function(){
 });
 
 $('.header-mobile-categories .header-mobile-category').click(function() {
-    var menuText = $(this).find('.header-mobile-category-text').data('trigger-menu');
+    var $menuText = $(this).find('.header-mobile-category-text').data('trigger-menu');
     $(this).toggleClass('active');
     $('.mobile-nav .mobile-category-list').removeClass('active');
     $('.mobile-nav, #overlay').addClass('active');
-    $('.mobile-nav').find("[data-mobile-category='" + menuText + "']").addClass('active');
+    $('.mobile-nav').find("[data-mobile-category='" + $menuText + "']").addClass('active');
 
     if ($(this).hasClass('active')) {
-        var siblingmenuText = $(this).siblings().find('.header-mobile-category-text').data('trigger-menu');
+        var $siblingmenuText = $(this).siblings().find('.header-mobile-category-text').data('trigger-menu');
         $(this).siblings().removeClass('active');
-        $(this).siblings().find('.header-mobile-category-text').text(siblingmenuText);
+        $(this).siblings().find('.header-mobile-category-text').text($siblingmenuText);
         $(this).find('.header-mobile-category-text').text('+');
     } else {
-        $(this).find('.header-mobile-category-text').text(menuText);
+        $(this).find('.header-mobile-category-text').text($menuText);
         $('.mobile-nav .mobile-category-list').removeClass('active');
         $('.mobile-nav, #overlay').removeClass('active');
     }
@@ -38,21 +38,21 @@ $('.header-mobile-categories .header-mobile-category').click(function() {
 
 $('#overlay').click(function() {
     $('.mobile-nav, #overlay').removeClass('active');
-    var menuText = $('.header-mobile-categories .active .header-mobile-category-text').data('trigger-menu');
-    $('.header-mobile-categories .active .header-mobile-category-text').text(menuText);
+    var $menuText = $('.header-mobile-categories .active .header-mobile-category-text').data('trigger-menu');
+    $('.header-mobile-categories .active .header-mobile-category-text').text($menuText);
     $('.header-mobile-categories .header-mobile-category').removeClass('active');
 });
 
 $('.mobile-search-button').click(function() {
-    var navwidth = $('header .nav-right').width();
-    $('.mobile-search-close').width(navwidth);
+    var $navwidth = $('header .nav-right').width();
+    $('.mobile-search-close').width($navwidth);
     $('.mobile-side-search').addClass('active');
     $('.mobile-side-search .header-search-field').focus();
 });
 
 $(window).on("load resize", function(e) {
-    var navwidth = $('header .nav-right').width();
-    $('.mobile-search-close').width(navwidth);
+    var $navwidth = $('header .nav-right').width();
+    $('.mobile-search-close').width($navwidth);
 });
 
 $('.mobile-search-icon').click(function() {
@@ -66,7 +66,6 @@ $('.mobile-nav .mobile-subnav-btn').click(function() {
 $('.header-search-field').focusout(function(){
     if (!$(".header-search-field").val()) {
         $(".search-recomendation").fadeIn();
-        console.log("this is empaty")
     } else {
         $(".search-recomendation").fadeOut();
     }
--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/search.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var processInclude = require('base/util');
-$(document).ready(function () {
-	processInclude(require('./search/jquery-custom-select'));
-	processInclude(require('./search/searchresult'));
-	processInclude(require('./search/search'));
-});
