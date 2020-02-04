8f7f3f5e047390190886092404f7167f43f80130
asimch <asim.shahzad@innovadeltech.com>
2020-02-04 21:10:00 +0500

    MSS-320 Fix confirmation status bug in case of error and decline .

--- link_riskified/cartridges/int_custom_riskified/cartridge/controllers/RiskifiedEndpoint.js
+++ link_riskified/cartridges/int_custom_riskified/cartridge/controllers/RiskifiedEndpoint.js
@@ -7,8 +7,13 @@
 var server = require('server');
 var page = module.superModule;
 server.extend(page);
-var riskifiedResponseResult = require('*/cartridge/scripts/riskified/RiskifiedParseResponseResult');
+
+var Order = require('dw/order/Order');
 var OrderMgr = require('dw/order/OrderMgr');
+var Transaction = require('dw/system/Transaction');
+
+var checkoutLogger = require('*/cartridge/scripts/helpers/customCheckoutLogger').getLogger();
+var riskifiedResponseResult = require('*/cartridge/scripts/riskified/RiskifiedParseResponseResult');
 
 /**
  * This function handles order analysis status request from Riskified. This
@@ -23,8 +28,22 @@ server.append('AnalysisNotificationEndpoint', function (req, res, next) {
     var order = OrderMgr.getOrder(orderId);
     var viewData = res.getViewData();
     var isError = viewData.isError ? viewData.isError : false;
+    var responseMessage = viewData.responseMessage ? viewData.responseMessage : "";
     if (order && !isError) {
         riskifiedResponseResult.parseRiskifiedResponse(order);
+    } else if (order && isError == true) {
+        Transaction.wrap(function () {
+            //if order status is CREATED
+            if (order.getStatus() == Order.ORDER_STATUS_CREATED) {
+                checkoutLogger.error('(RiskifiedParseResponseResult) -> parseRiskifiedResponse: There is an error with message ' + responseMessage + ' and riskified failed the order and order status is failed and order number is: ' + order.orderNo);
+                OrderMgr.failOrder(order);  //Order must be in status CREATED
+                order.setConfirmationStatus(Order.CONFIRMATION_STATUS_NOTCONFIRMED);
+            } else { //Only orders in status OPEN, NEW, or COMPLETED can be cancelled.
+                checkoutLogger.error('(RiskifiedParseResponseResult) -> parseRiskifiedResponse: There is an error with message ' + responseMessage + ' and order status is OPEN, NEW, or COMPLETED can be cancelled and order number is: ' + order.orderNo);
+                OrderMgr.cancelOrder(order);
+                order.setConfirmationStatus(Order.CONFIRMATION_STATUS_NOTCONFIRMED);
+            }
+        });
     }
 });
 
