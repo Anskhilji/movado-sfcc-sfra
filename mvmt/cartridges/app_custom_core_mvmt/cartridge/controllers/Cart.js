ce0a9142f8ef97df2bbb6497e1c62875e259b0e7
Hassan Tahir <hassan.tahir@innovadeltech.com>
2020-02-07 18:21:41 +0500

    [MSS-274] Pr comments are fixed

--- mvmt/cartridges/app_custom_core_mvmt/cartridge/controllers/Cart.js
+++ mvmt/cartridges/app_custom_core_mvmt/cartridge/controllers/Cart.js
@@ -5,22 +5,6 @@ var server = require('server');
 var page = module.superModule;
 server.extend(page);
 
-server.replace('MiniCart', function(req, res, next) {
-    var BasketMgr = require('dw/order/BasketMgr');
-
-    var currentBasket = BasketMgr.getCurrentBasket();
-    var quantityTotal;
-
-    if (currentBasket) {
-        quantityTotal = currentBasket.productQuantityTotal;
-    } else {
-        quantityTotal = 0;
-    }
-
-    res.render('/components/header/miniCart', { quantityTotal: quantityTotal });
-    next();
-});
-
 server.append('MiniCartShow', function(req, res, next) {
     res.setViewData({
         paypalButtonImg: customCartHelpers.getContentAssetContent('ca-paypal-button')
