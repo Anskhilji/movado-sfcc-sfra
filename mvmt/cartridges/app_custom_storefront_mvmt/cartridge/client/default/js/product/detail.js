394ecc89ec8c7150fe28cd4949bdde304e2c7f24
ma-it <ma-it@pixelmedia.com>
2020-02-07 22:14:04 +0500

    [MSS-551] Gallery slider, working

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/detail.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/detail.js
@@ -13,11 +13,19 @@ module.exports = {
             dots: false,
             arrows: true,
             autoplay: true,
+            prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
+            nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
             responsive: [
                 {
                     breakpoint: 768,
                     settings: {
                         slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 540,
+                    settings: {
+                        slidesToShow: 1,
                     }
                 },
             ]
