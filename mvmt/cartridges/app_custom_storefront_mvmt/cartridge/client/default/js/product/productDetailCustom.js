bc6af67f0fa3646bc8a73bfe232f979041a7235d
ma-it <ma-it@pixelmedia.com>
2020-02-13 16:10:05 +0500

    [MSS-447] resolve PR reviews

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/productDetailCustom.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/productDetailCustom.js
@@ -1,41 +1,22 @@
 'use strict';
 
 $('.carousel-pagination').slick({
-  slidesToShow: 6,
-  slidesToScroll: 1,
-  asNavFor: '.primary-images .main-carousel',
-  focusOnSelect: true,
-  infinite: false,
-  vertical: true,
-  verticalSwiping: true,
-  arrows: false,
-  responsive: [
-    {
-      breakpoint: 992,
-      settings: {
-        vertical: false,
-        verticalSwiping: false,
-      }
-    },
-  ]
-});
-  $('.primary-images .main-carousel').slick({
-    slidesToShow: 1,
+    slidesToShow: 6,
     slidesToScroll: 1,
-    dots: false,
-    arrows:false,
+    asNavFor: '.primary-images .main-carousel',
     focusOnSelect: true,
-    asNavFor: '.carousel-pagination',
+    infinite: false,
+    vertical: true,
+    verticalSwiping: true,
+    arrows: false,
     responsive: [
-      {
-        breakpoint: 544,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-          dots:true
-        }
-      },
+        {
+            breakpoint: 992,
+            settings: {
+                vertical: false,
+                verticalSwiping: false,
+            }
+        },
     ]
 });
 
@@ -56,21 +37,21 @@ $('.main-carousel .carousel-tile').zoom({
 });
 
 $('.primary-images .main-carousel').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  dots: false,
-  arrows:false,
-  focusOnSelect: true,
-  asNavFor: '.carousel-pagination',
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        dots:true
-      }
-    },
-  ]
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows:false,
+    focusOnSelect: true,
+    asNavFor: '.carousel-pagination',
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                dots:true
+            }
+        },
+    ]
 });
