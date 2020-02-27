94365c8684fb1d9a9e7857c4c87c650590252fb1
Nouman_Ahmad <noman.ahmad@innovadeltech.com>
2020-02-27 18:26:48 +0500

    Implemen A/B test for menu template.

--- ob_redesign/cartridges/app_custom_ob_redesign/cartridge/controllers/Page.js
+++ ob_redesign/cartridges/app_custom_ob_redesign/cartridge/controllers/Page.js
@@ -7,6 +7,33 @@ var cache = require('*/cartridge/scripts/middleware/cache');
 var page = module.superModule;
 server.extend(page);
 
+server.replace(
+        'IncludeHeaderMenu',
+        server.middleware.include,
+        cache.applyPromotionSensitiveCache,
+        function (req, res, next) {
+            var ABTestMgr = require('dw/campaign/ABTestMgr');
+            var catalogMgr = require('dw/catalog/CatalogMgr');
+            var Categories = require('*/cartridge/models/categories');
+            var siteRootCategory = catalogMgr.getSiteCatalog().getRoot();
+            
+            var topLevelCategories = siteRootCategory.hasOnlineSubCategories() ?
+                    siteRootCategory.getOnlineSubCategories() : null;
+            
+            var menuTemplate = null;
+            // A/B testing for header design
+            if (ABTestMgr.isParticipant('OBRedesignABTest','Control')) {
+                menuTemplate = '/components/header/old/menu';
+            } else if (ABTestMgr.isParticipant('OBRedesignABTest','render-new-design')) {
+                menuTemplate = '/components/header/menu';
+            } else {
+                menuTemplate = '/components/header/old/menu';
+                }
+                res.render(menuTemplate, new Categories(topLevelCategories));
+                next();
+            }
+);
+
 server.get(
     'IncludeHeader',
     server.middleware.include,
