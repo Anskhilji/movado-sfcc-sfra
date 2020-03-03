1436fbf2df0ec723993a5cc355d17b5ce3c79836
muhammad-abrar <muhammad.abrar@innovadeltech.com>
2020-03-03 22:45:52 +0500

    [MSS-502] OBUK header redesign solve PR reviews

--- oliviaburton_redesign/cartridges/app_custom_ob_redesign/cartridge/client/default/js/main.js
+++ oliviaburton_redesign/cartridges/app_custom_ob_redesign/cartridge/client/default/js/main.js
@@ -1,7 +1,5 @@
 var processInclude = require('base/util');
-processInclude(require('olivia-burton-uk/main'));
 
 $(document).ready(function () {
-    processInclude(require('./header/header'));
+    processInclude(require('olivia-burton-uk/main'));
 });
-
