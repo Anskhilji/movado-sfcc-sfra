8e5c498ba42032af51d6e374fbc3cab1934d3a74
Hassan Tahir <hassan.tahir@innovadeltech.com>
2020-03-25 21:17:53 +0500

    [MSS-261] MVMT eshopworld integreation

--- mvmt/cartridges/app_custom_core_mvmt/cartridge/controllers/Account.js
+++ mvmt/cartridges/app_custom_core_mvmt/cartridge/controllers/Account.js
@@ -9,12 +9,27 @@ server.append('Login', server.middleware.https, function (req, res, next) {
     var isMiniCart = empty(req.querystring.isMiniCart) ? false : req.querystring.isMiniCart;
     var viewData = res.getViewData();
     var authenticatedCustomer = viewData.authenticatedCustomer;
-    if (!empty(authenticatedCustomer) && isMiniCart) {
-        res.json({
-            success: true,
-            redirectUrl: URLUtils.url('Checkout-Login').toString()
-        });
+    // Custom Start: Adding ESW cartridge integration
+    if (authenticatedCustomer && authenticatedCustomer.authenticated) {
+        var checkoutLogin = req.querystring.checkoutLogin;
+        var eswHelper = require('*/cartridge/scripts/helper/eswHelper').getEswHelper();
+        if (eswHelper.getEShopWorldModuleEnabled()) {
+           var checkoutOrAccountUrl = isMiniCart ? URLUtils.url('Checkout-Login').toString() : URLUtils.url('Account-Show').toString();
+            res.json({
+                success: true,
+                redirectUrl: checkoutLogin
+                    ? URLUtils.https('EShopWorld-PreOrderRequest').toString()
+                    : checkoutOrAccountUrl
+            });
+        } else if (!eswHelper.getEShopWorldModuleEnabled() && isMiniCart) {
+            res.json({
+                success: true,
+                redirectUrl: URLUtils.url('Checkout-Login').toString()
+            });
+        }
     }
+    // Custom End
+    res.setViewData(viewData);
     next();
 });
 
