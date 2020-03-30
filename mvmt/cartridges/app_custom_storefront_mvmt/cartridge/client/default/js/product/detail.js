8edc07b21a765625ea0998271d2224c6ad88b700
ma-it <ma-it@pixelmedia.com>
2020-03-30 18:36:00 +0500

    [MSS-265] MVMT- PDP redesign-design-fixes

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/detail.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/detail.js
@@ -3,6 +3,36 @@ var movadoDetail = require('movado/product/detail');
 
 module.exports = {
 
+    clickEvents: function () {
+
+        $('.pdp-tabs-nav button').on('click', function(e) {
+            var thistab = $(this).data('tab');
+            $('.'+thistab+'').addClass('active').siblings().removeClass('active');
+            $(this).addClass('active').siblings().removeClass('active');
+
+            if ($(window).width() < 786) {
+                $('.pdp-mobile-accordian').removeClass('active');
+                $(this).find('.pdp-mobile-accordian').addBack('active')
+            }
+
+            setTimeout(function(){
+                $('.'+thistab+'').addClass('fadeIn').siblings().removeClass('fadeIn');
+             }, 300);
+        });
+
+        $('.pdp-mobile-accordian').on('click', function(e) {
+            $(this).toggleClass('active').siblings().slideToggle();
+        });
+
+        $('.call-see-fit-popup').on('click', function(e) {
+            $('.size-guide, #overlay').addClass('active');
+        });
+
+        $('.size-guide-close').on('click', function(e) {
+            $('.size-guide, #overlay').removeClass('active');
+        });
+    },
+
     gallerySlider: function () {
         $('.gallery-slider').slick({
             dots: true,
@@ -23,9 +53,100 @@ module.exports = {
                     }
                 },
                 {
-                    breakpoint: 540,
+                    breakpoint: 544,
                     settings: {
                         slidesToShow: 1,
+                    }
+                },
+            ]
+        });
+    },
+
+    linkedSlider: function () {
+        $('.linked-products').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            focusOnSelect: true,
+            infinite: false,
+            dots: false,
+            arrows: true,
+        });
+    },
+
+    stickybar: function () {
+        $(function() {
+            var $header = $('.header-menu-wrapper').height();
+            var $productdetail = $('.product-detail').height();
+            var $stickybar = $('.sticky-bar');
+            $(window).scroll(function() {
+                var $scroll = $(window).scrollTop();
+                console.log('w')
+                if ($scroll >= $productdetail) {
+                    $stickybar.css('top', $header +'px');
+                } else {
+                    $stickybar.css('top','-45px');
+                }
+            });
+        });
+    },
+
+    carouselPagination: function () {
+        $('.carousel-pagination').slick({
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            asNavFor: '.primary-images .main-carousel',
+            focusOnSelect: true,
+            infinite: false,
+            vertical: true,
+            verticalSwiping: true,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        vertical: false,
+                        verticalSwiping: false,
+                    }
+                },
+            ]
+        });
+    },
+
+    zoomFeature: function () {
+        $('.main-carousel .carousel-tile').zoom({
+            onZoomIn:function(){
+                $('.normal-zoom, .carousel-pagination').addClass('opacity-0');
+                $('.zoom-img').addClass('zoomed-img');
+                $('.main-carousel').addClass('pl-0');
+                $('.zoom-out, .main-carousel .carousel-tile').addClass('active');
+            },
+
+            onZoomOut:function(){
+                $('.normal-zoom, .carousel-pagination').removeClass('opacity-0');
+                $('.zoom-img').removeClass('zoomed-img');
+                $('.main-carousel').removeClass('pl-0');
+                $('.zoom-out, .main-carousel .carousel-tile').removeClass('active');
+            }
+        });
+    },
+
+    primarySlider: function () {
+
+        $('.primary-images .main-carousel').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false,
+            arrows:false,
+            focusOnSelect: true,
+            asNavFor: '.carousel-pagination',
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        arrows: false,
+                        dots:true
                     }
                 },
             ]
