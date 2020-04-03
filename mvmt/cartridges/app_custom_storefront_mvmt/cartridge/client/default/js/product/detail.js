25d8e332d1d84800a4a21e802515c2c2534b79bc
ma-it <ma-it@pixelmedia.com>
2020-04-03 14:36:29 +0500

    [MSS-265] MVMT- PDP redesign-design-bug-fix

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/detail.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/detail.js
@@ -94,8 +94,8 @@ module.exports = {
             dots: false,
             arrows: true,
             autoplay: false,
-            prevArrow: '<button type="button" data-role="none" class="slick-prev slick-arrow" aria-label="Previous" tabindex="0" role="button"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
-            nextArrow: '<button type="button" data-role="none" class="slick-next slick-arrow" aria-label="Next" tabindex="0" role="button"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
+            prevArrow: '<button type="button" data-role="none" class="slick-prev slick-arrow" aria-label="Previous" tabindex="0" role="button"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
+            nextArrow: '<button type="button" data-role="none" class="slick-next slick-arrow" aria-label="Next" tabindex="0" role="button"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
             responsive: [
                 {
                     breakpoint: 768,
