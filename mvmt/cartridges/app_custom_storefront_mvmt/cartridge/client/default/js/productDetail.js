e9efa7b5cbd352b3b801376361e4357e0d1fb524
ma-it <ma-it@pixelmedia.com>
2020-04-07 14:42:28 +0500

    frontend fixes

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/productDetail.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/productDetail.js
@@ -4,7 +4,6 @@ var processInclude = require('base/util');
 
 $(document).ready(function () {
     processInclude(require('movado/productDetail'));
-    processInclude(require('./product/detail'));
     processInclude(require('./product/zoom'));
-    processInclude(require('./product/productDetailCustom'));
+    processInclude(require('./product/detail'));
 });
