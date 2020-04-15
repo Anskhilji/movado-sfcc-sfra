89f9e121432386ba8f124971f09b1f006aad33ea
Nouman_Ahmad <noman.ahmad@innovadeltech.com>
2020-04-15 20:34:40 +0500

    [MSS-644] Fix MCSUS Cache issue.

---
+++ mcsus/cartridges/app_custom_core_mcsus/cartridge/controllers/Product.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var server = require('server');
+var Site = require('dw/system/Site');
+var page = module.superModule;
+server.extend(page);
+var productHelper = require('*/cartridge/scripts/helpers/productHelpers');
+
+server.get('ShowMcsProdutPrice', function (req, res, next) { 
+    var showProductPageHelperResult = productHelper.showProductPage(req.querystring, req.pageMetaData);
+    res.render('product/components/mcsProductPrice', {
+        isPLPProduct: req.querystring.isPLPProduct ? req.querystring.isPLPProduct : false,
+        product: showProductPageHelperResult.product,
+        loggedIn: req.currentCustomer.raw.authenticated,
+        restrictAnonymousUsersOnSalesSites: Site.getCurrent().preferences.custom.restrictAnonymousUsersOnSalesSites
+    });
+    next();
+});
+
+server.get('ShowMcsAffirmText', function (req, res, next) {
+    var showProductPageHelperResult = productHelper.showProductPage(req.querystring, req.pageMetaData);
+    res.render('product/components/mcsAffirmText', {
+        isPLPProduct: req.querystring.isPLPProduct ? req.querystring.isPLPProduct : false,
+        loggedIn: req.currentCustomer.raw.authenticated,
+        product: showProductPageHelperResult.product,
+        restrictAnonymousUsersOnSalesSites: Site.getCurrent().preferences.custom.restrictAnonymousUsersOnSalesSites
+    }); 
+    next();
+});
+
+
+module.exports = server.exports();
