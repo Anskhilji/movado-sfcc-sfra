b90fd84f74a804aa56be4308295130d866b1f963
Muhammad Aurangzaib <muhammad.aurangzaib@innovadeltech.com>
2020-04-16 16:03:58 +0500

    config updated

--- movado/cartridges/app_custom_movado/cartridge/client/default/js/components/slickCarousel.js
+++ movado/cartridges/app_custom_movado/cartridge/client/default/js/components/slickCarousel.js
@@ -7,9 +7,14 @@ var initCarousel = function ($container) {
     var $parent = $container || $body;
     $parent.find('.js-carousel').each(function () {
         var $carousel = $(this);
+        var test1 = $carousel.data();
+        console.log(test1);
         var config = $carousel.data() && $carousel.data().carouselConfig || {};
-        $carousel.slick(config);
-        $carousel.show();
+        config = atob(config);
+        config = JSON.parse(config);
+        console.log(config);
+        $carousel.slick(config);  
+        $carousel.show();  
         if ($carousel.hasClass('banner-carousel')) {
             $carousel.on('afterChange', function () {
                 if ($carousel.slick('slickCurrentSlide') === 0) {
