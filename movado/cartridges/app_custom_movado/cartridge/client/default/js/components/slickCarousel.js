3c63984fb9a8dcb56f11cf68ebf73ffe279c2ee4
Muhammad Aurangzaib <muhammad.aurangzaib@innovadeltech.com>
2020-04-20 17:45:30 +0500

    resolved review comments and changed the rendereing of content asset

--- movado/cartridges/app_custom_movado/cartridge/client/default/js/components/slickCarousel.js
+++ movado/cartridges/app_custom_movado/cartridge/client/default/js/components/slickCarousel.js
@@ -20,27 +20,6 @@ var initCarousel = function ($container) {
     }); 
 }; 
 
-var initCarouselBottomSlot = function ($container) {
-    var $parent = $container || $body;
-    $parent.find('.js-carousel-bottom-slot').each(function () {
-        var $carousel = $(this);
-        var config = $carousel.data() && $carousel.data().carouselConfig || {};
-        config = atob(config);
-        config = JSON.parse(config);
-        console.log(config);
-        $carousel.slick(config);  
-        $carousel.show();  
-        if ($carousel.hasClass('banner-carousel')) {
-            $carousel.on('afterChange', function () {
-                if ($carousel.slick('slickCurrentSlide') === 0) {
-                    $carousel.slick('slickPause');
-                }
-            });
-        }
-    }); 
-}; 
-
 module.exports = {
-    initCarousel: initCarousel,
-    initCarouselBottomSlot: initCarouselBottomSlot
+    initCarousel: initCarousel
 };
