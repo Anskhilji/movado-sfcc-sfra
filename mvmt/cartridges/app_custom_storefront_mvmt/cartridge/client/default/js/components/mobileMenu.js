9d4d8bc63b0afa78a866a359fa49447b07813233
Muhammad Aurangzaib <muhammad.aurangzaib@innovadeltech.com>
2020-04-27 12:49:28 +0500

    back button js issue is resolved

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/components/mobileMenu.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/components/mobileMenu.js
@@ -1,6 +1,16 @@
 'use strict';
 
 module.exports = function () {
+    $('.navbar-nav').on('click', '.back-menu', function (e) {
+        e.preventDefault();
+        $('.mvmt-menu-group .sub-dropdown').removeClass('show');
+    });
+
+    $('.navbar-nav').on('click', '.back-submenu', function (e) {
+        e.preventDefault();
+        $('.dropdown-item-mvmt').removeClass('show');
+    });
+
     $(".mvmt-menu-group .dropdown" ).click(function() {
         $(this).addClass('show');
     });
