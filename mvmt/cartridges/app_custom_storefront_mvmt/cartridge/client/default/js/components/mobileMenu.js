64cf2f8f72cb77ac10eab9d59b3129ef783207dd
Muhammad Aurangzaib <muhammad.aurangzaib@innovadeltech.com>
2020-04-29 17:51:39 +0500

    background, header logo, alignment fixed

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/components/mobileMenu.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/components/mobileMenu.js
@@ -1,6 +1,12 @@
 'use strict';
 
 module.exports = function () {
+    $('.navbar > .close-button').on('click', function (e) {
+        e.preventDefault();
+        $('.menu-toggleable-left').removeClass('in');
+        $('.modal-background').hide();
+    });
+    
     $('.navbar-nav').on('click', '.back-menu', function (e) {
         e.preventDefault();
         $('.mvmt-menu-group .sub-dropdown').removeClass('show');
@@ -15,12 +21,21 @@ module.exports = function () {
         $(this).addClass('show');
     });
     
+    $('.menu-icon-mvmt-hamburger').click(function() {
+        $('.third-level-men-menu').addClass('show');
+    });
+    
     $(".third-level-menu-tab .btn" ).click(function() {
         var tablink = $(this).data('tab');
         $('.tab-content').removeClass('show');
         $('.third-level-menu-tab .btn').removeClass('active');
         $(''+tablink+'').addClass('show');
         $(this).addClass('active');
+    });
+    
+    $(window).on('resize load',function () {
+        var height = $(window).height()-212;
+        $('.tab-content ul').height(height);
     });
     
 };
