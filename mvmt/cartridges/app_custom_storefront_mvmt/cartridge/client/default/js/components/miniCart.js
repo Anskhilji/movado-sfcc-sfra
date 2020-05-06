c17b68fd3dfa2d48d0932bc6b458112e9f1bc841
Hassan Tahir <hassan.tahir@innovadeltech.com>
2020-05-06 13:01:45 +0500

    [MSS-660] Mvmt mini cart design is updated

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/components/miniCart.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/components/miniCart.js
@@ -15,7 +15,7 @@ function setMiniCartProductSummaryHeight () {
     var $miniCartFooterHeight = isNaN(parseInt($('.mini-cart-data .minicart-footer').outerHeight(true))) ? 166 : parseInt($('.mini-cart-data .minicart-footer').outerHeight(true));
     $miniCartHeaderHeight = isNaN($miniCartHeaderHeight) ? 97 : $miniCartHeaderHeight;
     var $productSummaryHeight = $miniCartHeight - ($miniCartFooterHeight + $miniCartHeaderHeight);
-    $('.mini-cart-data .product-summary').css('padding-bottom', '');
+//    $('.mini-cart-data .product-summary').css('padding-bottom', '');
     $('.mini-cart-data .product-summary').css('max-height', $productSummaryHeight);
 }
 
@@ -24,9 +24,22 @@ module.exports = function () {
 
     $(window).resize( function() {
         if ($('.mini-cart-data .popover.show').length > 0) {
-            var $bannerHeight = $('.header-banner').height();
-            var $windowHeight = $(window).height() - $bannerHeight;
-            $('.mini-cart-data .popover').css({'max-height': $windowHeight+'px', 'top': $bannerHeight+'px'});
+            var $bannerHeight = $('.header-banner').outerHeight(true);
+            var $headerContainer = $('.header-container').outerHeight(true);
+            var $headerHeight = $bannerHeight + $headerContainer;
+            var $windowHeight = $(window).height() - $headerHeight;
+            var screenSize= $(window).width();
+            var mediumScreenSize = 992;
+            if (screenSize != null) {
+                if (screenSize <= mediumScreenSize) {
+                    $headerHeight = $bannerHeight + $headerContainer;
+                    $windowHeight = $(window).height() - $headerHeight;
+                } else {
+                    $headerHeight = $bannerHeight;
+                    $windowHeight = $(window).height() - $headerHeight;
+                }
+            }
+            $('.mini-cart-data .popover').css({'max-height': $windowHeight + 'px', 'top': $headerHeight + 'px'});
             var $miniCartHeaderTitle = parseInt($('.mini-cart-data .popover .title-free-shipping').outerHeight(true));
             var $miniCartHeaderHeight = $miniCartHeaderTitle;
             if ($('.mini-cart-header').is(':visible')) {
@@ -41,9 +54,22 @@ module.exports = function () {
     });
 
     $(window).on('load', function() {
-        var $bannerHeight = $('.header-banner').height();
-        var $windowHeight = $(window).height() - $bannerHeight;
-        $('.mini-cart-data .popover').css({'max-height': $windowHeight+'px', 'top': $bannerHeight+'px'});
+        var $bannerHeight = $('.header-banner').outerHeight(true);
+        var $headerContainer = $('.header-container').outerHeight(true);
+        var $headerHeight = $bannerHeight + $headerContainer;
+        var $windowHeight = $(window).height() - $headerHeight;
+        var screenSize= $(window).width();
+        var mediumScreenSize = 992;
+        if (screenSize != null) {
+            if (screenSize <= mediumScreenSize) {
+                $headerHeight = $bannerHeight + $headerContainer;
+                $windowHeight = $(window).height() - $headerHeight;
+            } else {
+                $headerHeight = $bannerHeight;
+                $windowHeight = $(window).height() - $headerHeight;
+            }
+        }
+        $('.mini-cart-data .popover').css({'max-height': $windowHeight + 'px', 'top': $headerHeight + 'px'});
         setMiniCartProductSummaryHeight();
     });
 
