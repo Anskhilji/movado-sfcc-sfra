b4fb6d92f257e1809b81c763844a33fed7b53522
muhammad-abrar <muhammad.abrar@innovadeltech.com>
2020-05-14 05:35:30 -0700

    [MSS-662] MVMT PDP tabs design

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/detail.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/detail.js
@@ -59,21 +59,14 @@ module.exports = {
 
         $('.pdp-tabs-nav button').on('click', function(e) {
             var thistab = $(this).data('tab');
-            $('.'+thistab+'').addClass('active').siblings().removeClass('active');
             $(this).addClass('active').siblings().removeClass('active');
-
-            if ($(window).width() < 786) {
-                $('.pdp-mobile-accordian').removeClass('active');
-                $(this).find('.pdp-mobile-accordian').addBack('active')
-            }
-
-            setTimeout(function(){
-                $('.'+thistab+'').addClass('fadeIn').siblings().removeClass('fadeIn');
-             }, 300);
+            $('html, body').animate({
+                scrollTop: $('.'+ thistab +'').offset().top - 100
+            }, 1000);
         });
 
         $('.pdp-mobile-accordian').on('click', function(e) {
-            $(this).toggleClass('active').siblings().slideToggle();
+            $(this).toggleClass('active').siblings().toggleClass('active');
         });
 
         $('.call-see-fit-popup').on('click', function(e) {
