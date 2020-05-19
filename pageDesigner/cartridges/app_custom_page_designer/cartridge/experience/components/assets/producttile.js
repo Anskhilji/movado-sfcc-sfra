35bf1fdef124fdef328bc8b3e984bf640a24f38d
Nouman_Ahmad <noman.ahmad@innovadeltech.com>
2020-05-19 10:44:50 +0500

    Setup cartrige for MVMT Page Designer

---
+++ pageDesigner/cartridges/app_custom_page_designer/cartridge/experience/components/assets/producttile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var Template = require('dw/util/Template');
+var HashMap = require('dw/util/HashMap');
+
+/**
+ * Render logic for the assets.productTile.
+ * 
+ * @param {dw.experience.PageScriptContext} context The page script context object.
+ * @returns {string} The Template text.
+ */
+module.exports.render = function (context) {
+    var content = context.content;
+    var model = new HashMap();
+    model.product = content.product;
+    model.ratings = content.ratings;
+    model.swatches = content.swatches;
+
+    return new Template('experience/components/assets/producttile').render(model).text;
+};
---
+++ pageDesigner/cartridges/app_custom_page_designer/cartridge/experience/components/einstein/noanchorrecommendation.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var Template = require('dw/util/Template');
+var HashMap = require('dw/util/HashMap');
+var PageRenderHelper = require('*/cartridge/experience/utilities/PageRenderHelper.js');
+var recommendionType = "einstein-noanchor-recommendations";
+
+/**
+ * Render logic for the assets.einsteinproductrecommendation.
+ * 
+ * @param {dw.experience.PageScriptContext} context The page script context object.
+ * @returns {string} The Template text.
+ */
+module.exports.render = function (context) {
+    var model = new HashMap();
+    var content = context.content;
+
+    var recommender = content.recommender;
+    model.limit = parseInt(content.count) || 0;
+    model.carouselTitle = content.carouselTitle;
+    model.isCarouselTitleLeftAligned = content.isCarouselTitleLeftAligned;
+    model.carouselJsonSettings = content.carouselJsonSettings;
+    model.recommendionType = recommendionType;
+
+    if (recommender) {
+        model.recommender = recommender.value;
+    } else {
+        throw new Error('No recommender available');
+    }
+    model.productLoadUrl = dw.web.URLUtils.abs('AsyncComponents-Load');
+    model.id = 'carousel-' + PageRenderHelper.safeCSSClass(context.component.getID());
+    return new Template('experience/components/einstein/recommendationCarousel').render(model).text;
+};
+
