97de4033e0651e6ddbde1a65792d6416adc919fb
Nouman_Ahmad <noman.ahmad@innovadeltech.com>
2020-05-19 18:12:19 +0500

    control bar styling fixed

---
+++ mvmt/cartridges/app_custom_page_designer_mvmt/cartridge/client/default/js/controlBar.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(document).on('click', '.blog-control-bar-btn', function() {
+      $('.blog-control-bar-inner').addClass('active');
+    });
+    
+    $(document).on('click', '.blog-control-bar-inner-btn', function() {
+        $('.blog-control-bar-inner').removeClass('active');
+    });
+});
