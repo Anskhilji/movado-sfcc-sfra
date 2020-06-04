3feebd241e510e1733d3e2b77704dfd1a3a742a9
Hassan Tahir <hassan.tahir@innovadeltech.com>
2020-06-04 17:25:46 +0500

    [MSS-765] ESW disabled currency sign issues are fixed

--- movado/cartridges/app_custom_movado/cartridge/models/productLineItem/decorators/priceTotal.js
+++ movado/cartridges/app_custom_movado/cartridge/models/productLineItem/decorators/priceTotal.js
@@ -8,60 +8,66 @@ var collections = require('*/cartridge/scripts/util/collections');
 var renderTemplateHelper = require('*/cartridge/scripts/renderTemplateHelper');
 var priceHelper = require('*/cartridge/scripts/helpers/priceHelper');
 
-
 /**
  * get the total price for the product line item
  * @param {dw.order.ProductLineItem} lineItem - API ProductLineItem instance
  * @returns {Object} an object containing the product line item total info.
  */
 function getTotalPrice(lineItem) {
+    var eswHelper = require('*/cartridge/scripts/helper/eswSFRAHelper');
     var context;
     var price;
     var result = {};
     var template = 'checkout/productCard/productCardProductRenderedTotalPrice';
     var savingsPrice;
     var eswHelper;
-    var isEswEnabled = !empty(Site.current.getCustomPreferenceValue('eswEshopworldModuleEnabled')) ? Site.current.getCustomPreferenceValue('eswEshopworldModuleEnabled') : false;
+    var orderHistoryFlag = false;
+    var eswShopperCurrencyCode = null;
+    var eswModuleEnabled = eswHelper.getEShopWorldModuleEnabled();
 
     // Custom Start: Adding ESW cartridge integration
-    if (isEswEnabled) {
-        eswHelper = require('*/cartridge/scripts/helper/eswSFRAHelper');
-        if (lineItem.priceAdjustments.getLength() > 0) {
-            var nonAdjustedPrice = eswHelper.getMoneyObject(lineItem.basePrice, false, false).value * lineItem.quantity.value;
-            result.nonAdjustedPrice = new Money(nonAdjustedPrice, lineItem.basePrice.currencyCode);
+    if (lineItem.lineItemCtnr && Object.hasOwnProperty.call(lineItem.lineItemCtnr, 'orderNo')) {
+        if (lineItem.lineItemCtnr.orderNo != null) {
+            orderHistoryFlag = true;
+            eswShopperCurrencyCode = lineItem.lineItemCtnr.originalOrder.custom.eswShopperCurrencyCode;
         }
     }
-    // Custom End
-
-    price = lineItem.adjustedPrice;
-
-    // The platform does not include prices for selected option values in a line item product's
-    // price by default.  So, we must add the option price to get the correct line item total price.
-    collections.forEach(lineItem.optionProductLineItems, function (item) {
-        price = price.add(item.adjustedNetPrice);
-    });
-    // Custom Start: Adding ESW cartridge integration
-    if (isEswEnabled) {
+    if (lineItem.priceAdjustments.getLength() > 0) {
+        var nonAdjustedPrice = (eswModuleEnabled) ? eswHelper.getMoneyObject(lineItem.basePrice, false, false).value * lineItem.quantity.value : lineItem.getPrice();
+        result.nonAdjustedPrice = (eswModuleEnabled) ? new Money(nonAdjustedPrice, lineItem.basePrice.currencyCode) : formatMoney(nonAdjustedPrice);
+    }
+    // If not for order history calculations
+    if (!orderHistoryFlag) {
+        price = lineItem.adjustedPrice;
+        // The platform does not include prices for selected option values in a line item product's
+        // price by default.  So, we must add the option price to get the correct line item total price.
+        collections.forEach(lineItem.optionProductLineItems, function (item) {
+            price = price.add(item.adjustedNetPrice);
+        });
         if (lineItem.quantityValue !== 1) {
-            result.price = formatMoney(eswHelper.getSubtotalObject(lineItem, false));
+            result.price = (eswModuleEnabled) ? formatMoney(eswHelper.getSubtotalObject(lineItem, false)) : formatMoney(price);
         } else {
-            result.price = formatMoney(eswHelper.getUnitPriceCost(lineItem));
+            result.price = (eswModuleEnabled) ? formatMoney(eswHelper.getUnitPriceCost(lineItem)) : formatMoney(price);
         }
-    } else {
-        result.price = formatMoney(price);
+    } else if (orderHistoryFlag) {
+        // If order placed using calculated price model
+        if (eswShopperCurrencyCode != null) {
+            price = new Number((lineItem.custom.eswShopperCurrencyItemPriceInfo * lineItem.quantityValue)); // eslint-disable-line no-new-wrappers
+            result.price = formatMoney(new dw.value.Money(price, eswShopperCurrencyCode));
+        } else {
+            price = lineItem.adjustedPrice;
+            // The platform does not include prices for selected option values in a line item product's
+            // price by default.  So, we must add the option price to get the correct line item total price.
+            collections.forEach(lineItem.optionProductLineItems, function (item) {
+                price = price.add(item.adjustedPrice);
+            });
+            result.price = formatMoney(price);
+        }
     }
-    // Custom End
-
-    savingsPrice = priceHelper.getsavingsPrice(lineItem.getPrice(), price);
-    if (savingsPrice) {
-        result.formattedSavingPrice = formatMoney(savingsPrice);
-        result.savingPrice = savingsPrice;
-    }
-
     context = { lineItem: { priceTotal: result } };
     result.renderedPrice = renderTemplateHelper.getRenderedHtml(context, template);
-
     return result;
+    // Custom End
 }
 
 module.exports = function (object, lineItem) {
