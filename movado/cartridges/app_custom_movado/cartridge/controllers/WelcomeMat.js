eb673f87244d7115e34a4e5cad9cadc7ced7ddc8
Hassan Tahir <hassan.tahir@innovadeltech.com>
2020-06-04 13:58:05 +0500

    [MSS-538] Pr comments are fixed

--- movado/cartridges/app_custom_movado/cartridge/controllers/WelcomeMat.js
+++ movado/cartridges/app_custom_movado/cartridge/controllers/WelcomeMat.js
@@ -3,21 +3,59 @@
 var server = require('server');
 var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
 
-server.get('Show', server.middleware.https, consentTracking.consent, function (
-  req,
-  res,
-  next
-) {
-    var ContentMgr = require('dw/content/ContentMgr');
-    var apiContent = ContentMgr.getContent('welcome-mat');
-    res.render('welcomeMat/welcomeMatModal', {
-        contentBody:
-      apiContent && apiContent.custom.body ? apiContent.custom.body : '',
-        currentCountry: request.geolocation.countryName
-    });
+server.get('Show', server.middleware.https, consentTracking.consent, function (req, res, next) {
+    var eswCustomHelper = require('*/cartridge/scripts/helpers/eswCustomHelper');
+    if (eswCustomHelper.isEshopworldModuleEnabled()) {
+        if ((!eswCustomHelper.isEswEnableLandingPage() && !eswCustomHelper.isEswEnableLandingpageBar()) && 
+            (request.httpCookies['movado.Landing.Played'] == null || request.httpCookies['movado.Landing.Played'] == false)) {
+            var Cookie = require('dw/web/Cookie');
+            var URLUtils = require('dw/web/URLUtils');
+            var allCountries = null;
+            var geoLocationCountry = null;
+            var geoLocationCountryCode = request.geolocation.countryCode;
+            var isGeoLocation = eswCustomHelper.isGeoLocationEnabled();
+            var locale = request.getLocale();
+            var movadoLandingObject = {};
+            var movadoLandingCookie = new Cookie('movado.Landing.Played', true);
+            movadoLandingCookie.setPath('/');
+            response.addHttpCookie(movadoLandingCookie);
 
+            if (isGeoLocation) {
+                geoLocationCountry = eswCustomHelper.getCustomCountryByCountryCode(geoLocationCountryCode);
+            }
+
+            var customCountries = eswCustomHelper.getCustomCountries();
+            allCountries = eswCustomHelper.getAlphabeticallySortedCustomCountries(customCountries, locale);
+            movadoLandingObject.isGeoLocation = false;
+
+            if (!empty(geoLocationCountry)) {
+                movadoLandingObject.isGeoLocation = true;
+                movadoLandingObject.selectedCountry = geoLocationCountry.countryCode;
+                movadoLandingObject.selectedCountryName = geoLocationCountry.displayName;
+                movadoLandingObject.selectedCurrency = geoLocationCountry.currencyCode;
+            } else {
+                var firstCountry = allCountries.get(0);
+                movadoLandingObject.selectedCountry = firstCountry.value;
+                movadoLandingObject.selectedCountryName = firstCountry.displayValue;
+                movadoLandingObject.selectedCurrency = '';
+            }
+
+            movadoLandingObject.setLocale = URLUtils.https('Page-SetLocale').toString();
+            movadoLandingObject.allCountries = allCountries;
+            res.render('welcomeMat/newWelcomeMatModal', {movadoLandingObject : movadoLandingObject});
+        }
+    } else {
+        var ContentMgr = require('dw/content/ContentMgr');
+        var apiContent = ContentMgr.getContent('welcome-mat');
+        res.render('welcomeMat/welcomeMatModal', {
+            contentBody:
+          apiContent && apiContent.custom.body ? apiContent.custom.body : '',
+            currentCountry: request.geolocation.countryName
+        });
+    }
     next();
 });
+
 server.get(
   'SetWelcomeMatHide',
   server.middleware.https,
