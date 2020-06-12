8f02d3dea1cd3bd59910bf7b8b59e00613957f6e
Muhammad Aurangzaib <muhammad.aurangzaib@innovadeltech.com>
2020-06-12 19:54:08 +0500

    controllers are created for legacy logic also js click functions are made

--- mvmt/cartridges/app_custom_core_mvmt/cartridge/controllers/Account.js
+++ mvmt/cartridges/app_custom_core_mvmt/cartridge/controllers/Account.js
@@ -11,4 +11,47 @@ server.get(
         next();
     }
 );
+
+server.post(
+    'LegacyCustomer',
+    function (req, res, next) {
+        var CustomerMgr = require('dw/customer/CustomerMgr');
+        var URLUtils = require('dw/web/URLUtils');
+
+        var email = req.form.loginEmail;
+        var customer = CustomerMgr.getCustomerByLogin(email);
+        res.setViewData({ legacyCustomer: 'fasle' });
+        session.custom.emailResetProfile = 'muhammad.aurangzaib@innovadeltech.com';
+        res.json({
+            success: true,
+            customer: customer,
+            legacyCustomer: false,
+            relativeURL: URLUtils.url('Page-Show','cid', 'legacy-customer-reset-password').toString()
+        });
+
+        return next();
+    }
+);
+
+
+server.post('PasswordResetEmail', server.middleware.https, function (req, res, next) {
+    var CustomerMgr = require('dw/customer/CustomerMgr');
+    var Resource = require('dw/web/Resource');
+    var URLUtils = require('dw/web/URLUtils');
+    var accountHelpers = require('*/cartridge/scripts/helpers/accountHelpers');
+
+    var email;
+    var resettingCustomer;
+    if (session.custom.emailResetProfile) {
+        email = session.custom.emailResetProfile;
+        resettingCustomer = CustomerMgr.getCustomerByLogin(email);
+            if (resettingCustomer) {
+                accountHelpers.sendPasswordResetEmail(email, resettingCustomer);
+            }
+            session.custom.emailResetProfile = null;
+    }  
+    
+    next();
+});
+
 module.exports = server.exports();
--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/login/login.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/login/login.js
@@ -17,4 +17,84 @@ module.exports = function () {
         $('#forget-password-section').removeClass('d-block').addClass('d-none');
         $('#login-section').addClass('d-block').removeClass('d-none');
     });
+
+    $('legacy-reset-password').on('click', function (e) {
+        var urlLeg = $(this).data('action-url');
+        $.ajax({
+            url: urlLeg,
+            type: 'post',
+            dataType: 'json',
+            success: function (data) {
+                // if (!data.success) {
+                //     formValidation(form, data);
+                //     $('form.login').trigger('login:error', data);
+                // } else {
+                //     $('form.login').trigger('login:success', data);
+                //     location.href = data.redirectUrl;
+                // }
+                var test = '';
+            },
+            error: function (data) {
+                // if (data.responseJSON.redirectUrl) {
+                //     window.location.href = data.responseJSON.redirectUrl;
+                // } else {
+                //     $('form.login').trigger('login:error', data);
+                //     form.spinner().stop();
+                // }
+            }
+        });
+    }
+
+    $('form.login').off('submit').on('submit', function (e) {
+        var form = $(this);
+        e.preventDefault();
+        var urlLegacy = form.data('legacy-url');
+        $.ajax({
+            url: urlLegacy,
+            type: 'post',
+            dataType: 'json',
+            data: form.serialize(),
+            success: function (data) {
+                var test = data;
+                if (data.success && data.legacyCustomer) {
+                    var url = form.attr('action');
+                    form.spinner().start();
+                    $('form.login').trigger('login:submit', e);
+                    $.ajax({
+                        url: url,
+                        type: 'post',
+                        dataType: 'json',
+                        data: form.serialize(),
+                        success: function (data) {
+                            form.spinner().stop();
+                            if (!data.success) {
+                                formValidation(form, data);
+                                $('form.login').trigger('login:error', data);
+                            } else {
+                                $('form.login').trigger('login:success', data);
+                                location.href = data.redirectUrl;
+                            }
+                        },
+                        error: function (data) {
+                            if (data.responseJSON.redirectUrl) {
+                                window.location.href = data.responseJSON.redirectUrl;
+                            } else {
+                                $('form.login').trigger('login:error', data);
+                                form.spinner().stop();
+                            }
+                        }
+                    });
+
+                    return false;
+                }
+                 else {
+                    window.location.href = data.relativeURL;
+                }
+            }   
+        });
+
+        
+    });
 };
+
+
