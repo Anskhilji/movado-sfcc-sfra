3aac6e34f9706a3f3d711c99c41f0248968159d5
Muhammad Aurangzaib <muhammad.aurangzaib@innovadeltech.com>
2020-06-16 18:13:36 +0500

    implement the logic to show/hide the Swell Loyalty for specific countries

---
+++ mvmt/cartridges/app_custom_core_mvmt/cartridge/scripts/helpers/utilCustomHelpers.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var Site = require('dw/system/Site');
+
+var Constants = require('~/cartridge/scripts/util/Constants');
+
+function isSwellLoyaltyAllowedCountry() {
+    
+    var isEswEnabled = !empty(Site.current.preferences.custom.eswEshopworldModuleEnabled) ? Site.current.preferences.custom.eswEshopworldModuleEnabled : false;
+    var allCountries = Constants.ALL_COUNTRIES;
+    if (isEswEnabled) {
+        var countryCode = !empty(request.httpCookies['esw.location'].value) ? request.httpCookies['esw.location'].value : '';
+        var deliveryAllowedCountryCodes = Site.getCurrent().preferences.custom.swellLoyaltyAllowedCountries;
+        for (var i = 0; i < deliveryAllowedCountryCodes.length; i++) {
+            if ((countryCode && countryCode.equalsIgnoreCase(deliveryAllowedCountryCodes[i])) || allCountries.equalsIgnoreCase(deliveryAllowedCountryCodes[i])) {
+                return true;
+            }
+        }
+        return false;
+    } 
+    return true;
+    
+}
+
+module.exports = {
+    isSwellLoyaltyAllowedCountry: isSwellLoyaltyAllowedCountry
+};
