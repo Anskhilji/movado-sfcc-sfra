244f076a552fab1b80f5cf9d7781c72fc731482a
Bilal Javed <bilal.javed@innovadeltech.com>
2020-06-18 12:45:43 +0500

    MSS-654 Add states.xml in mvmt and change the input field.

--- mvmt/cartridges/app_custom_core_mvmt/cartridge/controllers/Address.js
+++ mvmt/cartridges/app_custom_core_mvmt/cartridge/controllers/Address.js
@@ -8,7 +8,22 @@ var Site = require('dw/system/Site');
 server.append(
     'AddAddress',
     function (req, res, next) {
+        // Custom Start: Get all esw supported countries
         var countries = Site.current.getCustomPreferenceValue('eswAllCountries');
+        // Custom End
+        res.setViewData({
+            countries : countries
+        });
+        next();
+    }
+);
+
+server.append(
+    'EditAddress',
+    function (req, res, next) {
+        // Custom Start: Get all esw supported countries
+        var countries = Site.current.getCustomPreferenceValue('eswAllCountries');
+        // Custom End
         res.setViewData({
             countries : countries
         });
