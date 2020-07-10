333c0f8801ca65a25da71efd244d3b1217b4e611
muhammad-abrar <muhammad.abrar@innovadeltech.com>
2020-07-10 20:08:47 +0500

    Filter click fixed

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/search.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/search.js
@@ -5,6 +5,7 @@ var processInclude = require('base/util');
 $(document).ready(function () {
     processInclude(require('./search/jquery-custom-select'));
     processInclude(require('movado/search'));
-    processInclude(require('./search/search'));
     processInclude(require('./product/base'));
+    processInclude(require('./search/search'));
+
 });
