5fb1738ed83986d7fdd8b0487433b92faf045136
Bilal Javed <bilal.javed@innovadeltech.com>
2020-08-18 20:57:38 +0500

    category name bug fix.

--- movado/cartridges/app_custom_movado/cartridge/scripts/helpers/productCustomHelpers.js
+++ movado/cartridges/app_custom_movado/cartridge/scripts/helpers/productCustomHelpers.js
@@ -869,6 +869,7 @@ function getMarketingProducts(apiProduct, quantity) {
     var priceFactory = require('*/cartridge/scripts/factories/price');
     var productFactory = require('*/cartridge/scripts/factories/product');
     var productHelper = require('*/cartridge/scripts/helpers/productHelpers');
+    var stringUtils = require('*/cartridge/scripts/helpers/stringUtils');
 
     try {
         var defaultVariant = apiProduct.variationModel.defaultVariant;
@@ -903,13 +904,13 @@ function getMarketingProducts(apiProduct, quantity) {
 
         if (apiProduct.getOnlineCategories().length > 0) {
             apiCategories = apiProduct.getOnlineCategories();
-            productCategory = apiCategories[apiCategories.length-1].displayName;
+            productCategory = stringUtils.removeSingleQuotes(apiCategories[apiCategories.length-1].displayName);
         }
 
         if (empty(productCategory) && apiProduct.variant &&
             apiProduct.variationModel.master.getOnlineCategories().length > 0) {
                 apiCategories = apiProduct.variationModel.master.getOnlineCategories();
-                productCategory = apiCategories[apiCategories.length-1].displayName;
+                productCategory = stringUtils.removeSingleQuotes(apiCategories[apiCategories.length-1].displayName);
         }
 
         marketingProductData = {
