f5af36c20a7e53aba4d6ec4b705773a51331f3d9
Waseem Akram <waseem.akram@innovadeltech.com>
2020-08-27 17:19:54 +0500

    [MSS-891] Fixed pdp varation selection issue

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/base.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/base.js
@@ -781,6 +781,9 @@ movadoBase.selectAttribute = function () {
     $(document).off('change', selector);
     $(document).off('click', selector).on('click', selector, function (e) {
         e.preventDefault();
+        if ($(this).attr('disabled') || $(this).hasClass('active')) {
+            return;
+        }
 
         var value = $(e.currentTarget).is('input[type="radio"]') ? $(e.currentTarget).data('value-url') : e.currentTarget.value;
 
@@ -796,7 +799,7 @@ movadoBase.colorAttribute = function () {
     $(document).off('click', '[data-attr="color"] a').on('click','[data-attr="color"] a', function (e) {
         e.preventDefault();
     
-        if ($(this).attr('disabled')) {
+        if ($(this).attr('disabled') || $(this).hasClass('active')) {
             return;
         }
     
