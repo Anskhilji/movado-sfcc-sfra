2b2fce0da6f178b028af83c179a9451ae90fcce0
Nouman_Ahmad <noman.ahmad@innovadeltech.com>
2020-09-11 18:10:48 +0500

    [MSS-978] Add defnive check for country specific checkout

--- mvmt/cartridges/app_custom_core_mvmt/cartridge/controllers/Checkout.js
+++ mvmt/cartridges/app_custom_core_mvmt/cartridge/controllers/Checkout.js
@@ -5,10 +5,11 @@ var server = require('server');
 var page = module.superModule;
 server.extend(page);
 
+var URLUtils = require('dw/web/URLUtils');
+
 server.append(
     'Login',
     function (req, res, next) {
-        var URLUtils = require('dw/web/URLUtils');
         var actionUrl = URLUtils.url('Account-Login', 'rurl', 2, 'pageType', 'checkout');
         res.setViewData({
             actionUrl: actionUrl
@@ -16,5 +17,44 @@ server.append(
         return next();
     }
 );
+
+/**
+ * Prepending to control Country based checkout
+ */
+server.prepend(
+    'Login',
+    function (req, res, next) {
+        var eswCustomHelper = require('*/cartridge/scripts/helpers/eswCustomHelper');
+        if (eswCustomHelper && eswCustomHelper.isEshopworldModuleEnabled()) {
+            var customCartHelpers = require('*/cartridge/scripts/helpers/customCartHelpers');
+            var countrySwitch = customCartHelpers.getCountrySwitch();
+            if (countrySwitch && !empty(countrySwitch)) {
+                res.redirect(URLUtils.https('Cart-Show').toString());
+                return next();
+            }
+        }
+        return next();
+    }
+);
+
+/**
+ * Prepending to control Country based checkout
+ */
+server.prepend(
+    'Begin',
+    function (req, res, next) {
+        var eswCustomHelper = require('*/cartridge/scripts/helpers/eswCustomHelper');
+        if (eswCustomHelper && eswCustomHelper.isEshopworldModuleEnabled()) {
+            var customCartHelpers = require('*/cartridge/scripts/helpers/customCartHelpers');
+            var countrySwitch = customCartHelpers.getCountrySwitch();
+            if (countrySwitch && !empty(countrySwitch)) {
+                res.redirect(URLUtils.https('Cart-Show').toString());
+                return next();
+            }
+        }
+        return next();
+    }
+);
+
 
 module.exports = server.exports();
