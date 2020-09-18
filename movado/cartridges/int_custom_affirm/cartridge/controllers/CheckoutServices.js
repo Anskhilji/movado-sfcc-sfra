668623a7c58a71ec04c5637c0bd4ba682e7cb770
Reagan Boone <reagan.boone@capgemini.com>
2020-09-18 08:37:21 -0500

    Add Site declaration to function + use site declarartion instead of js ref

--- movado/cartridges/int_custom_affirm/cartridge/controllers/CheckoutServices.js
+++ movado/cartridges/int_custom_affirm/cartridge/controllers/CheckoutServices.js
@@ -86,6 +86,7 @@ server.replace('PlaceOrder', server.middleware.https, function (req, res, next) 
 	  var COCustomHelpers = require('*/cartridge/scripts/checkout/checkoutCustomHelpers');
 	  var hooksHelper = require('*/cartridge/scripts/helpers/hooks');
 	  var orderCustomHelpers = require('*/cartridge/scripts/helpers/orderCustomHelper');
+	  var Site = require('dw/system/Site');
 
 	  var currentBasket = BasketMgr.getCurrentBasket();
 	  checkoutLogger.debug('(CheckoutServices) -> PlaceOrder: Inside PlaceOrder to validate the payment and order');
@@ -267,7 +268,7 @@ server.replace('PlaceOrder', server.middleware.https, function (req, res, next) 
 	  }
 
       // Salesforce Order Management attributes.  Note: The Order Ingestion process that pushes orders from SFCC to SOM doesn't support all objects for custom attributes.  Uses order ingestion functionality as of April 2020.  As it's imporoved, you may want to eliminate some of this:
-	  if ('SOMIntegrationEnabled' in require('dw/system/Site').getCurrent().preferences.custom && require('dw/system/Site').getCurrent().preferences.custom.SOMIntegrationEnabled) {
+	  if ('SOMIntegrationEnabled' in Site.preferences.custom && Site.getCurrent().preferences.custom.SOMIntegrationEnabled) {
         var populateOrderJSON = require('*/cartridge/scripts/jobs/populateOrderJSON');
         var somLog = require('dw/system/Logger').getLogger('SOM', 'CheckoutServices');
         somLog.debug('Processing Order ' + order.orderNo);
