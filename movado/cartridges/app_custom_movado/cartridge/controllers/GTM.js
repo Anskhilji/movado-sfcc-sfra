1bb752cd33ba8391d1229cd2481424727ee7d9d2
Muhammad Aurangzaib <muhammad.aurangzaib@innovadeltech.com>
2020-09-23 21:28:31 +0500

    add rakuten tags for mcs and movado

---
+++ movado/cartridges/app_custom_movado/cartridge/controllers/GTM.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var server = require('server');
+server.extend(module.superModule);
+
+function getOrderIDfromQueryString(queryString) {
+    var orderTokenArray = queryString.split('&');
+    var orderToken = orderTokenArray[0].split('=');
+    return orderToken[1];
+}
+
+server.append(
+    'LoadDataLayer',
+    function (req, res, next) {
+        var OrderMgr = require('dw/order/OrderMgr');
+
+        var action = req.querystring.urlAction.toLowerCase();
+        var currentCustomer = req.currentCustomer;
+        var customerStatus = currentCustomer.raw.anonymous ? 'New' : 'Existing';
+        var customerID = !empty(currentCustomer) && !empty(currentCustomer.profile) ? currentCustomer.profile.customerNo : '';
+        var discountCode = '';
+        var reqQueryString = req.querystring;
+        var queryString;
+        if (!empty(reqQueryString)) {
+            queryString = req.querystring.urlQueryString;
+
+            if (action.equals('order-confirm')) {
+                var orderId = getOrderIDfromQueryString(queryString);
+                if (orderId) {
+                    var order = OrderMgr.getOrder(orderId);
+                    var couponLineItemsItr = order.getCouponLineItems().iterator();
+                    while (couponLineItemsItr.hasNext()) {
+                        var couponLineItem = couponLineItemsItr.next();
+                        var couponCode = couponLineItem.getCouponCode();
+                        if (couponCode && !empty(discountCode)) {
+                            discountCode = discountCode + ', ' + couponCode;
+                        } else if (couponCode) {
+                            discountCode = couponCode;
+                        }
+                    }
+                }
+            }
+        }
+        var test = '';
+
+        res.setViewData({
+            customerStatus: customerStatus,
+            customerID: customerID,
+            discountCode: discountCode
+		});
+        
+        next();
+    }
+);
+
+module.exports = server.exports();
+
