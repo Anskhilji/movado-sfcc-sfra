8bd567ff3eb9d7bd5593eee24de42e56f6dea75b
waseem akram <waseem.akram@innovadeltech.com>
2020-10-28 17:50:08 +0500

    [MSS-1067] Rakuten Pixel - Cookie Consent blocking

--- movado/cartridges/app_custom_movado/cartridge/scripts/helpers/utils/Constants.js
+++ movado/cartridges/app_custom_movado/cartridge/scripts/helpers/utils/Constants.js
@@ -3,4 +3,4 @@
 exports.YEAR_MONTH_DATE_PATTERN = 'yyyy-MM-dd';
 exports.PATTERN_TO_CONVERT_TIME_TO_12_HOURS = 'h:mm a';
 exports.ALL_COUNTRIES = 'ALL';
-exports.OPTANON-CATEGORY-C0004 = 'C0004';
+exports.ONETRUST_TARGETING_COOKIES_CLASS = 'optanon-category-C0004';
