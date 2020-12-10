e8f0f5f45f28e3c8b9832c59bd3b664c4097ad01
Muhammad Aurangzaib <muhammad.aurangzaib@innovadeltech.com>
2020-12-10 14:38:41 +0500

    added try catch block added ticket num into comment for future tracking

--- mvmt/cartridges/app_custom_core_mvmt/cartridge/controllers/Order.js
+++ mvmt/cartridges/app_custom_core_mvmt/cartridge/controllers/Order.js
@@ -6,17 +6,23 @@ server.extend(module.superModule);
 server.append('Confirm', function (req, res, next) {
     var OrderMgr = require('dw/order/OrderMgr');
     var sfmcApi = require('*/cartridge/scripts/api/SFMCApi');
+    var Logger = require('dw/system/Logger').getLogger('MarketingCloud');
 
-    var viewData = res.getViewData();
-    var orderNo = !empty(viewData.order) && !empty(viewData.order.orderNumber) ? viewData.order.orderNumber : session.custom.orderNumber;
-    var order = OrderMgr.getOrder(orderNo);
-    var requestParams = {
-        email : order.getCustomerEmail() ? order.getCustomerEmail() : ''
-    }
+    try {
+        var viewData = res.getViewData();
+        var orderNo = !empty(viewData.order) && !empty(viewData.order.orderNumber) ? viewData.order.orderNumber : session.custom.orderNumber;
+        var order = OrderMgr.getOrder(orderNo);
+        var requestParams = {
+            email : order.getCustomerEmail() ? order.getCustomerEmail() : ''
+        }
 
-    if (!empty(requestParams.email)) {
-        sfmcApi.sendSubscriberToSFMC(requestParams);
+        if (!empty(requestParams.email)) {
+            sfmcApi.sendSubscriberToSFMC(requestParams);
+        }
+    } catch (e) {
+        Logger.error('(Order-Confirm) -> Exception occurred while try to send email to SFMC: {0}', e.toString());
     }
+    
     next();
 });
 
