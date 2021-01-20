c57ff5e4f5015091758437ccf6c6b8fb0efb1c9a
Bilal Javed <bilal.javed@innovadeltech.com>
2021-01-20 11:31:22 +0000

    Merged in feature/MSS-1175-mvmt--0-orders (pull request #1285)
    Feature/MSS-1175 mvmt  0 orders
    
    * MSS-1175 Create new cartirdge basic structure.
    
    * MSS-1175 Implement logic to send $0 order to OMS.
    
    Approved-by: Haroon Chaudhry

---
+++ bm_customer_service_orders/cartridge/scripts/helpers/customerServicesOrder.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * This hooks is called when we place an order via Customer Service Center
+ *
+ * @param {Order} order Order
+ * 
+ */
+
+function afterPOST(order) {
+    var Site = require('dw/system/Site');
+    var somIntegrationEnabled = !empty(Site.current.preferences.custom.SOMIntegrationEnabled) ? Site.current.preferences.custom.SOMIntegrationEnabled : false;
+    if (somIntegrationEnabled) {
+        var populateOrderJSON = require('*/cartridge/scripts/jobs/populateOrderJSON');
+        var somLog = require('dw/system/Logger').getLogger('SOM', 'CheckoutServices');
+        somLog.debug('Processing Order ' + order.orderNo);
+        try {
+            var Transaction = require('dw/system/Transaction');
+            
+            Transaction.wrap(function () {
+                populateOrderJSON.populateByOrder(order);
+            });
+        } catch (exSOM) {
+            somLog.error('SOM attribute process failed: ' + exSOM.message + ',exSOM: ' + JSON.stringify(exSOM));
+        }
+    }
+}
+
+module.exports = {
+    afterPOST: afterPOST
+}
