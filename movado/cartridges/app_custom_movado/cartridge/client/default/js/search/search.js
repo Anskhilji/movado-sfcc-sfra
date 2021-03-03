369d95e8fca3d29e9898918b6bc64eaf5ee954fe
Muhammad Yasir <muhammad.yasir@innovadeltech.com>
2021-03-03 15:38:45 +0500

    [MSS-545] Track all clickable elements throughout site and send to Google Add Click Event on Filter Panel Fix comments

--- movado/cartridges/app_custom_movado/cartridge/client/default/js/search/search.js
+++ movado/cartridges/app_custom_movado/cartridge/client/default/js/search/search.js
@@ -300,7 +300,7 @@ module.exports = {
             // Push Data into gtm For Sorting Rules Filters
             var $filteredText = $(this).find(':selected').text().trim();
             var $filterCategory = $(this).find(':selected').data('filter-category');
-            var $OpenFilter = "Open";
+            var $OpenFilter = 'Open';
             var $OpenFilterCategory = $filterCategory + " " + $OpenFilter;
             if ($filteredText !==undefined) {
                 dataLayer.push({
