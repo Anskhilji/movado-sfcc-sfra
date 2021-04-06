d729268c4960ba0522c09ce571094ea71fd49ae2
muhammad-abrar <muhammad.abrar@innovadeltech.com>
2021-04-06 21:51:51 +0500

    [MSS-1275] MVMT header redesign, hamburger menu, AB test, Right align design

--- mvmt/cartridges/app_custom_core_mvmt/cartridge/controllers/Page.js
+++ mvmt/cartridges/app_custom_core_mvmt/cartridge/controllers/Page.js
@@ -2,6 +2,7 @@
 
 var server = require('server');
 var page = module.superModule;
+var cache = require('*/cartridge/scripts/middleware/cache');
 server.extend(page);
 
 var URLUtils = require('dw/web/URLUtils');
@@ -10,6 +11,26 @@ server.append(
     'IncludeHeaderMenu',
     function (req, res, next) {
         res.setViewData({ loggedIn: req.currentCustomer.raw.authenticated });
+        next();
+    }
+);
+
+// AB test logic implemented for mobile header
+server.get(
+    'IncludeHeader',
+    server.middleware.include,
+    cache.applyPromotionSensitiveCache,
+    function (req, res, next) {
+        var ABTestMgr = require('dw/campaign/ABTestMgr');
+
+        var headerTemplate = null;
+        // A/B testing for header design
+        if (!ABTestMgr.isParticipant('MVMT-header-redesign','MobileHeader-right')) {
+            headerTemplate = '/components/header/old/pageHeader';
+        } else {
+            headerTemplate = '/components/header/pageHeader';
+        }
+        res.render(headerTemplate);
         next();
     }
 );
