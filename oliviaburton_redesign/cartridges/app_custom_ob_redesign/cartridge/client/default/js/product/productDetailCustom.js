99fb0cd4cbd2bc783881e51e8d27a95cd93f1b6b
Muhammad Yasir <muhammad.yasir@innovadeltech.com>
2021-05-12 20:37:35 +0500

    MSS-1327-OB-Redesign-personalization-backend-working

--- oliviaburton_redesign/cartridges/app_custom_ob_redesign/cartridge/client/default/js/product/productDetailCustom.js
+++ oliviaburton_redesign/cartridges/app_custom_ob_redesign/cartridge/client/default/js/product/productDetailCustom.js
@@ -46,30 +46,60 @@ module.exports = {
             e.preventDefault();
         });  
 
-        $('.product-detail-redesign .debossing-cancel, .product-detail-redesign .popup-close-btn').on('click', function(e) {
-            if ($('.pdp-v-one .debossing-text').text() === '') {
-                e.stopPropagation();
-                $('.pdp-v-one .debossing-input').val('');
-                $(".prices-add-to-cart-actions").removeClass('extra-z-index');
-                $('body, html').removeClass('no-overflow');
-                $('body').removeClass('no-scroll');
-                $('.popup-opened').hide();
-                e.preventDefault();
-                return;
-            } else {
-                $('.debossing-cancel').removeClass('submitted');
-                $('.debossing-cancel').removeAttr('form');
-                $('.debossing-cancel').removeAttr('type');
-            }
+        // $('.product-detail-redesign .debossing-cancel, .product-detail-redesign .popup-close-btn').on('click', function(e) {
+        //     if ($('.pdp-v-one .debossing-text').text() === '') {
+        //         e.stopPropagation();
+        //         $('.pdp-v-one .debossing-input').val('');
+        //         $(".prices-add-to-cart-actions").removeClass('extra-z-index');
+        //         $('body, html').removeClass('no-overflow');
+        //         $('body').removeClass('no-scroll');
+        //         $('.popup-opened').hide();
+        //         e.preventDefault();
+        //         return;
+        //     } else {
+        //         $('.debossing-cancel').removeClass('submitted');
+        //         $('.debossing-cancel').removeAttr('form');
+        //         $('.debossing-cancel').removeAttr('type');
+        //     }
     
-            $('.pdp-v-one .debossing-text').text('');
-            $('.pdp-v-one .debossing-form .text-on-watch span').text('');
-            $('.pdp-v-one .debossing-input').val('');
-            var targeted_popup_class = jQuery(this).attr('pd-popup-close');
-            $('[pd-popup="' + targeted_popup_class + '"]').fadeOut(200).removeClass('popup-opened');
-            $('body, html').removeClass('no-overflow');
-            $('body').removeClass('no-scroll');
-            $('.popup-opened').hide();
-        });
+        //     $('.pdp-v-one .debossing-text').text('');
+        //     $('.pdp-v-one .debossing-form .text-on-watch span').text('');
+        //     $('.pdp-v-one .debossing-input').val('');
+        //     var targeted_popup_class = jQuery(this).attr('pd-popup-close');
+        //     $('[pd-popup="' + targeted_popup_class + '"]').fadeOut(200).removeClass('popup-opened');
+        //     $('body, html').removeClass('no-overflow');
+        //     $('body').removeClass('no-scroll');
+        //     $('.popup-opened').hide();
+        // });
     }
 }
+$(function() {
+$('.pdp-v-one .debossing-form .popup-action-btns .save').on('click', function() {
+    $('.debossing-btn > edit-popup').addClass('show-deboss-text');
+    var a = $.trim($('.pdp-v-one .debossing-input.valid').val());
+    setTimeout(function() {
+        var debossingtext=$.trim($('.pdp-v-one .debossing-form .text-area .debossing-input.valid').val());
+        console.log(debossingtext);
+        if(debossingtext == undefined || debossingtext == "") {
+            $('.pdp-v-one .debossing-text').text("");
+        } else {
+            $('.pdp-v-one .debossing-text').text(debossingtext);
+            $('.pdp-v-one .debossing-form .text-on-watch span').text(debossingtext);
+            $('.debossing-cancel').addClass('submitted');
+            $('.debossing-cancel').attr('form', 'embossingForm');
+            $('.debossing-cancel').attr('type', 'submit');
+        }
+    }, 100);
+});
+
+$('.pdp-v-one .popup-close-btn').on('click', function(e) {
+    e.stopPropagation();
+    var targeted_popup_class = jQuery(this).attr('pd-popup-close');
+    $('[pd-popup="' + targeted_popup_class + '"]').fadeOut(200).removeClass('popup-opened');
+    $(".prices-add-to-cart-actions").removeClass('extra-z-index');
+    $('body, html').removeClass('no-overflow');
+    $('.popup-opened').hide();
+    e.preventDefault();
+});
+
+});
