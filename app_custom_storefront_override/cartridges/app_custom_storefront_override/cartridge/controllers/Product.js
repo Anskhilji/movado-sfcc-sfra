4aa4122fa6e90001621cf2fc588f08623026d01a
DESKTOP-0AG35KP\Geo Information <muhammad.abrar@hotmail.com>
2021-05-25 18:34:51 +0500

    MSS-1395-ob-redesign-PDP-abtest, Implemented

---
+++ app_custom_storefront_override/cartridges/app_custom_storefront_override/cartridge/controllers/Product.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var server = require('server');
+server.extend(module.superModule);
+
+var cache = require('*/cartridge/scripts/middleware/cache');
+var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
+var pageMetaData = require('*/cartridge/scripts/middleware/pageMetaData');
+
+/**
+ * @typedef ProductDetailPageResourceMap
+ * @type Object
+ * @property {String} global_availability - Localized string for "Availability"
+ * @property {String} label_instock - Localized string for "In Stock"
+ * @property {String} global_availability - Localized string for "This item is currently not
+ *     available"
+ * @property {String} info_selectforstock - Localized string for "Select Styles for Availability"
+ */
+
+server.replace('Show', cache.applyPromotionSensitiveCache, consentTracking.consent, function (req, res, next) {
+    var ABTestMgr = require('dw/campaign/ABTestMgr');
+    var productHelper = require('*/cartridge/scripts/helpers/productHelpers');
+    var showProductPageHelperResult = productHelper.showProductPage(req.querystring, req.pageMetaData);
+    var productType = showProductPageHelperResult.product.productType;
+    if (!ABTestMgr.isParticipant('OBRedesignPDPABTest','render-new-design')) {
+        showProductPageHelperResult.template =  'product/old/productDetails';
+    } else {
+        showProductPageHelperResult.template = 'product/productDetails';
+    }
+    if (!showProductPageHelperResult.product.online && productType !== 'set' && productType !== 'bundle') {
+        res.setStatusCode(404);
+        res.render('error/notFound');
+    } else {
+        res.render(showProductPageHelperResult.template, {
+            product: showProductPageHelperResult.product,
+            addToCartUrl: showProductPageHelperResult.addToCartUrl,
+            resources: showProductPageHelperResult.resources,
+            breadcrumbs: showProductPageHelperResult.breadcrumbs
+        });
+    }
+    next();
+}, pageMetaData.computedPageMetaData);
+
+module.exports = server.exports();
