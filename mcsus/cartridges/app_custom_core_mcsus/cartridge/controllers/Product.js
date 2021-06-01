ae1eb96908dfe296a3d3563c93a7946bbb3af4e2
Muhammad_Usman556 <usman.irshad@innovadeltech.com>
2021-06-01 20:39:29 +0500

    [MSS-1411] PDP pickupSoreSelect

--- mcsus/cartridges/app_custom_core_mcsus/cartridge/controllers/Product.js
+++ mcsus/cartridges/app_custom_core_mcsus/cartridge/controllers/Product.js
@@ -1,10 +1,14 @@
 'use strict';
 
 var server = require('server');
+var URLUtils = require('dw/web/URLUtils');
 var Site = require('dw/system/Site');
+var Template = require('dw/util/Template');
+var HashMap = require('dw/util/HashMap');
 var page = module.superModule;
 server.extend(page);
 var productHelper = require('*/cartridge/scripts/helpers/productHelpers');
+var storeHelpers = require('*/cartridge/scripts/helpers/customStoreHelper');
 
 server.get('ShowMcsProdutPrice', function (req, res, next) { 
     var showProductPageHelperResult = productHelper.showProductPage(req.querystring, req.pageMetaData);
@@ -23,6 +27,30 @@ server.get('ShowMcsAffirmText', function (req, res, next) {
         product: showProductPageHelperResult.product,
         restrictAnonymousUsersOnSalesSites: Site.getCurrent().preferences.custom.restrictAnonymousUsersOnSalesSites
     }); 
+    next();
+});
+
+server.get('getStoresList', function (req, res, next) {
+    var radius = req.querystring.radius;
+    var zipCode = req.querystring.zipCode;
+    var geolocation = req.geolocation;
+    // geolocation = {
+    //     countryCode: "CA",
+    //     latitude: 49.206217, longitude: -122.985902
+    // }
+
+    var stores = storeHelpers.getStores(radius, null, null, geolocation, zipCode, false);
+    var path = '/modalpopup/pickupStoreList.isml';
+    var tmplate = new Template(path);
+    var data = new HashMap();
+
+    data.put('stores', stores);
+    var html = tmplate.render(data);
+    var result = {
+        html: html.text
+    }
+
+    res.json(result);
     next();
 });
 
--- omnichannel/cartridges/int_omnichannel/cartridge/client/default/js/pickupStoreSelection.js
+++ omnichannel/cartridges/int_omnichannel/cartridge/client/default/js/pickupStoreSelection.js
@@ -1,3 +1,34 @@
-$(document).ready(function(){
-  
+
+$(document).ready(function () {
+    var $searchStore = $("#search-store");
+    $searchStore.click(function () {
+
+        var $zipCode = $("#zip-code");
+        var $radius = $("#radius");
+        url = $searchStore.data('url');
+        data = {
+            zipCode: $zipCode.val(),
+            radius: $radius.val()
+        }
+        $.spinner().start();
+        $.ajax({
+            url: url,
+            type: 'GET',
+            data: data,
+            success: function (resData) {
+                $("#store-list").html(resData.html);
+                $.spinner().stop();
+            }, error: function (error) {
+                $("#store-list").html("<div class='no-store'>We're sorry, we couldn't find results for your search.</div>");
+                $.spinner().stop();
+            }
+
+        })
+    })
+})
+
+$(document).on('click', '.store-pickup-select', function () {
+    var stringifyData = JSON.stringify($(this).data('store'));
+    localStorage.setItem("currentStore", stringifyData);
+    console.log(JSON.parse(localStorage.getItem("currentStore")));
 })
