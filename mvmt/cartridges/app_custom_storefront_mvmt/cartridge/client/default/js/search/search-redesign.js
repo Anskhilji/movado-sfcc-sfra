d4d32230de34b3f964911b7e3f4180dfbcd2f7ac
DESKTOP-0AG35KP\Geo Information <muhammad.abrar@hotmail.com>
2021-06-02 21:00:26 +0500

    MSS-1363-MVMT-PLP-redesign-lifestyle-image-code-update

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/search/search-redesign.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/search/search-redesign.js
@@ -1,9 +1,10 @@
 module.exports = {
     plpTileCarousel: function () {
         $('.plp-image-carousel').slick({
+            lazyLoad: 'ondemand',
             slidesToShow: 1,
             slidesToScroll: 1,
-            infinite: true,
+            infinite: false,
             dots: true,
             arrows: false,
         });
