3ba2acf4897b91a4418f1407eb04909b70d52ebf
Muhammad Yasir <muhammad.yasir@innovadeltech.com>
2021-06-14 15:37:26 +0500

    MSS-1417 Design to Select / Update Store on PDP

--- mcsus/cartridges/app_custom_storefront_mcsus/cartridge/client/default/js/pickupStoreModal.js
+++ mcsus/cartridges/app_custom_storefront_mcsus/cartridge/client/default/js/pickupStoreModal.js
@@ -15,7 +15,7 @@ $(document).on('click', '.storeModal', function () {
             $("#pickupStoreModal").modal('show');
             $("#radius").val(resData.radius || 15);
             $("#zip-code").val(resData.zipCode || '');
-            $("#store-list").html("<div class='no-store'>We're sorry, we couldn't find results for your search.</div>");
+            $("#store-list").html("<div class='no-store'>"+Resources.BOPIS_STORE_FETCHING_ERROR+"</div>");
             $.spinner().stop();
         }
 
