95610b39e56d5ed3ed572e8a45cbb84d19e40682
Nouman_Ahmad <noman.ahmad@innovadeltech.com>
2021-06-14 13:05:47 +0500

    [MSS-1369] Fix typos

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/search/search.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/search/search.js
@@ -294,8 +294,8 @@ function bulidLifeStyleCarousel() {
 
     $(document).on('beforeChange', '.product-grid .plp-image-carousel', function (event, slick, currentSlide, nextSlide) {
         var nextSlide = slick.$slides.get(nextSlide);
-        var $slideSoureSets = $(nextSlide).find('source');
-        $($slideSoureSets).each(function () {
+        var $slideSourceSets = $(nextSlide).find('source');
+        $($slideSourceSets).each(function () {
             $(this).attr('srcset', $(this).data('lazy'));
         });
     });
