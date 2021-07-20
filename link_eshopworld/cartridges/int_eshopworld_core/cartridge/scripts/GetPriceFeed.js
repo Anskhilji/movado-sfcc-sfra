4496f2cd2381c89f7ca5fd9ecea0e72ca1245290
Muhammad Yasir <muhammad.yasir@innovadeltech.com>
2021-07-20 19:38:11 +0500

    MSS-1491-ESW-Patch-catridge-implementation

--- link_eshopworld/cartridges/int_eshopworld_core/cartridge/scripts/GetPriceFeed.js
+++ link_eshopworld/cartridges/int_eshopworld_core/cartridge/scripts/GetPriceFeed.js
@@ -2,17 +2,18 @@
  * Script file for calling price feed service api and update site preferences from response.
  * @return {boolean} - returns execute result
  */
-function execute() {
+ function execute() {
     var eswHelper = require('*/cartridge/scripts/helper/eswHelper').getEswHelper(),
         eswServices = require('*/cartridge/scripts/services/EswCoreService').getEswServices(),
         logger = require('dw/system/Logger'),
+        Status = require('dw/system/Status'),
         Site = require('dw/system/Site').getCurrent();
     try {
         var priceFeedServiceObj,
             priceFeedResult,
             today = new Date();
 
-        var oAuthObj = eswServices.getOAuthService(),
+        var oAuthObj = eswServices.getPricingOAuthService(),
             dayOfAPICall = eswHelper.getDayOfLastAPICall(),
             shortenedDayOfAPICall = dayOfAPICall;
         if (!empty(dayOfAPICall)) {
@@ -26,10 +27,10 @@ function execute() {
                 'scope': 'pricing.advisor.api.all' // eslint-disable-line quote-props
             };
             formData.client_id = eswHelper.getClientID();
-            formData.client_secret = eswHelper.getClientSecret();
+            formData.client_secret = eswHelper.getSelectedPriceFeedInstance() === 'production' ? eswHelper.getProductionClientSecret() : eswHelper.getClientSecret();
             var oAuthResult = oAuthObj.call(formData);
             if (oAuthResult.status === 'OK' && !empty(oAuthResult.object)) {
-                priceFeedServiceObj = eswServices.getPricingV2Service();
+                priceFeedServiceObj = eswServices.getPricingV3Service();
                 priceFeedResult = priceFeedServiceObj.call(JSON.parse(oAuthResult.object).access_token);
 
                 if (priceFeedResult.status === 'OK' && !empty(priceFeedResult.object)) {
@@ -43,17 +44,24 @@ function execute() {
                     Site.setCustomPreferenceValue('eswRounding', JSON.stringify(rounding));
                     Site.setCustomPreferenceValue('eswDay', priceFeed.lastUpdated);
                     Site.setCustomPreferenceValue('eswPricingSynchronizationId', priceFeed.pricingSynchronizationId);
+                } else if (priceFeedResult.status === 'ERROR' || empty(priceFeedResult.object)) {
+                    if (priceFeedResult.error === 403) {
+                        logger.error('ESW PriceFeed Service Forbidden Error: 403: Make sure that service URL configurations, Client ID and Client Secret are aligned and correct.');
+                        return new Status(Status.ERROR);
+                    }
+                    logger.error('ESW PriceFeed Service Error: {0} {1}', priceFeedResult.error, priceFeedResult.msg);
+                    return new Status(Status.ERROR);
                 }
             } else if (oAuthResult.status === 'ERROR' || empty(oAuthResult.object)) {
                 logger.error('ESW Service Error: {0}', oAuthResult.errorMessage);
-                return true;
+                return new Status(Status.ERROR);
             }
         }
     } catch (e) {
         logger.error('ESW PriceFeed Error: {0} {1}', e.message, e.stack);
-        return true;
+        return new Status(Status.ERROR);
     }
-    return false;
+    return new Status(Status.OK);
 }
 
 exports.execute = execute;
