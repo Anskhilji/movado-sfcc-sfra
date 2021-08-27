3c7e5cff12f6ba8efc58758a7110b7523e263852
Muhammad_Usman556 <usman.irshad@innovadeltech.com>
2021-08-27 19:01:00 +0500

    MSS-1450 Fix Country issue on Meta1

--- listrak/cartridges/int_custom_listrak/cartridge/static/default/js/ltkSendOrder.js
+++ listrak/cartridges/int_custom_listrak/cartridge/static/default/js/ltkSendOrder.js
@@ -3,6 +3,7 @@
 /* eslint-disable block-scoped-var */
 /* eslint-disable consistent-return */
 /* eslint-disable no-loop-func */
+var orderIsSent = false;
 (function (d) {
     if (document.addEventListener) document.addEventListener('ltkAsyncListener', d);
     else {
@@ -10,7 +11,15 @@
             if (e.propertyName === 'ltkAsyncProperty') { d(); }
         });
     }
+    if (!orderIsSent) {
+        sendOrder();
+    }
 }(function () {
+    if (!orderIsSent) {
+        sendOrder();
+    }
+}));
+function sendOrder() {
     var scriptVars = document.querySelector('script[src*="ltkSendOrder.js"]');
     var order = JSON.parse(decodeURI(scriptVars.getAttribute('ltk-data-order')));
     var sca = JSON.parse(decodeURI(scriptVars.getAttribute('ltk-data-sca')));
@@ -22,20 +31,21 @@
     if (typeof sca === 'undefined') {
         sca = '';
     }
-	/* If the Order object is not null or empty, iterate the order items and call JS to ship data to Listrak. */
-    if (order !== null)	{
-		/* Add the basic order information. */
+    /* If the Order object is not null or empty, iterate the order items and call JS to ship data to Listrak. */
+    if (order !== null) {
+        /* Add the basic order information. */
         _ltk.Order.HandlingTotal = order.HandlingTotal;
         _ltk.Order.ItemTotal = order.ItemTotal;
         _ltk.Order.OrderNumber = order.OrderNumber;
         _ltk.Order.OrderTotal = order.OrderTotal;
         _ltk.Order.ShippingTotal = order.ShipTotal;
         _ltk.Order.TaxTotal = order.TaxTotal;
+        _ltk.Order.Meta1 = ltkCountryCode;
         _ltk.Order.SetCustomer(order.EmailAddress, order.FirstName, order.LastName);
 
-		/* If there are order items, add them to the object for submission. */
-        if (order.OrderItems.length > 0)		{
-            for (i = 0; i < order.OrderItems.length; i++)			{
+        /* If there are order items, add them to the object for submission. */
+        if (order.OrderItems.length > 0) {
+            for (i = 0; i < order.OrderItems.length; i++) {
                 _ltk.Order.AddItem(order.OrderItems[i].Sku, order.OrderItems[i].Qty, order.OrderItems[i].Price);
             }
         }
@@ -44,9 +54,9 @@
         _ltk.Order.Submit();
     }
 
-	/* If the SCA object is not null or empty, iterate through it and call JS to ship data to Listrak. */
-    if (sca != null)	{
-		/* Set the SCA parameters. */
+    /* If the SCA object is not null or empty, iterate through it and call JS to ship data to Listrak. */
+    if (sca != null) {
+        /* Set the SCA parameters. */
         _ltk.SCA.FirstName = sca.FirstName;
         _ltk.SCA.LastName = sca.LastName;
         _ltk.SCA.Email = sca.Email;
@@ -59,34 +69,35 @@
         _ltk.SCA.Stage = sca.Stage;
         _ltk.SCA.Total = sca.Total;
 
-		/* If there are cart items, add them. */
-        if (sca.Items.length > 0)		{
-            for (i = 0; i < sca.Items.length; i++)			{
+        /* If there are cart items, add them. */
+        if (sca.Items.length > 0) {
+            for (i = 0; i < sca.Items.length; i++) {
                 var item = new SCAItem(sca.Items[i].Sku, sca.Items[i].Quantity, sca.Items[i].Price, sca.Items[i].Name);
                 item.ImageUrl = sca.Items[i].ImageUrl;
                 item.LinkUrl = sca.Items[i].LinkUrl;
                 item.Meta1 = sca.Items[i].Meta1;
                 item.Meta2 = sca.Items[i].Meta2;
 
-				/* Add the expanded cart item to the object for submission. */
+                /* Add the expanded cart item to the object for submission. */
                 _ltk.SCA.AddItemEx(item);
             }
         }
 
-		/* Submit the cart information. */
+        /* Submit the cart information. */
         _ltk.SCA.Submit();
 
-		/* If clear cart, set it. */
-        if (sca.ClearCart)		{
+        /* If clear cart, set it. */
+        if (sca.ClearCart) {
             _ltk.SCA.ClearCart();
         }
     }
 
-	/* Reset the Order flag so we don't ship it up again unless another order is placed. */
+    /* Reset the Order flag so we don't ship it up again unless another order is placed. */
     var path = clearUrl;
     jQuery.ajax({
-		  url: path,
-		  context: document,
-		  success: function () { }
+        url: path,
+        context: document,
+        success: function () { }
     });
-}));
+    orderIsSent = true;
+}
