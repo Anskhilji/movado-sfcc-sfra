7ae2e5711f6f8c75d4e285e532f88226e7bdae90
Nouman_Ahmad <noman.ahmad@innovadeltech.com>
2021-09-15 21:56:31 +0500

    [MSS-1546] Credit Card Form Redesign Input Fields Redesigned Validation Functionality Done For Input Fields

--- movado/cartridges/app_custom_movado/cartridge/client/default/js/adyen-cse.js
+++ movado/cartridges/app_custom_movado/cartridge/client/default/js/adyen-cse.js
@@ -38,7 +38,7 @@ function getCardData(selectedCard) {
     if (!selectedCard) {
         cardData.number = $('#cardNumber').val();
         cardData.holderName = $('#holderName').val();
-        cardData.expiryMonth = $('#expirationMonthandYear').val();
+        cardData.expirationMonth = $('#expirationMonth').val();
         cardData.cvc = $('#securityCode').val();
     } else {
         cardData.cvc = $('.selected-payment #saved-payment-security-code').val();
@@ -51,6 +51,7 @@ function encryptData(cardData, options) {
     var encryptedDataValue;
     var cseInstance = adyen.createEncryption(options);
     var validationResult = cseInstance.validate(cardData);
+    validationResult =  customcardvalidation(cardData,validationResult);
     $('#invalidCardDetails').hide();
     if (!validationResult.valid) {
         showValidation(validationResult);
@@ -61,6 +62,17 @@ function encryptData(cardData, options) {
     encryptedDataValue = cseInstance.encrypt(cardData);
     encryptedData.val(encryptedDataValue);
     return true;
+}
+function customcardvalidation(cardData, validationResult) {
+    var nameRegex = new RegExp("^[a-zA-Z ]+$");
+    if (!nameRegex.test(cardData.holderName)){
+        validationResult.holderName=false;
+    }
+    var expdateRegex = new RegExp("^(?:0?[1-9]|1[0-2]) *\/ *[1-9][0-9]$");
+    if (!expdateRegex.test(cardData.expirationMonth)){
+        validationResult.expirationMonth=false;
+    }
+    return validationResult;
 }
 
 function showValidation(validationResult) {
@@ -74,11 +86,8 @@ function showValidation(validationResult) {
                 case 'number':
                     $('#cardNumber').addClass('is-invalid');
                     break;
-                case 'expiryMonth':
+                case 'expirationMonth':
                     $('#expirationMonth').addClass('is-invalid');
-                    break;
-                case 'expirationMonthandYear':
-                    $('#expirationMonthandYear').addClass('is-invalid');
                     break;
                 case 'expiryYear':
                     $('#expirationYear').addClass('is-invalid');
@@ -100,7 +109,6 @@ function clearValidations() {
     $('#expirationMonth').removeClass('is-invalid');
     $('#expirationYear').removeClass('is-invalid');
     $('#securityCode').removeClass('is-invalid');
-    $('#expirationMonthandYear').removeClass('is-invalid');
 }
 
 function maskValue(value) {
