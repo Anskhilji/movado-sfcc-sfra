cdd8b833b93ea6b14e9044913d5525eb4c8cd35c
Muhammad_Usman556 <usman.irshad@innovadeltech.com>
2021-09-26 21:14:11 +0500

    Fix issues reported by QA

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/base.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/base.js
@@ -67,17 +67,34 @@ function loadAmazonButton() {
         tries++;
         if ($('.dw-apple-pay-button').length) {
             applePayLength = 0;
+            $('.amazon-mini-button').removeClass('pl-1');
         }
         if (!$('#AmazonPayButtonCheckout').attr('class')) {
             $('.amazon-mini-button').remove();
         }
         $('.checkout-btn-adjustment').removeClass('col-12 col-6 col-4');
+        $('.apple-btn-adjustment').removeClass('col-12 col-6 col-4 pl-0');
         var colSize = 12 / ($('.shipping-paypal-btn > div').length - applePayLength);
         $('.checkout-btn-adjustment').addClass('col-' + colSize);
         if ($('.dw-apple-pay-button').length) {
             $('.apple-btn-adjustment').addClass('col-' + colSize);
         }
         $('#AmazonPayButtonCheckout').css('width', 'inherit');
+        if ($(window).width() < 480 && colSize == 4) {
+            $('.checkout-btn-adjustment').removeClass('col-12 col-6 col-4');
+            $('.checkout-btn-adjustment').addClass('col-6');
+            $('.apple-btn-adjustment').addClass('col-6');
+            $('.paypal-mini-button').addClass('col-12');
+            $('.apple-btn-adjustment').addClass('pl-0');
+            $(".dw-apple-pay-button").css({ "margin-left": "0", "height": "20px" });
+        } else if (colSize == 4) {
+            $(".dw-apple-pay-button").css({ "height": "34px" });
+            $('.shipping-paypal-btn img').css('height', '24px')
+        } else if (colSize == 6 && $(window).width() <= 742) {
+            $('.shipping-paypal-btn img').css('height', '20px')
+        }else if(colSize == 6){
+            $('.shipping-paypal-btn img').css('height', '24px')
+        }
         if (tries >= 10) {
             clearInterval(interval);
         }
