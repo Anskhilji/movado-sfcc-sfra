d87b795f58ac7095a73ffbd1150b363ceb73665e
Muhammad Yasir <muhammad.yasir@innovadeltech.com>
2021-10-21 14:41:25 +0500

    MSS-1538 auto complete Address validations UAT Fixes

--- olivia-burton-uk/cartridges/app_custom_ob_uk/cartridge/client/default/js/checkout.js
+++ olivia-burton-uk/cartridges/app_custom_ob_uk/cartridge/client/default/js/checkout.js
@@ -6,8 +6,14 @@ var processInclude = require('base/util');
 $(document).ready(function () {
     processInclude(require('movado/checkout'));
     $('body').load(function() {
-        if (history.state == "shipping") {
+        if (history.state == "shipping") { 
             $('body').trigger('checkOutStage:success', history.state);
         } 
     });
+
+});
+
+$(document).ready(function () { 
+        $('#shippingState').attr("id", "shippingCounty");
+        $('#billingState').attr("id", "billingCounty");
 });
