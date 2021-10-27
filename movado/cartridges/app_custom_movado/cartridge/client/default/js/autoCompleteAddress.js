b00c75d880bd6bb61e4d60c3408f4f1d8c628763
Muhammad_Usman556 <usman.irshad@innovadeltech.com>
2021-10-27 15:19:39 +0500

    udpate condition

--- movado/cartridges/app_custom_movado/cartridge/client/default/js/autoCompleteAddress.js
+++ movado/cartridges/app_custom_movado/cartridge/client/default/js/autoCompleteAddress.js
@@ -5,7 +5,9 @@ var processInclude = require('base/util');
 $(document).ready(function () {
     processInclude(require('./checkout/autoCompleteAddress'));
     try {
-        google.maps.event.addDomListener(window, 'load', initAutocomplete);
+        if (typeof google != 'undefined') {
+            google.maps.event.addDomListener(window, 'load', initAutocomplete);
+        }
     } catch (error) { }
 });
 
