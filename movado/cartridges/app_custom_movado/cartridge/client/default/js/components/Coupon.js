7f3ec95593d3f72440c7635dbe5ece925cd324a3
Bilal Javed <bilal.javed@innovadeltech.com>
2021-11-08 17:43:54 +0500

    MSS-1589 Fix try catch conditon.

--- movado/cartridges/app_custom_movado/cartridge/client/default/js/components/Coupon.js
+++ movado/cartridges/app_custom_movado/cartridge/client/default/js/components/Coupon.js
@@ -1,24 +1,28 @@
-var urlParams;
-    (window.onpopstate = function () {
-        var match,
-            pl     = /\+/g,  // Regex for replacing addition symbol with a space
-            search = /([^&=]+)=?([^&]*)/g,
-            decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
-            query  = window.location.search.substring(1);
-        urlParams = {};
-        while (match = search.exec(query))
-           urlParams[decode(match[1])] = decode(match[2]);
-    })();
 
 $(document).ready(function(){
-    if(urlParams.popup_code) {
-        var url = Resources.COUPONCODE_URL+'?popup_code=' + urlParams.popup_code
-        $.ajax({
-            type: "POST",
-            url: url,
-            data: "",
-            success: function (response) {
-            }
-        });
-    }
-})
+    try {
+        var urlParams;
+        (window.onpopstate = function () {
+            var match,
+                pl     = /\+/g,  // Regex for replacing addition symbol with a space
+                search = /([^&=]+)=?([^&]*)/g,
+                decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+                query  = window.location.search.substring(1);
+                urlParams = {};
+            while (match = search.exec(query))
+               urlParams[decode(match[1])] = decode(match[2]);
+        })();
+
+
+        if(urlParams.popup_code) {
+            var url = Resources.COUPONCODE_URL+'?popup_code=' + urlParams.popup_code
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: "",
+                success: function (response) {
+                }
+            });
+        }
+    } catch (error) { }
+});
