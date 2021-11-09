15cf5bb88ab4b62640ead0996dd6cd1e3fd5a5f8
Muhammad Yasir <muhammad.yasir@innovadeltech.com>
2021-11-09 20:51:22 +0500

    implement-all-brands--order-lookup-oms

--- SalesforceOrderManagement/cartridges/int_movado_som/cartridge/controllers/Order.js
+++ SalesforceOrderManagement/cartridges/int_movado_som/cartridge/controllers/Order.js
@@ -144,4 +144,110 @@ server.replace(
     }
 );
 
+server.replace(
+    'Track',
+    consentTracking.consent,
+    server.middleware.https,
+    csrfProtection.validateRequest,
+    csrfProtection.generateToken,
+    function (req, res, next) {
+        var OrderMgr = require('dw/order/OrderMgr');
+        var OrderModel = require('*/cartridge/models/order');
+        var Locale = require('dw/util/Locale');
+
+        var order;
+        var validForm = true;
+
+        var profileForm = server.forms.getForm('profile');
+        profileForm.clear();
+
+        var emailAddress = req.querystring.trackOrderEmail;
+        var SalesforceModel = require('*/cartridge/scripts/SalesforceService/models/SalesforceModel');
+
+        var orders = SalesforceModel.getOrdersByCustomerEmail({
+            emailAddress: emailAddress,
+            salesChannel: Site.getCurrent().getID()
+        });
+
+        var ordersArray = orders.object.orders;
+        var orderNumberParam = req.querystring.trackOrderNumber;
+        if (!empty(ordersArray) && !empty(orderNumberParam)) {
+            var filteredOrder = ordersArray.filter(function (orderIndex) {
+                return orderIndex.num == orderNumberParam
+            });
+        }
+
+        if (req.querystring.trackOrderEmail
+            && req.querystring.trackOrderPostal
+            && req.querystring.trackOrderNumber) {
+            order = OrderMgr.getOrder(req.querystring.trackOrderNumber);
+        } else {
+            validForm = false;
+        }
+
+        if (!order) {
+            res.render('/account/login', {
+                navTabValue: 'login',
+                orderTrackFormError: validForm,
+                profileForm: profileForm,
+                userName: ''
+            });
+            next();
+        } else {
+            var config = {
+                numberOfLineItems: '*'
+            };
+
+            var currentLocale = Locale.getLocale(req.locale.id);
+
+            var orderModel = new OrderModel(
+                order,
+                { config: config, countryCode: currentLocale.country, containerView: 'order' }
+            );
+
+            // check the email and postal code of the form
+            if (req.querystring.trackOrderEmail.toLowerCase()
+                    !== orderModel.orderEmail.toLowerCase()) {
+                validForm = false;
+            }
+
+            if (req.querystring.trackOrderPostal
+                !== orderModel.billing.billingAddress.address.postalCode) {
+                validForm = false;
+            }
+
+            if (validForm) {
+                var exitLinkText;
+                var exitLinkUrl;
+
+                exitLinkText = !req.currentCustomer.profile
+                    ? Resource.msg('link.continue.shop', 'order', null)
+                    : Resource.msg('link.orderdetails.myaccount', 'account', null);
+
+                exitLinkUrl = !req.currentCustomer.profile
+                    ? URLUtils.url('Home-Show')
+                    : URLUtils.https('Account-Show');
+
+                res.render('account/orderDetails', {
+                    order: orderModel,
+                    exitLinkText: exitLinkText,
+                    exitLinkUrl: exitLinkUrl,
+                    orders: orders.object.orders,
+                    filteredOrder: filteredOrder
+
+                });
+            } else {
+                res.render('/account/login', {
+                    navTabValue: 'login',
+                    profileForm: profileForm,
+                    orderTrackFormError: !validForm,
+                    userName: ''
+                });
+            }
+
+            next();
+        }
+    }
+);
+
 module.exports = server.exports();
