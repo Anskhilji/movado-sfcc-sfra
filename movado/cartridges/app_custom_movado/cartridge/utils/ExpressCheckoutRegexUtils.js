7ca10f3ec4d808f46cf2db6075c096fa9f2f20a0
muhammad_yasir_bashir <muhammad.yasir@innovadeltech.com>
2021-11-25 20:46:08 +0500

    MSS- Update Regex again

--- movado/cartridges/app_custom_movado/cartridge/utils/ExpressCheckoutRegexUtils.js
+++ movado/cartridges/app_custom_movado/cartridge/utils/ExpressCheckoutRegexUtils.js
@@ -4,7 +4,7 @@ var checkoutFieldsRegex = {};
 
 checkoutFieldsRegex.firstName = new RegExp("^[^(\\'\\\\>\\\)]+$");
 checkoutFieldsRegex.lastName = new RegExp("^[^(\\'\\\\>\\\)]+$");
-checkoutFieldsRegex.email = /^(?=[a-zA-Z0-9_-]{1,64}(?!.*?\.\.)+(?!\@)+[a-zA-Z0-9!.#$%&;'*+-=?^_`{|}~\S+-]{1,64})+[^\@\s]{1,64}[^\@\.\s\-]+@[^\-\.\s][a-zA-Z0-9\.-]+[\.]+(?!.*web|.*'')[a-zA-Z0-9\.]{2,6}[^\-\.,;@\s]$/;
+checkoutFieldsRegex.email = /^(?=[a-zA-Z0-9_-]{1,64}(?!.*?\.\.)+(?!\@)+[a-zA-Z0-9!.#$%&;'*+-=?^_`{|}~\S+-]{1,64})+[^\@,;\s]{1,64}[^\@\.,;\s\-]+@[^\-\.\s][a-zA-Z0-9\.-]+[\.]+(?!.*web|.*'')[a-zA-Z0-9\.]{2,6}[^\-\.,;@\s]$/;
 checkoutFieldsRegex.companyName = new RegExp("(^[^(\\'\\\\>\\\)]+$)");
 checkoutFieldsRegex.address1 = new RegExp("(^((?!(([\\'\\\\\\>\\;])|(\b(?:[pP](?:[oO][sS][tT](?:[aA][lL])?)?[\.\-\s]*(?:(?:[oO](?:[fF][fF][iI][cC][eE])?[\.\-\s]*)?[bB](?:[oO][xX]|[iI][nN]|\b|\d)|[oO](?:[fF][fF][iI][cC][eE])(?:[-\s]*)|[cC][oO][dD][eE]))))).)*$)");
 checkoutFieldsRegex.postalCode = new RegExp("(^[a-zA-Z0-9 ]{4,10})");
