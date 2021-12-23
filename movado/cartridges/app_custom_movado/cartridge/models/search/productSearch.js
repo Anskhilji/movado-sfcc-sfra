d58f686b52275e7465b8ab70e55d5609ed54250e
muhammad_yasir_bashir <muhammad.yasir@innovadeltech.com>
2021-12-23 17:31:40 +0500

    MSS-1613 fixed

--- movado/cartridges/app_custom_movado/cartridge/models/search/productSearch.js
+++ movado/cartridges/app_custom_movado/cartridge/models/search/productSearch.js
@@ -273,8 +273,7 @@ function ProductSearch(productSearch, httpParams, sortingRule, sortingOptions, r
             pageTitle: productSearch.category.pageTitle,
             pageDescription: productSearch.category.pageDescription,
             pageKeywords: productSearch.category.pageKeywords,
-            raw: productSearch.category,
-            renderingTemplate: productSearch.category.template
+            raw: productSearch.category
         };
     }
     this.pageMetaTags = productSearch.pageMetaTags;
