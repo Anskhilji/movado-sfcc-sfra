2e4346aaad1773a2892e00f7aca29a95c113177c
Hamza Yasin <hyasin@pixelmedia.com>
2022-02-17 23:33:17 +0500

    MSS-1671 Fix Bug

--- link_clyde/cartridges/int_clyde_sfra/cartridge/scripts/util/resource.js
+++ link_clyde/cartridges/int_clyde_sfra/cartridge/scripts/util/resource.js
@@ -16,7 +16,7 @@ ResourceHelper.getClydePreferences = function () {
         CLYDE_WIDGET_ENABLED: Site.current.preferences.custom.clydeWidgetDisplay ? true : false, // eslint-disable-line no-unneeded-ternary,
         CLYDE_API_KEY: Site.getCurrent().getCustomPreferenceValue('clydeAPIKey') || '',
         CLYDE_WIDGET_ENVIRONMENT: Site.getCurrent().getCustomPreferenceValue('clydeWidgetEnviroment') ? Site.current.preferences.custom.clydeWidgetEnviroment.value : '',
-        CLYDE_WIDGET_TYPE: Site.getCurrent().getCustomPreferenceValue('clydeWidgetType') ? Site.getCurrent().getCustomPreferenceValue('clydeWidgetType').value : '',
+        CLYDE_WIDGET_TYPE: !empty(Site.getCurrent().getCustomPreferenceValue('clydeWidgetType')) ? Site.getCurrent().getCustomPreferenceValue('clydeWidgetType').value : '',
         CLYDE_SKIP_GEO_IP: Site.getCurrent().getCustomPreferenceValue('clydeGeofencing') ? true : false  // eslint-disable-line no-unneeded-ternary
     };
 };
