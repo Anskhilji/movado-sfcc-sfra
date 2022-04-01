a3f8149c289bd219842365fe3e7a0da4036459be
Ahmad_Usman123 <ahmad.usman@innovadeltech.com>
2022-04-01 18:27:31 +0500

    MSS-1769 Load Store Locator with United States set as the default country

--- movado/cartridges/app_custom_movado/cartridge/controllers/Stores.js
+++ movado/cartridges/app_custom_movado/cartridge/controllers/Stores.js
@@ -34,9 +34,9 @@ server.replace('Find', server.middleware.https, cache.applyDefaultCache, consent
 
     var storeResult = storeHelpers.getStores(radius, lat, lng, req.geolocation, countryCode, showMap, null, null);
     if(!countryCode){
-        
         countryCode = request.geolocation.countryCode;
     }
+    
     viewData = {
         countries: countries,
         stores: storeResult,
