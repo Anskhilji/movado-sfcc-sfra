c31386dcbab0c763c88f33a51dea4957b3590eb8
AmmarSadiq <ammar.sadiq@innovadeltech.com>
2022-04-21 14:21:09 +0500

    MSS-1625

--- movado/cartridges/app_custom_movado/cartridge/client/default/js/productTile.js
+++ movado/cartridges/app_custom_movado/cartridge/client/default/js/productTile.js
@@ -5,12 +5,3 @@ var processInclude = require('base/util');
 $(document).ready(function () {
     processInclude(require('./product/quickView'));
 });
-
-if ($(window).width() > 768) {
-    $(document).ready(function() {
-        $(window).resize(function() {
-        var productTitle = $(".homepagetile-wrapper-box").height() - 10;
-            $('.main-container-inner img').css({'height': productTitle});
-        }).resize();
-    });
-}
