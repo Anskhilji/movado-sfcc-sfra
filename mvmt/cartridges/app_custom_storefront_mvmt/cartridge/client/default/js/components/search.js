e6c93bb1e800d34bac16363d84b6ccef62b584f1
M.Hamza Yasin <hamza.yasin@innovadeltech.com>
2022-05-13 13:35:41 +0500

    MSS-1826 Complete ticket functionality

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/components/search.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/components/search.js
@@ -142,9 +142,10 @@ function getSuggestions(scope) {
             success: function (data) {
                 var resposeCount = $('#searchCount', $(data).context).val();
                 processResponse;
-                if (resposeCount == currentCount) { 
+                if (resposeCount == currentCount) {
                     $('body').trigger('siteSearch:success', $(scope).val());
                 }
+                slickSearchSwatch();
             },
 
             error: function () { $.spinner().stop();}
@@ -159,6 +160,28 @@ function getSuggestionsSlots() {
     if ($('input.search-field').val().length === 0) {
         $suggestionsSlots.show();
     }
+}
+
+function slickSearchSwatch () {
+    $('.product-tile-redesign .swatches').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        autoplay: false,
+        prevArrow: '<button type="button" data-role="none" class="slick-prev slick-arrow" aria-label="Previous" tabindex="0" role="button"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
+        nextArrow: '<button type="button" data-role="none" class="slick-next slick-arrow" aria-label="Next" tabindex="0" role="button"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
+        responsive: [
+            {
+                breakpoint: 544,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+        ]
+    });
 }
 
 module.exports = function () {
