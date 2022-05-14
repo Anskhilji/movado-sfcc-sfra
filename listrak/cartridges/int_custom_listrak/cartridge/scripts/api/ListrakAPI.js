632fec26f44dce523a3dc7e315270dd5efaf6b97
muhammad_yasir_bashir <muhammad.yasir@innovadeltech.com>
2022-05-14 18:24:06 +0500

    mss-1797-listrack-transactional-email

--- listrak/cartridges/int_custom_listrak/cartridge/scripts/api/ListrakAPI.js
+++ listrak/cartridges/int_custom_listrak/cartridge/scripts/api/ListrakAPI.js
@@ -48,7 +48,32 @@ function sendTransectionalEmailToListrak(requestParams) {
         params.password = requestParams.passwordText;
         params.messageContext = requestParams.messageContext;
         params.passwordReset = requestParams.passwordReset;
-
+        params.orderNumber = requestParams.orderNumber;
+        params.totalTax = requestParams.totalTax;
+        params.subTotal = requestParams.subTotal;
+        params.grandTotal = requestParams.grandTotal; 
+        params.creationDate=  requestParams.creationDate; 
+        params.billingFirstName = requestParams.billingFirstName; 
+        params.billingLastName = requestParams.billingLastName;
+        params.billingAddress1 = requestParams.billingAddress1;
+        params.billingAddress2 = requestParams.billingAddress2;
+        params.billingCity = requestParams.billingCity;
+        params.billingStateCode = requestParams.billingStateCode;       
+        params.billingPostalCode = requestParams.billingPostalCode; 
+        params.billingCountryCode = requestParams.billingCountryCode; 
+        params.billingPhone = requestParams.billingPhone;
+        params.shippingFirstName = requestParams.shippingFirstName; 
+        params.shippingLastName = requestParams.shippingLastName;
+        params.shippingAddress1 = requestParams.shippingAddress1; 
+        params.shippingAddress2 = requestParams.shippingAddress2;
+        params.shippingCity = requestParams.shippingCity;
+        params.shippingStateCode = requestParams.shippingStateCode; 
+        params.shippingPostalCode = requestParams.shippingPostalCode;
+        params.shippingCountry = requestParams.shippingCountry;
+        params.shippingPhone = requestParams.shippingPhone;
+        params.shippingMethod = requestParams.shippingMethod;
+        params.paymentMethod = requestParams.paymentMethod;
+        params.email = requestParams.email;
         service = LTKAPIHelper.getTransectionalAPIService(serviceID, Constants.LTK_TRANSACTIONAL_API_ENDPOINT, accessToken, requestParams.messageId);
         var result = LTKAPIHelper.addTransectionalEmailToLTK(params, service);
     } catch (e) {
