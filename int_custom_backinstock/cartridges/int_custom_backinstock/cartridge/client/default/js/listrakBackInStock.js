f4ad866ef736d5590577f44c5af9b2aedf5788e0
Anskhan <ans.qasim@innovadeltech.com>
2022-05-16 22:55:37 +0500

    MSS-1828-[Listrak - Back in Stock - Push Email & SMS Signup to Listrak]-fixed-review-comments

--- int_custom_backinstock/cartridges/int_custom_backinstock/cartridge/client/default/js/listrakBackInStock.js
+++ int_custom_backinstock/cartridges/int_custom_backinstock/cartridge/client/default/js/listrakBackInStock.js
@@ -21,11 +21,20 @@ $('.form').submit(function(e) {
     if ($form.find('.back-in-stock-notification-email').length > 0) {
         $email = $('.back-in-stock-notification-email').val().trim();
         var $pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i
-        var $isValid = $pattern.test($email);
+        var $isValid;
+
+        if ($email) {
+            $isValid = $pattern.test($email);
+        }
+        
         if ($form.find('.back-in-stock-notification-phone').length > 0) {
             $phone = $('.back-in-stock-notification-phone').val().trim();
             var $phonePattern = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
-            var $isValidPhone = $phonePattern.test($phone)
+            var $isValidPhone;
+
+            if ($phone) {
+                $isValidPhone = $phonePattern.test($phone)
+            }
 
             if ($phone == '' || $phone == undefined) {
                 if ($email == '' || $email == undefined) {
@@ -41,20 +50,20 @@ $('.form').submit(function(e) {
                 }
             } else if ($phone !== '' && !$isValidPhone && $email !== ''  && !$isValid) {
                 $emailInvalid.text(window.Resources.EMIAL_ADDRESS_INVALID);
-                $phoneInvalid.text(window.Resources.PHONE_NUMBER_INVALID)
+                $phoneInvalid.text(window.Resources.PHONE_NUMBER_INVALID);
                 $email = '';
                 $phone = '';
                 return;
             } else if ($email == '' && !$isValidPhone) {
-                $phoneInvalid.text(window.Resources.PHONE_NUMBER_INVALID)
+                $phoneInvalid.text(window.Resources.PHONE_NUMBER_INVALID);
                 $email = '';
                 $phone = '';
                 return;
             } else if ($isValid && !$isValidPhone) {
-                $phoneInvalid.text(window.Resources.PHONE_NUMBER_INVALID)
+                $phoneInvalid.text(window.Resources.PHONE_NUMBER_INVALID);
                 $email = '';
                 $phone = '';
-            } else if ($isValidPhone && !$isValid) {
+            } else if (!$isValidPhone && !$isValid) {
                 $emailInvalid.text(window.Resources.EMIAL_ADDRESS_INVALID);
                 $email = '';
                 $phone = '';
@@ -75,7 +84,12 @@ $('.form').submit(function(e) {
         if ($form.find('.back-in-stock-notification-phone').length > 0) {
             $phone = $('.back-in-stock-notification-phone').val().trim();
             var $phonePattern = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
-            var $isValidPhone = $phonePattern.test($phone);
+            var $isValidPhone;
+            
+            if ($phone) {
+                $isValidPhone = $phonePattern.test($phone)
+            }
+
             if ($phone == '' || $phone == undefined) {
                 $phoneInvalid.text(window.Resources.PHONE_NUMBER_REQUIRED);
                 $phone = '';
