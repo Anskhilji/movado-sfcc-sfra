b19d0ff46328976ddb315c1d57048523d601dd87
muhammad_yasir_bashir <muhammad.yasir@innovadeltech.com>
2022-05-24 13:50:25 +0500

    MSS-1755 Added condition to block coupon specific promotion

--- movado/cartridges/app_custom_movado/cartridge/scripts/helpers/productSetCustomHelper.js
+++ movado/cartridges/app_custom_movado/cartridge/scripts/helpers/productSetCustomHelper.js
@@ -48,7 +48,7 @@ function getProductSetSalePrice(productID) {
         var PromotionItr = PromotionMgr.activePromotions.getProductPromotions(currentProductSetProduct).iterator();
         if (!empty(PromotionItr)) {
             for each(var promo in PromotionItr) {
-                if (promo.getPromotionClass()!= null && promo.getPromotionClass().equals(Promotion.PROMOTION_CLASS_PRODUCT)) {
+                if (promo.getPromotionClass()!= null && promo.getPromotionClass().equals(Promotion.PROMOTION_CLASS_PRODUCT) && !promo.basedOnCoupons) {
                     if (currentProductSetProduct.optionProduct) {
                         currentPromotionalPrice = promo.getPromotionalPrice(currentProductSetProduct, currentProductSetProduct.getOptionModel());
                         promoCalloutMsg = promo.calloutMsg ? promo.calloutMsg.markup : '';
