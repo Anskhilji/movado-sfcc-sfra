2ef3279d32213fd1c27988872e8d2629a283ba20
AmmarSadiq <ammar.sadiq@innovadeltech.com>
2022-06-07 22:27:43 +0500

    MSS-1829

--- mcsus/cartridges/app_custom_storefront_mcsus/cartridge/client/default/js/product/carouselInitializer.js
+++ mcsus/cartridges/app_custom_storefront_mcsus/cartridge/client/default/js/product/carouselInitializer.js
@@ -27,6 +27,16 @@ $(document).ready(function() {
         centerMode: true,
         focusOnSelect: true,
     });
+    $('.carousel-nav-redesign').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.primary-images .main-carousel',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true,
+        arrows:false,
+        centerPadding:'0',
+    });
 
     $('.zoom-carousel').slick({
         slidesToShow: 1,
--- movado/cartridges/app_custom_movado/cartridge/models/product/fullProduct.js
+++ movado/cartridges/app_custom_movado/cartridge/models/product/fullProduct.js
@@ -31,10 +31,10 @@ module.exports = function fullProduct(product, apiProduct, options) {
 
     if (options.variationModel) {
         // Custom Start: Define view type 'gallery' for DIS
-        decorators.images(product, options.variationModel, { types: ['pdp533','tile532X300','tile640','tile520','tile300','tile150', 'tile156', 'zoom830', 'zoom1660', 'gallery','tile300X375','tile512X640', 'tile256', 'tile300X300','pdp600', 'tile100', 'pdp700', 'pdp453'], quantity: 'all' });
+        decorators.images(product, options.variationModel, { types: ['pdp533','tile532X300','tile640','tile520','tile300','tile150', 'tile156', 'zoom830', 'zoom1660', 'gallery','tile300X375','tile512X640', 'tile256', 'tile300X300','pdp600', 'tile100', 'tile126', 'pdp700', 'pdp453'], quantity: 'all' });
     } else {
      // Custom Start: Define view type for 'gallery' for DIS
-        decorators.images(product, apiProduct, { types: ['pdp533','tile260xtile340','tile532X300','tile640','tile520','tile300','tile150', 'tile156', 'zoom830', 'zoom1660', 'gallery','tile300X375','tile512X640', 'tile256', 'tile300X300','pdp600', 'tile100', 'pdp700', 'pdp453'], quantity: 'all' });
+        decorators.images(product, apiProduct, { types: ['pdp533','tile260xtile340','tile532X300','tile640','tile520','tile300','tile150', 'tile156', 'zoom830', 'zoom1660', 'gallery','tile300X375','tile512X640', 'tile256', 'tile300X300','pdp600', 'tile100', 'tile126', 'pdp700', 'pdp453'], quantity: 'all' });
     }
     decorators.emailImage(product, apiProduct, { types: ['tile150'], quantity: 'single' });
     decorators.quantity(product, apiProduct, options.quantity);
