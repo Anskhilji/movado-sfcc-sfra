e11ad40f3a1cd28b954521405ee61a07a4e4f7f7
muhammad_yasir_bashir <muhammad.yasir@innovadeltech.com>
2022-06-21 16:33:53 +0500

    MSS-1880 Fixed Quantity Bug

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/cart/cart.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/cart/cart.js
@@ -566,6 +566,9 @@ module.exports = function () {
                     </div>
                     </div>`;
                     var $cartContainer =  $header + $image + $description + $urls;
+                    // Custom Start: To Update Product Quantity on MiniCart
+                    $('.minicart-quantity').empty().append(data.basket.numItems);
+                    // Custom End
                     $('.mini-cart-data .popover').empty();
                     updateCartTotals(data.basket);
                     $('.mini-cart-data .popover').append($cartContainer);
@@ -573,13 +576,6 @@ module.exports = function () {
                     if (typeof $cartIcon !== 'undefined' && ($cartIcon !== '' || $cartIcon.length > 0)) {
                         $cartIcon.removeClass('fill-cart-icon');
                     }
-                    // Custom Start: To Update Product Quantity on MiniCart
-                    var $cartItems = $('.cart-quantity-items').data('quantity-id');
-                    if ($cartItems !== '' && $cartItems !== undefined && $cartItems == 0) {
-                        var $cartCount = $('.header-nav-cart-count');
-                        $cartCount.empty().append('0');
-                    }
-                    // Custom End
                 } else {
                     if (data.toBeDeletedUUIDs && data.toBeDeletedUUIDs.length > 0) {
                         for (var i = 0; i < data.toBeDeletedUUIDs.length; i++) {
@@ -686,13 +682,6 @@ module.exports = function () {
                     if (typeof $cartIcon !== 'undefined' && ($cartIcon !== '' || $cartIcon.length > 0)) {
                         $cartIcon.removeClass('fill-cart-icon');
                     }
-                    // Custom Start: To Update Product Quantity on MiniCart
-                    var $cartItems = $('.cart-quantity-items').data('quantity-id');
-                    if ($cartItems !== '' && $cartItems !== undefined && $cartItems == 0) {
-                        var $cartCount = $('.header-nav-cart-count');
-                        $cartCount.empty().append('0');
-                    }
-                    // Custom End
                     if(data.cartAnalyticsTrackingData && typeof setAnalyticsTrackingByAJAX != 'undefined') {
                         setAnalyticsTrackingByAJAX.cartAnalyticsTrackingData = data.cartAnalyticsTrackingData;
                         window.dispatchEvent(setAnalyticsTrackingByAJAX);
