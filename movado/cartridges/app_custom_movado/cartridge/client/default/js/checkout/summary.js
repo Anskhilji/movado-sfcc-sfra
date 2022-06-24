2b82ee1535ea965090733f557afbbfd61b251529
Anskhan <ans.qasim@innovadeltech.com>
2022-06-25 00:14:31 +0500

    MSS-1795 Checkout Form Alignment & Design

--- movado/cartridges/app_custom_movado/cartridge/client/default/js/checkout/summary.js
+++ movado/cartridges/app_custom_movado/cartridge/client/default/js/checkout/summary.js
@@ -14,6 +14,7 @@ function updateTotals(totals) {
     $('.tax-total').text(totals.totalTax);
     $('.sub-total').text(totals.subTotal);
     $('.grand-total-sum').text(totals.grandTotal);
+    $('.sub-total-redesign').text($('.sub-total-redesign').data('subTotal'));
 
     if (totals.orderLevelDiscountTotal.value > 0) {
         $('.order-discount').removeClass('hide-order-discount');
@@ -29,6 +30,8 @@ function updateTotals(totals) {
     } else {
         $('.shipping-discount').addClass('hide-shipping-discount');
     }
+
+    $('.order-discount-total-redesign').text('- ' + $('.order-discount-total-redesign').data('discount'));
 }
 
 /**
