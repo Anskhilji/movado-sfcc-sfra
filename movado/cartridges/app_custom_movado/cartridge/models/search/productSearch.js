1083d64442b2f4e87fd47668eed7a4f21c328fdf
Haroon Rasheed <haroon.rasheed@innovadeltech.com>
2022-07-09 21:25:43 -0400

    [MSS-711] Fix indentation

--- movado/cartridges/app_custom_movado/cartridge/models/search/productSearch.js
+++ movado/cartridges/app_custom_movado/cartridge/models/search/productSearch.js
@@ -254,22 +254,8 @@ function getSortedProductsOnBasisOfSalesPrice(productSearch, httpParams, sorting
 
     }
     allSearchHitsProducts.forEach(function (searchHitResultProduct) {
-        var defaultVariant = null;
-        if (!empty(searchHitResultProduct.productID)) {
-            if (!empty(searchHitResultProduct.productSearchHit.product.variationModel) && !empty(searchHitResultProduct.productSearchHit.product.variationModel.defaultVariant)) {
-                defaultVariant = searchHitProduct.product.variationModel.defaultVariant;
-            }
-        }
-
-        var searchHitProductID;
-        if (defaultVariant !== null) {
-            searchHitProductID = defaultVariant.ID
-        } else {
-            searchHitProductID = searchHitResultProduct.productID
-        }
-
         paramContainer = {
-            pid: searchHitProductID
+            pid: searchHitResultProduct.productID
         };
 
         factoryProduct = ProductFactory.get(paramContainer);
