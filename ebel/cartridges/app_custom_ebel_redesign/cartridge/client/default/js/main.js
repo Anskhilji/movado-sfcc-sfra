dbdd06ba111b653093ecbe7ab3ca35799a8b8607
Anskhan <ans.qasim@innovadeltech.com>
2022-07-20 00:18:21 +0500

    MSS-1713-Apply Movado Design to EBEL website

--- ebel/cartridges/app_custom_ebel_redesign/cartridge/client/default/js/main.js
+++
@@ -1,9 +0,0 @@
-var processInclude = require('base/util');
-processInclude(require('movado/main'));
-
-$(document).ready(function () {
-    processInclude(require('./header/header'));
-    processInclude(require('malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min'));
-    processInclude(require('./components/footer'));
-});
-
--- ebel/cartridges/app_custom_ebel_redesign/cartridge/client/default/js/productDetail.js
+++
@@ -1,7 +0,0 @@
-var processInclude = require('base/util');
-processInclude(require('movado/productDetail'));
-
-$(document).ready(function () {
-    processInclude(require('./product/zoom'));
-    processInclude(require('./product/productDetailCustom'));
-});
