16b6dc1fdd3377fa7ae56e8026d09e6100b38daa
muhammad_yasir_bashir <muhammad.yasir@innovadeltech.com>
2022-08-11 13:42:31 +0500

    mss-1903-listrak-subscription-for-backin-stock

--- int_custom_backinstock/cartridges/int_custom_backinstock/cartridge/client/default/js/listrakBackInStock.js
+++ int_custom_backinstock/cartridges/int_custom_backinstock/cartridge/client/default/js/listrakBackInStock.js
@@ -1,5 +1,4 @@
 var backInStockNotification = require('../js/backInStockNotification');
-var triggerEmail = true;
 
 $('.form').submit(function(e) {
     e.preventDefault();
@@ -14,6 +13,7 @@ $('.form').submit(function(e) {
     var $emailInvalid = $('.back-in-stock-notification-error-invalid');
     var $phoneInvalid = $('.back-in-stock-notification-invalid-phone');
     var $backInStockListrakPreference = $('#backInStockMarketingCloudPreference');
+    var $triggerEmail = true;
     $emailRequired.text('');
     $emailInvalid.text('');
     $phoneInvalid.text('');
@@ -27,9 +27,9 @@ $('.form').submit(function(e) {
             $isValid = $pattern.test($email);
             if ($isValid) {
                 if ($backInStockListrakPreference.length > 0) {
-                    if (triggerEmail) {
+                    if ($triggerEmail) {
                         e.preventDefault();
-                        triggerEmail = false;
+                        $triggerEmail = false;
                         var $selector;
                         if ($backInStockContainerMain.length > 0) {
                             $selector = $('.listrak-back-in-stock-notification-container-main');
