5797a6f4943d092c07e943f8ea2cf203338ed8cb
Anskhan <ans.qasim@innovadeltech.com>
2022-08-31 21:17:27 +0500

    MSS-1412-pickup-model-design-complete

--- mcsus/cartridges/app_custom_mcsus_redesign/cartridge/controllers/PickupStore.js
+++ mcsus/cartridges/app_custom_mcsus_redesign/cartridge/controllers/PickupStore.js
@@ -1,16 +1,27 @@
 'use strict';
 
-var server = require('server');
 
+var server = require('server');
 var page = module.superModule;
 server.extend(page);
 
 server.replace('GetPreferredStore', function (req, res, next) {
     var StoreMgr = require('dw/catalog/StoreMgr');
+    var ABTestMgr = require('dw/campaign/ABTestMgr');
     var preferedPickupStore;
     var isPdp = req.querystring.isPdp;
     var address1;
     var phone;
+    var template = null;
+
+   // Custom Comment Start: A/B testing for OB Redesign PDP
+   if (ABTestMgr.isParticipant('MCSRedesignPDPABTest','Control')) {
+       template = 'product/components/pdpStorePickUp';
+   } else if (ABTestMgr.isParticipant('MCSRedesignPDPABTest','render-new-design')) {
+       template =  'product/components/pdpStorePickUpRedesign';
+   } else {
+       template = 'product/components/pdpStorePickUp';
+   }
     if (session.privacy.pickupStoreID) {
         preferedPickupStore = StoreMgr.getStore(session.privacy.pickupStoreID);
         address1 = preferedPickupStore.address1;
@@ -20,7 +31,7 @@ server.replace('GetPreferredStore', function (req, res, next) {
         address1: address1,
         phone: phone
     }
-    res.render(isPdp ? 'product/components/pdpStorePickUpRedesign' : 'modalpopup/modelPopUpButton', {
+    res.render(isPdp ? template : 'modalpopup/modelPopUpButton', {
         storeObject: storeObject,
         isPdp: isPdp
     });
