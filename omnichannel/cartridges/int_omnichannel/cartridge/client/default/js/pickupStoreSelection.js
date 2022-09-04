f8586847d31989abb86ab14126d5e0d2fa54b65d
INNOVADELTECH\ammar.sadiq <ammar.sadiq@innovadeltech.com>
2022-09-04 17:28:06 +0500

    MSS-1417-plp-checkout

--- omnichannel/cartridges/int_omnichannel/cartridge/client/default/js/pickupStoreSelection.js
+++ omnichannel/cartridges/int_omnichannel/cartridge/client/default/js/pickupStoreSelection.js
@@ -11,7 +11,8 @@ $(document).ready(function () {
             radius: $radius.val(),
             isSearch: true
         }
-        $.spinner().start();
+
+        $('#pickupStoreModal').spinner().start();
         $.ajax({
             url: url,
             type: 'GET',
