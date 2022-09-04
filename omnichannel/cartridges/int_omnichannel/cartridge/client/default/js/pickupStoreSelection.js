247da7962311a7d7c2d6f86da795e3719968a9ff
INNOVADELTECH\ammar.sadiq <ammar.sadiq@innovadeltech.com>
2022-09-04 19:16:40 +0500

    MSS-1417-checkout-widget

--- omnichannel/cartridges/int_omnichannel/cartridge/client/default/js/pickupStoreSelection.js
+++ omnichannel/cartridges/int_omnichannel/cartridge/client/default/js/pickupStoreSelection.js
@@ -11,7 +11,6 @@ $(document).ready(function () {
             radius: $radius.val(),
             isSearch: true
         }
-
         $('#pickupStoreModal').spinner().start();
         $.ajax({
             url: url,
