d3fcd97bf9cf529506cfd93db167a912d3e8afd6
Anskhan <ans.qasim@innovadeltech.com>
2022-09-04 21:42:16 +0500

    MSS-1946 MVMT watch swatches

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/base.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/base.js
@@ -1,4 +1,5 @@
 'use strict';
+const { constant } = require('lodash');
 var movadoBase = require('movado/product/base');
 if(Resources.IS_CLYDE_ENABLED){
     var clydeWidget = require('link_clyde/getClydeWidget.js');
@@ -1059,6 +1060,15 @@ function handleVariantResponse(response, $productContainer) {
         var $productNameSelector = $('.product-side-details .product-name');
         $productNameSelector.text(response.product.productName);
         var $variationProductURL = $('.variationAttribute').data('url') + '?pid=' + response.product.id + '&isStrapAjax=true';
+        
+        //update case diameter for watches
+        if (typeof response.product.caseDiameterRedesigned !== 'undefined' && response.product.caseDiameterRedesigned !== '' && response.product.caseDiameterRedesigned !== null) {
+            if ($productContainer.find('.product-brand-info .watches-case-diameter').length > 0) {
+                $productContainer.find('.product-brand-info .watches-case-diameter').text(response.product.caseDiameterRedesigned);
+            } else {
+                $productNameSelector.html(response.product.productName + '<span class="watches-case-diameter w-100" data-selected-variation-attr="caseDiameter">'+ response.product.caseDiameterRedesigned +'</span>')
+            }
+        }
 
         $.ajax({
             url: $variationProductURL,
