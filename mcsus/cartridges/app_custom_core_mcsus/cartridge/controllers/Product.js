5f10049ec7e337aea18679033fb225d61b564046
Ahmad_Usman123 <ahmad.usman@innovadeltech.com>
2022-09-08 21:45:29 +0500

    MSS-1407

--- mcsus/cartridges/app_custom_core_mcsus/cartridge/controllers/Product.js
+++ mcsus/cartridges/app_custom_core_mcsus/cartridge/controllers/Product.js
@@ -8,25 +8,6 @@ var ProductMgr = require('dw/catalog/ProductMgr');
 server.extend(page);
 var productHelper = require('*/cartridge/scripts/helpers/productHelpers');
 
-server.get('ShowMcsProdutPrice', function (req, res, next) {
-    var showProductPageHelperResult = productHelper.showProductPage(req.querystring, req.pageMetaData);
-
-    if (Site.getCurrent().getCustomPreferenceValue('Listrak_ActivityTracker_Enabled') &&
-    dw.system.Site.current.preferences.custom.Listrak_Cartridge_Enabled) {
-        var ltkHelper = require('*/cartridge/scripts/helper/ltkHelper.js');
-        var product = ProductMgr.getProduct(showProductPageHelperResult.product.id);
-        var ltkProductPrice = ltkHelper.getProductPrice(product);
-        session.privacy.ltkProductPrice = ltkProductPrice;
-    }
-
-    res.render('product/components/mcsProductPrice', {
-        product: showProductPageHelperResult.product,
-        loggedIn: req.currentCustomer.raw.authenticated,
-        restrictAnonymousUsersOnSalesSites: Site.getCurrent().preferences.custom.restrictAnonymousUsersOnSalesSites
-    });
-    next();
-});
-
 server.get('ShowMcsAffirmText', function (req, res, next) {
     var showProductPageHelperResult = productHelper.showProductPage(req.querystring, req.pageMetaData);
     res.render('product/components/mcsAffirmText', {
