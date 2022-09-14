9e9ca636414a50f79c6ec19cbf09f8388ca5f5b6
INNOVADELTECH\ammar.sadiq <ammar.sadiq@innovadeltech.com>
2022-09-14 18:19:54 +0500

    MSS-1407-google-pay-issues

--- omnichannel/cartridges/int_omnichannel/cartridge/scripts/helpers/resourceHelper.js
+++ omnichannel/cartridges/int_omnichannel/cartridge/scripts/helpers/resourceHelper.js
@@ -18,7 +18,8 @@ function getResources(pageContext) {
     var resources = {
         BOPIS_STORE_FETCHING_ERROR: Resource.msg('store.pickup.search.result.error', 'storePickUp', null),
         BOPIS_STORE_AVAILABLE_TEXT: Resource.msg('store.pickup.search.available.text', 'storePickUp', null),
-        BOPIS_STORE_CART_ERROR: Resource.msg('store.pickup.cart.error', 'storePickUp', null)
+        BOPIS_STORE_CART_ERROR: Resource.msg('store.pickup.cart.error', 'storePickUp', null),
+        GOOGLE_PAY_ENABLED: Site.current.preferences.custom.isGooglePayEnabled || false
     };
     return resources;
 }
