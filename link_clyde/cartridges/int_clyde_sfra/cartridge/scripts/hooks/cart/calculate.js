e89e3519fdb8fddc7267f4b039524faa21f52842
Ahmad_Usman123 <ahmad.usman@innovadeltech.com>
2022-10-03 12:11:07 +0500

    MSS-1953 create promotion price book

--- link_clyde/cartridges/int_clyde_sfra/cartridge/scripts/hooks/cart/calculate.js
+++ link_clyde/cartridges/int_clyde_sfra/cartridge/scripts/hooks/cart/calculate.js
@@ -129,11 +129,11 @@ function calculateProductPrices(basket) {
         }
 
         // set price when product have salePriceBook 1953 Research creating a new promotion job to build sale Price book
-        productLineItem.setGrossPrice(productLineItem.basePrice);
         var adjustments = productLineItem.priceAdjustments;
         if (!adjustments.isEmpty() && productLineItem.product.priceModel.priceInfo.priceBook.ID.toLowerCase().indexOf(Constants.ECOM_SALE_PRICE_BOOK) > -1) {
             var adjustment = adjustments.iterator().next();
             adjustment.setPriceValue(0);
+            productLineItem.setGrossPrice(productLineItem.basePrice);
         }
 
         var product = productLineItem.product;
