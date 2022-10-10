08729271afff6e990f5160c0cffb540f3641010e
Anskhan <ans.qasim@innovadeltech.com>
2022-10-10 15:48:15 +0500

    MSS-1983-fixed-review-comments

--- oliviaburton_redesign/cartridges/app_custom_ob_redesign/cartridge/client/default/js/search/search-redesign.js
+++ oliviaburton_redesign/cartridges/app_custom_ob_redesign/cartridge/client/default/js/search/search-redesign.js
@@ -699,80 +699,80 @@ module.exports = {
         });
     },
 
-// Custom start: Listrak persistent popup
-listrakPersistentApply: function () {
-    $(document).on('click','.listrak-popup', function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        var isContainListrakPopup = e.target.closest('.listrak-popup');
-        var targetEl = e.target;
-        var isTargetContain = targetEl.classList.contains('close-icon-popup');
-        if (isContainListrakPopup && !isTargetContain) {
-            var listrakPersistenPopupUrl = document.querySelector('.listrak-persistent-url');
-            var url = listrakPersistenPopupUrl.dataset.listrakUrl;
-            $.ajax({
-                url: url,
-                method: 'GET',
-                success: function (response) {
-                    if (response.success == true) {
-                        var interval = setInterval(function() {
-                            if (typeof _ltk != "undefined" && typeof _ltk.Popup != "undefined") {
-                                _ltk.Popup.openManualByName(response.popupID);
-                                clearInterval(interval);
-                            }
-                        }, 1000);
+    // Custom start: Listrak persistent popup
+    listrakPersistentApply: function () {
+        $(document).on('click','.listrak-popup', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            var isContainListrakPopup = e.target.closest('.listrak-popup');
+            var targetEl = e.target;
+            var isTargetContain = targetEl.classList.contains('close-icon-popup');
+            if (isContainListrakPopup && !isTargetContain) {
+                var listrakPersistenPopupUrl = document.querySelector('.listrak-persistent-url');
+                var url = listrakPersistenPopupUrl.dataset.listrakUrl;
+                $.ajax({
+                    url: url,
+                    method: 'GET',
+                    success: function (response) {
+                        if (response.success == true) {
+                            var interval = setInterval(function() {
+                                if (typeof _ltk != "undefined" && typeof _ltk.Popup != "undefined") {
+                                    _ltk.Popup.openManualByName(response.popupID);
+                                    clearInterval(interval);
+                                }
+                            }, 1000);
+                        }
+                    },
+                    error: function () {
+                        $.spinner().stop();
                     }
-                },
-                error: function () {
-                    $.spinner().stop();
+                });
+            }
+        });
+    },
+
+    listrakPersistentClose: function () {
+        $(document).on('click','.close-icon-popup', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            var isContainListrakPopup = e.target.closest('.listrak-popup');
+            var targetEl = e.target;
+            var isTargetContain = targetEl.classList.contains('close-icon-popup');
+            if (isContainListrakPopup && isTargetContain) {
+                sessionStorage.setItem("listrakPersistenPopup", "false");
+                isContainListrakPopup.remove();
+            }
+        });
+    },
+
+    listrakPersistentCheckLoad: function () {
+        window.onload = () => {
+            var listrakPopup = document.querySelector('.listrak-popup');
+            var listrakPopupSearchResult = document.querySelector('.listrak-popup-search-result');
+            var listrakPopupProductDetail = document.querySelector('.listrak-popup-product-detail');
+            var data = sessionStorage.getItem("listrakPersistenPopup");
+            if (data == null) {
+                var isListrakPopupContain = listrakPopup.classList.contains('listrak-persistent-popup');
+            
+                if (isListrakPopupContain) {
+                    listrakPopup.classList.remove('listrak-persistent-popup');
                 }
-            });
-        }
-    });
-},
-
-listrakPersistentClose: function () {
-    $(document).on('click','.close-icon-popup', function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        var isContainListrakPopup = e.target.closest('.listrak-popup');
-        var targetEl = e.target;
-        var isTargetContain = targetEl.classList.contains('close-icon-popup');
-        if (isContainListrakPopup && isTargetContain) {
-            sessionStorage.setItem("listrakPersistenPopup", "false");
-            isContainListrakPopup.remove();
-        }
-    });
-},
-
-listrakPersistentCheckLoad: function () {
-    window.onload = () => {
-        var listrakPopup = document.querySelector('.listrak-popup');
-        var listrakPopupSearchResult = document.querySelector('.listrak-popup-search-result');
-        var listrakPopupProductDetail = document.querySelector('.listrak-popup-product-detail');
-        var data = sessionStorage.getItem("listrakPersistenPopup");
-        if (data == null) {
-            var isListrakPopupContain = listrakPopup.classList.contains('listrak-persistent-popup');
-        
-            if (isListrakPopupContain) {
-                listrakPopup.classList.remove('listrak-persistent-popup');
             }
-        }
-        if (listrakPopupSearchResult) {
-            var mediumWidth = 992;
-            var $windowWidth = $(window).width();
-            if ($windowWidth < mediumWidth) {
-                listrakPopup.classList.add('button-search-result');
+            if (listrakPopupSearchResult) {
+                var mediumWidth = 992;
+                var $windowWidth = $(window).width();
+                if ($windowWidth < mediumWidth) {
+                    listrakPopup.classList.add('button-search-result');
+                }
             }
-        }
-        if (listrakPopupProductDetail) {
-            var mediumWidth = 992;
-            var $windowWidth = $(window).width();
-            if ($windowWidth < mediumWidth) {
-                listrakPopup.classList.add('button-product-detail');
+            if (listrakPopupProductDetail) {
+                var mediumWidth = 992;
+                var $windowWidth = $(window).width();
+                if ($windowWidth < mediumWidth) {
+                    listrakPopup.classList.add('button-product-detail');
+                }
             }
-        }
-    };
-}
-// Custom End: Listrak persistent popup
+        };
+    }
+    // Custom End: Listrak persistent popup
 };
