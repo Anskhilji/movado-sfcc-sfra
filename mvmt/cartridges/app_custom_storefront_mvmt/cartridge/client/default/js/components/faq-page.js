4da0e7301093e8865e351646d1194e637985a535
INNOVADELTECH\ammar.sadiq <ammar.sadiq@innovadeltech.com>
2022-11-03 16:58:17 +0500

    MSS-1530-faq-page

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/components/faq-page.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/components/faq-page.js
@@ -44,6 +44,25 @@ $(document).ready(function() {
     }
 });
 
+$(window).on('resize', function() {
+    var $tabsURL = window.location.href;
+    if ($tabsURL.indexOf('#') > 0) {
+        var $URL = $tabsURL.split('#');
+        var $absoluteURLWithID = '#'+$URL[1];
+        $('.faq-nav-control-bar-link').each(function(value,element){
+            var $this = $(element);
+            var $mobileTabTitle;
+            
+            if ($this.attr('href') == $absoluteURLWithID) {
+                $mobileTabTitle = $this.attr('title');
+                if ($mobileTabTitle !== '' && $mobileTabTitle !== undefined) {
+                    $('.faq-nav-control-bar-btn').text($mobileTabTitle);
+                }
+            }
+        });
+    }
+});
+
 $(window).on('load', function() {
     var $generaltab = $('.faq-nav-control-bar-link');
     $('html, body').animate({ scrollTop: 0 }, "fast");
@@ -65,11 +84,7 @@ $(window).on('load', function() {
             }
         });
 
-        $('html, body').animate({ scrollTop: 0 }, "fast");
-        $('.tab-pane-control').addClass('d-none');
-        var $id = $($generaltab.attr('href'));
-        $id.removeClass('d-none');
-        
+        $('html, body').animate({ scrollTop: 0 }, "fast");        
         $('.faq-nav-control-bar-inner').removeClass('active');
         $('.faq-nav-control-bar-btn span').text($(this).text());
         var showHelpContainer = $generaltab.data('show-help');
@@ -110,6 +125,10 @@ module.exports = function () {
     });
     
     $('.faq-nav-control-bar-link').on('click', function() {
+        var $windowWidth = $(window).width();
+        if ($windowWidth <= 991) {
+            $('.faq-nav-control-bar-btn').text('');
+        }
         $('html, body').animate({ scrollTop: 0 }, "fast");
         $(this).siblings().removeClass('is-active');
         $(this).addClass('is-active');
@@ -118,7 +137,7 @@ module.exports = function () {
         $id.removeClass('d-none');
 
         $('.faq-nav-control-bar-inner').removeClass('active');
-        $('.faq-nav-control-bar-btn span').text($(this).text());
+        $('.faq-nav-control-bar-btn').text($(this).text());
         var showHelpContainer = $(this).data('show-help');
         if (showHelpContainer) {
             $helpContainer.removeClass('d-none');
