ead99b7dab7a7b5336b7e988d6482cd2c3d0c4f3
muhammad_yasir_bashir <muhammad.yasir@innovadeltech.com>
2022-12-16 00:07:01 +0500

    MSS-2026 Optimize Code

--- movado/cartridges/app_custom_movado/cartridge/controllers/Cart.js
+++ movado/cartridges/app_custom_movado/cartridge/controllers/Cart.js
@@ -459,12 +459,9 @@ server.replace(
         var CartModel = require('*/cartridge/models/cart');
         var basketCalculationHelpers = require('*/cartridge/scripts/helpers/basketCalculationHelpers');
         var Site = require('dw/system/Site');
-        var TotalsModel = require('*/cartridge/models/totals');
-
         var couponErrorMessages = !empty(Site.current.preferences.custom.couponErrorMessages) ? Site.current.preferences.custom.couponErrorMessages : false;
 
         var currentBasket = BasketMgr.getCurrentBasket();
-        var totalsModel = new TotalsModel(currentBasket);
 
         if (!currentBasket) {
             res.setStatusCode(500);
@@ -515,19 +512,18 @@ server.replace(
             }
         }
 
-
-
         Transaction.wrap(function () {
             basketCalculationHelpers.calculateTotals(currentBasket);
         });
 
         var basketModel = new CartModel(currentBasket);
 
-        if (!empty(basketModel.totals)) {
+        if (!empty(basketModel) && !empty(basketModel.couponLineItems)) {
             var couponLineItems = basketModel.couponLineItems;
             for (var i = 0; i < couponLineItems.length; i++) {
                 var couponLineItem = couponLineItems[i];
-                if (couponLineItem.applied === false) { 
+
+                if (couponLineItem && couponLineItem.applied === false) { 
                     error = true;
     
                     Transaction.wrap(function () {
@@ -538,11 +534,10 @@ server.replace(
                     if (couponErrorMessages) {
                         var errorCodes = JSON.parse(couponErrorMessages);
                         var localeErrorCodes = errorCodes[req.locale.id] || errorCodes['default'];
-                        var errorMessage = 'coupon cannot applied';
+                        var errorMessage = localeErrorCodes.COUPON_NOT_APPLIED || localeErrorCodes.DEFAULT;
                     }
                 }
             }
-            
         }
 
         if (error) {
