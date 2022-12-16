21e01c5f03ac2dc70f0fb2d22a6de4ba1c56f285
Anskhan <ans.qasim@innovadeltech.com>
2022-12-16 19:37:43 +0500

    MSS-2092-fixed-review-comment

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/base.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/base.js
@@ -951,7 +951,7 @@ function handleVariantResponse(response, $productContainer) {
 
             for (var i = 0; i < response.product.promotions.length; i++) {
                 if (response.product.promotions[i].promotionBadge == true && response.product.promotions[i].promotionMsg !== '') {
-                    $exclusiveBadges.prepend('<span class="badge custom-promotion-badge badge-bg text-uppercase hide-plp ">' + response.product.promotions[i].promotionMsg + '</span>');
+                    $exclusiveBadges.prepend('<span class="badge custom-promotion-badge badge-bg text-uppercase hide-plp">' + response.product.promotions[i].promotionMsg + '</span>');
                 }
             }
         }
