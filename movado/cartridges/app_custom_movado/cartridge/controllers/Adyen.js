983f3b5e248965a7bdcffc9ba61bf7b65d73ec8d
muhammad_yasir_bashir <muhammad.yasir@innovadeltech.com>
2022-12-20 20:20:51 +0500

    MSS-1653 Added Loggers for AmazonPay

--- movado/cartridges/app_custom_movado/cartridge/controllers/Adyen.js
+++ movado/cartridges/app_custom_movado/cartridge/controllers/Adyen.js
@@ -46,6 +46,13 @@ server.replace('Redirect', server.middleware.https, function (req, res, next) {
             order.orderNo,
             order.paymentInstrument,
             require('*/cartridge/scripts/hooks/fraudDetectionHook').checkoutCreate);
+
+            var email = order.customerEmail;
+            if (!empty(email)) {
+                var maskedEmail = checkoutCustomHelpers.maskEmail(email);
+                checkoutLogger.info('(Adyen) -> SubmitPayment: Step-2: Customer Email is ' + maskedEmail);
+            }
+
         if (!riskifiedCheckoutCreateResponse) {
             checkoutLogger.error('Riskified API Call failed for order number: ' + order.orderNo);
             res.render('error', {
@@ -277,6 +284,13 @@ server.replace('ShowConfirmation', server.middleware.https, function (req, res, 
 
         checkoutLogger.debug('(Adyen) -> ShowConfirmation: Going to send the order confirmation email to the user and order number is: ' + orderNumber);
         COCustomHelpers.sendConfirmationEmail(order, req.locale.id);
+
+        var email = order.customerEmail;
+        if (!empty(email)) {
+            var maskedEmail = COCustomHelpers.maskEmail(email);
+            checkoutLogger.info('(Adyen) -> PlaceOrder: Step-3: Customer Email is ' + maskedEmail);
+        }
+
         checkoutLogger.debug('(Adyen) -> ShowConfirmation: Going to the order confirmation page and order number is: ' + orderNumber);
         res.redirect(URLUtils.url('Order-Confirm', 'ID', order.orderNo, 'token', order.orderToken).toString());
         session.custom.klarnaRiskifiedFlag = '';
