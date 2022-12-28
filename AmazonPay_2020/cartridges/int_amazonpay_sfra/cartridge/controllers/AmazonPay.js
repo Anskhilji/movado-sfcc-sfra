382b6f0a716b16e4c8686c47525f651e3bb7f59d
muhammad_yasir_bashir <muhammad.yasir@innovadeltech.com>
2022-12-28 17:16:13 +0500

    MSS-2056 Amazon-Pay-total-authorization

--- AmazonPay_2020/cartridges/int_amazonpay_sfra/cartridge/controllers/AmazonPay.js
+++ AmazonPay_2020/cartridges/int_amazonpay_sfra/cartridge/controllers/AmazonPay.js
@@ -1345,9 +1345,9 @@ server.get('UpdateAmazonPayCheckout', function (req, res, next) {
         Transaction.wrap(function () {
             
             if (currentBasket) {
-                if (currentBasket.custom.AmzPaysabrixTaxError) { //if sabrix call fails
+                if (currentBasket.custom.AmzPaySabrixTaxError) { //if sabrix call fails
                     HookMgr.callHook('dw.order.calculateTax', 'calculateTax', currentBasket);
-                    currentBasket.custom.AmzPaysabrixTaxError = false;
+                    currentBasket.custom.AmzPaySabrixTaxError = false;
                 }
             }
 
