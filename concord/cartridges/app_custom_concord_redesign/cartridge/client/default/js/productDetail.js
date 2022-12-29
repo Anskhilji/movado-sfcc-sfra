f9e12072e1cb6ee8884f282c6f006579e27472e4
muhammad_yasir_bashir <muhammad.yasir@innovadeltech.com>
2022-12-29 20:19:52 +0500

    MSS-2014-movado-pdp-remove-thumbs-and-arrows

--- concord/cartridges/app_custom_concord_redesign/cartridge/client/default/js/productDetail.js
+++ concord/cartridges/app_custom_concord_redesign/cartridge/client/default/js/productDetail.js
@@ -2,6 +2,6 @@ var processInclude = require('base/util');
 processInclude(require('movado/productDetail'));
 
 $(document).ready(function () {
-    processInclude(require('./product/zoom'));
+    processInclude(require('movad_redesign/product/zoom'));
     processInclude(require('./product/productDetailCustom'));
 }); 
