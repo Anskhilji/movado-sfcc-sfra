9767ab52e1105e394ab8125e606f8e818a7affb8
ansKhan <ans.qasim@innovadeltech.com>
2023-01-10 19:41:12 +0500

    feature/MSS-2116-movado-mcs-obus-obuk---add-to-cart

--- movado/cartridges/app_custom_movado/cartridge/client/default/js/utilities/gtm-events.js
+++ movado/cartridges/app_custom_movado/cartridge/client/default/js/utilities/gtm-events.js
@@ -133,21 +133,23 @@ var onPDPAddProductClickEvent = function () {
             var addtoCartData = JSON.parse(data);
             dataLayer.push({
                 event: 'addToCart',
-                ecommerce: { currencyCode: addtoCartData.currency,
-                    add: { actionField: { list: addtoCartData.list },
-				      products: [{
-				          id: addtoCartData.id,
-				          name: addtoCartData.name,
-				          price: addtoCartData.price,
-				          category: addtoCartData.category,
-				          variant: addtoCartData.variant,
-                          currency: addtoCartData.currency,
-                          brand: addtoCartData.brand,
-                          quantity: addtoCartData.quantity,
-                          deparmentIncludedSubCategory: addtoCartData.deparmentIncludedCategoryName,
-                          discountPrice: addtoCartData.discountPrice,
-				          addToCartLocation: addtoCartData.addToCartLocation
-				      }]
+                ecommerce: {
+                    currencyCode: addtoCartData.currency,
+                    add: {
+                        actionField: { list: addtoCartData.list },
+                        products: [{
+                            id: addtoCartData.id,
+                            name: addtoCartData.name,
+                            price: addtoCartData.price,
+                            category: addtoCartData.category,
+                            variant: addtoCartData.variant,
+                            currency: addtoCartData.currency,
+                            brand: addtoCartData.brand,
+                            quantity: addtoCartData.quantity,
+                            deparmentIncludedSubCategory: addtoCartData.deparmentIncludedCategoryName,
+                            discountPrice: addtoCartData.discountPrice,
+                            addToCartLocation: addtoCartData.addToCartLocation
+                        }]
                     }
                 }
             });
@@ -156,20 +158,22 @@ var onPDPAddProductClickEvent = function () {
             var addtoCartData = JSON.parse(data);
             dataLayer.push({
                 event: 'addToCart',
-                ecommerce: { currencyCode: addtoCartData.currency,
-                    add: { actionField: { list: addtoCartData.list },
-				      products: [{
-				          id: addtoCartData.id,
-				          name: addtoCartData.name,
-				          price: addtoCartData.price,
-				          category: addtoCartData.category,
-				          variant: addtoCartData.variant,
-                          currency: addtoCartData.currency,
-                          brand: addtoCartData.brand,
-                          deparmentIncludedSubCategory: addtoCartData.deparmentIncludedCategoryName,
-                          discountPrice: addtoCartData.discountPrice,
-				          addToCartLocation: addtoCartData.addToCartLocation
-				      }]
+                ecommerce: {
+                    currencyCode: addtoCartData.currency,
+                    add: {
+                        actionField: { list: addtoCartData.list },
+                        products: [{
+                            id: addtoCartData.id,
+                            name: addtoCartData.name,
+                            price: addtoCartData.price,
+                            category: addtoCartData.category,
+                            variant: addtoCartData.variant,
+                            currency: addtoCartData.currency,
+                            brand: addtoCartData.brand,
+                            deparmentIncludedSubCategory: addtoCartData.deparmentIncludedCategoryName,
+                            discountPrice: addtoCartData.discountPrice,
+                            addToCartLocation: addtoCartData.addToCartLocation
+                        }]
                     }
                 }
             });
