1a50c15972edfe25b12aea24ee856262b1fb7226
Anskhan <ans.qasim@innovadeltech.com>
2023-01-16 19:30:03 +0500

    MSS-1953-missing-factory-file-added

--- storefront-reference-architecture/cartridges/modules/server/request.js
+++ storefront-reference-architecture/cartridges/modules/server/request.js
@@ -323,37 +323,41 @@ function Request(request, customer, session) {
         //Custom End
 
         //Custom Start: Set applicable sale pricebook only on plp urls
-        var salePriceBooks = JSON.parse(dw.system.Site.getCurrent().getCustomPreferenceValue('promotionalPriceBookConverstion'));
+        var salePriceBooks = !empty(dw.system.Site.getCurrent().getCustomPreferenceValue('promotionalPriceBookConverstion')) ? JSON.parse(dw.system.Site.getCurrent().getCustomPreferenceValue('promotionalPriceBookConverstion')) : false;
+
         var applicablePricebooks;
         var filterPriceBooks;
         var localizeSalePriceBooks;
         var localizeBasePriceBooks;
-        salePriceBooks.forEach(function (localizeObj) {
-            localizeSalePriceBooks = localizeObj.promotionalConversion.sale_pricebook;
-            localizeBasePriceBooks = localizeObj.promotionalConversion.base_pricebook;
-        });
 
-        if (!empty(localizeSalePriceBooks) || !empty(localizeBasePriceBooks)) {
-            var productShow = URLUtils.url('Search-Show').toString();
-            var updateGrid = URLUtils.url('Search-UpdateGrid').toString();
-
-            if (request.httpPath == productShow || request.httpPath == updateGrid) {
-                applicablePricebooks = PriceBookMgr.applicablePriceBooks.toArray();
-                filterPriceBooks = applicablePricebooks.filter(function(data) {
-                    return data.ID == localizeSalePriceBooks;
-                });
-                if (!filterPriceBooks) {
-                    PriceBookMgr.setApplicablePriceBooks(localizeSalePriceBooks);
+        if (salePriceBooks) {
+            salePriceBooks.forEach(function (localizeObj) {
+                localizeSalePriceBooks = localizeObj.promotionalConversion.sale_pricebook;
+                localizeBasePriceBooks = localizeObj.promotionalConversion.base_pricebook;
+            });
+    
+            if (!empty(localizeSalePriceBooks) || !empty(localizeBasePriceBooks)) {
+                var productShow = URLUtils.url('Search-Show').toString();
+                var updateGrid = URLUtils.url('Search-UpdateGrid').toString();
+    
+                if (request.httpPath == productShow || request.httpPath == updateGrid) {
+                    applicablePricebooks = PriceBookMgr.applicablePriceBooks.toArray();
+                    filterPriceBooks = applicablePricebooks.filter(function(data) {
+                        return data.ID == localizeSalePriceBooks;
+                    });
+                    if (!filterPriceBooks) {
+                        PriceBookMgr.setApplicablePriceBooks(localizeSalePriceBooks);
+                    }
+                } else {
+                    applicablePricebooks = PriceBookMgr.applicablePriceBooks.toArray();
+                    filterPriceBooks = applicablePricebooks.filter(function(data) {
+                        return data.ID !== localizeSalePriceBooks;
+                    });
+    
+                    PriceBookMgr.setApplicablePriceBooks(filterPriceBooks);
                 }
-            } else {
-                applicablePricebooks = PriceBookMgr.applicablePriceBooks.toArray();
-                filterPriceBooks = applicablePricebooks.filter(function(data) {
-                    return data.ID !== localizeSalePriceBooks;
-                });
-
-                PriceBookMgr.setApplicablePriceBooks(filterPriceBooks);
+                 
             }
-             
         }
         //Custom End
 
