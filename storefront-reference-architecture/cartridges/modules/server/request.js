110b1afd9ffec823785b247a52cf2c52d2298722
Anskhan <ans.qasim@innovadeltech.com>
2023-01-16 20:54:25 +0500

    MSS-1953-fixed-review-comments

--- storefront-reference-architecture/cartridges/modules/server/request.js
+++ storefront-reference-architecture/cartridges/modules/server/request.js
@@ -323,14 +323,14 @@ function Request(request, customer, session) {
         //Custom End
 
         //Custom Start: Set applicable sale pricebook only on plp urls
-        var salePriceBooks = !empty(dw.system.Site.getCurrent().getCustomPreferenceValue('promotionalPriceBookConverstion')) ? JSON.parse(dw.system.Site.getCurrent().getCustomPreferenceValue('promotionalPriceBookConverstion')) : '';
+        var salePriceBooks = !empty(dw.system.Site.current.preferences.custom.promotionalPriceBookConverstion) ? JSON.parse(dw.system.Site.current.preferences.custom.promotionalPriceBookConverstion) : false;
 
         var applicablePricebooks;
         var filterPriceBooks;
         var localizeSalePriceBooks;
         var localizeBasePriceBooks;
 
-        if (!empty(salePriceBooks)) {
+        if (salePriceBooks) {
             salePriceBooks.forEach(function (localizeObj) {
                 localizeSalePriceBooks = localizeObj.promotionalConversion.sale_pricebook;
                 localizeBasePriceBooks = localizeObj.promotionalConversion.base_pricebook;
