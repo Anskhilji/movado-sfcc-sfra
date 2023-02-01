440905c2d2e7d1f9d06b50a19476f7a5b0c0ff1f
alihiader786 <ali.haider@innovadeltech.com>
2023-02-01 15:54:36 +0500

    MSS_2093 QA issues fixed

--- mcsus/cartridges/app_custom_storefront_mcsus/cartridge/client/default/js/productDetail.js
+++ mcsus/cartridges/app_custom_storefront_mcsus/cartridge/client/default/js/productDetail.js
@@ -15,3 +15,9 @@ $('.ratings > .yotpoBottomLine').on('click', function () {
     }, 10);
 });
 
+// 2093 by default open accordion when clicked on plp product rating
+var myid = '#yotpo-reviews-top-div'
+if(location.hash == myid) {
+    $(".reviews-accordion").removeClass('collapsed');
+    $("#reviews").addClass('show');
+}
