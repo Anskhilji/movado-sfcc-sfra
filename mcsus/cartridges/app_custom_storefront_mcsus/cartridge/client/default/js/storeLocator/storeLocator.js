071c9062d5c49e3ab8b81f3e9c4e26758da9b00f
Ali Haider <ali.haider@innovadeltech.com>
2023-02-02 22:16:15 +0500

    MSS-2137 UI completed click event add

--- mcsus/cartridges/app_custom_storefront_mcsus/cartridge/client/default/js/storeLocator/storeLocator.js
+++ mcsus/cartridges/app_custom_storefront_mcsus/cartridge/client/default/js/storeLocator/storeLocator.js
@@ -10,3 +10,34 @@ $('.store-sidebar-link').click(function(){
     $('.store-sidebar').addClass('show');
     $('.store-Locator-overlayer').addClass('d-block');
 });
+
+$('.store-sidebar-header-close').click(function(){
+    $('.store-sidebar').removeClass('show');
+    $('.store-Locator-overlayer').removeClass('d-block');
+});
+
+$('.filter-button').click(function(){
+    $('.store-sidebar').addClass('hide-scroll');
+    $('.radius-sidebar').addClass('show');
+});
+
+$('.radius-sidebar-header-title').click(function(){
+    $('.store-sidebar').removeClass('hide-scroll');
+    $('.radius-sidebar').removeClass('show');
+});
+
+$('.store-Locator-overlayer').click(function(){
+    $('.radius-sidebar').removeClass('show');
+    $('.store-sidebar').removeClass('hide-scroll');
+    $('.store-sidebar').removeClass('show');
+    $('.store-Locator-overlayer').removeClass('d-block');
+});
+
+
+$('.store-sidebar-card .select-time').each(function() {
+    $(this).on('click', function() {
+        $(this).siblings('.morecontent-wrapper').css('display', 'inline');
+
+        $('.select-time-dropdwon').toggleClass('show')
+    });
+});
