37c9ce192bd0f73dc8e56a345989ab2ec909bd75
Ali Haider <ali.haider@innovadeltech.com>
2023-02-13 16:35:46 +0500

    MSS-2150 issue fixed

--- movado/cartridges/app_custom_movado/cartridge/client/default/js/components/menu.js
+++ movado/cartridges/app_custom_movado/cartridge/client/default/js/components/menu.js
@@ -9,20 +9,6 @@ var clearSelection = function (element) {
     $(element).closest('.dropdown.show').removeClass('show');
     $(element).closest('li').detach();
 };
-    
-
-// Custom:MSS-2150 Apply the .header-banner and .header-wrapper height to the sticky header placeholder as an inline style
-function updateStickyHeaderHeight() {
-        var $headerBannerHeight = $('.header-banner').length > 0 ? $('.header-banner').height() : 0;
-        var $headerWrapperHeight = $('.header-wrapper').length > 0 ? $('.header-wrapper').height() : 0;   
-        var $totalHeight = $headerBannerHeight + $headerWrapperHeight;
-        $('.sticky-header-placeholder').css('height', $totalHeight + 'px');
-}
-
-// Custom:MSS-2150 Call the updateStickyHeaderHeight function both when the document is ready and when the screen is resized
-$(window).resize(function(){
-    updateStickyHeaderHeight();
-});
 
 $(document).ready(function() {
     var visitAccessibility = true;
@@ -36,9 +22,6 @@ $(document).ready(function() {
             visitAccessibility = false;
         }
     });
-
-// Custom:MSS-2150 Call the updateStickyHeaderHeight function both when the document is ready
-    updateStickyHeaderHeight();
 });
 
 module.exports = function () {
