8d539c365777bec75909d681a0d7afc05663110c
Ali Haider <ali.haider@innovadeltech.com>
2023-03-07 21:26:16 +0500

    MSS-2188 sticky  header issue fixed

--- movado/cartridges/app_custom_movado/cartridge/client/default/js/components/menu.js
+++ movado/cartridges/app_custom_movado/cartridge/client/default/js/components/menu.js
@@ -13,10 +13,13 @@ var clearSelection = function (element) {
 
 // Custom:MSS-2150 Apply the .header-banner and .header-wrapper height to the sticky header placeholder as an inline style
 function updateStickyHeaderHeight() {
-        var $headerBannerHeight = $('.tophat-sticky .header-banner').length > 0 ? $('.header-banner').height() : 0;
+        var $headerBannerHeight = $('.header-banner.slide-up').length > 0 ? $('.header-banner').height() : 0;
         var $headerWrapperHeight = $('.tophat-sticky .header-wrapper').length > 0 ? $('.header-wrapper').height() : 0;   
         var $totalHeight = $headerBannerHeight + $headerWrapperHeight;
-        $('.tophat-sticky .sticky-header-placeholder').css('height', $totalHeight + 'px');
+        console.log($headerBannerHeight + 'height 1');
+        console.log($headerWrapperHeight + 'height 2');
+        console.log($totalHeight + 'height 3');
+        $('.header-placeholder-sec').css('height', $totalHeight + 'px');
 }
 
 // Custom:MSS-2150 Call the updateStickyHeaderHeight function both when the document is ready and when the screen is resized
