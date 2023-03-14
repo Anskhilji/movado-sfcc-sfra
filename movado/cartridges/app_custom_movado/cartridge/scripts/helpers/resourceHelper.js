530d7e4bdc67b7a80e90524b430fe5fb19f38b52
Anskhan <ans.qasim@innovadeltech.com>
2023-03-14 15:45:39 +0500

    MSS-2140-fixed-QA -Reported-Issue

--- movado/cartridges/app_custom_movado/cartridge/scripts/helpers/resourceHelper.js
+++ movado/cartridges/app_custom_movado/cartridge/scripts/helpers/resourceHelper.js
@@ -81,6 +81,7 @@ function getResources(pageContext) {
         FEDEX_RECOMMENDED_ADDRESS_MESSAGE: Resource.msg('popup.label.content.sub', 'checkout', null),
         INFO_PRODUCT_AVAILABILITY_PREORDER: Resource.msg('info.product.availability.preorder', 'common', null),
         INFO_PRODUCT_AVAILABILITY_BACK_ORDER: Resource.msg('info.product.availability.backorder', 'common', null),
+        BUTTON_ADD_TO_CART: Resource.msg('button.addtocart', 'common', null),
         BUTTON_PREORDER_NOW: Resource.msg('button.preorder.now', 'common', null)
     };
     return resources;
