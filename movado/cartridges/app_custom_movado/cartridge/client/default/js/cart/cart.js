43e202bfa00c8b1d03cc11f0135f0e375d81cd43
muhammad_yasir_bashir <muhammad.yasir@innovadeltech.com>
2023-03-15 21:29:50 +0500

    MSS-2172 Fixed Review Comments

--- movado/cartridges/app_custom_movado/cartridge/client/default/js/cart/cart.js
+++ movado/cartridges/app_custom_movado/cartridge/client/default/js/cart/cart.js
@@ -998,24 +998,24 @@ module.exports = function () {
     });
 
     $('body').on('click', '.checkout-btn, .paypal-btn', function (e) {
-        var applyButtton = $('.add-gift-message');
-        var errorMsg = Resources.GIFT_MESSAGE_CART_ERROR;
-        var giftMessageArray = [];
+        var $applyButtton = $('.add-gift-message');
+        var $errorMsg = Resources.GIFT_MESSAGE_CART_ERROR;
+        var $giftMessageArray = [];
 
-        applyButtton.each(function () {
-            var giftMessageApply = $(this).find('.gift-message-apply').val();
-            giftMessageArray.push(giftMessageApply);
+        $applyButtton.each(function () {
+            var $giftMessageApply = $(this).find('.gift-message-apply').val();
+            $giftMessageArray.push($giftMessageApply);
         });
 
-        if (giftMessageArray && giftMessageArray.indexOf('false') > -1) {
-            var errorHtml = '<div class="alert card alert-dismissible ' +
+        if ($giftMessageArray && $giftMessageArray.indexOf('false') > -1) {
+            var $errorHtml = '<div class="alert card alert-dismissible ' +
             'fade show" role="alert">' +
             '<button type="button" class="close" style="top: -4px"data-dismiss="alert" aria-label="Close">' +
             '<span aria-hidden="true">&times;</span>' +
-            '</button>' + errorMsg + '</div>';
+            '</button>' + $errorMsg + '</div>';
 
-            if (errorMsg) {
-                $('.cart-error').empty().append(errorHtml);
+            if ($errorMsg) {
+                $('.cart-error').empty().append($errorHtml);
             }
             e.stopPropagation();
             e.preventDefault();
