ac29e36bb6dab619440ff106372fa233ac07cc8f
Bilal Javed <bilal.javed@innovadeltech.com>
2023-04-03 12:02:38 +0000

    Revert "MSS-1397 All Brands Carousel PDP Video Phase 2 (pull request #3622)"

--- mvmt/cartridges/app_custom_core_mvmt/cartridge/models/product/fullProduct.js
+++ mvmt/cartridges/app_custom_core_mvmt/cartridge/models/product/fullProduct.js
@@ -154,13 +154,6 @@ module.exports = function fullProduct(product, apiProduct, options) {
         });
     }
 
-    if (!empty(product)) {
-        Object.defineProperty(product, 'posterFrame', {
-            enumerable: true,
-            value: product.images.posterFrame[0] ? product.images.posterFrame[0] : ''
-        });
-    }
-
     if (!empty(masterProductID)) {
         Object.defineProperty(product, 'masterProductID', {
             enumerable: true,
