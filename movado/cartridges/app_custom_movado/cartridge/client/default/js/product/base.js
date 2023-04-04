42bc24b3572ca42c7a3abf136e537b48f4beba77
muhammad_yasir_bashir <muhammad.yasir@innovadeltech.com>
2023-04-04 16:20:12 +0500

    MSS-2187-movado-clyde-new-add-to-cart-popup-model

--- movado/cartridges/app_custom_movado/cartridge/client/default/js/product/base.js
+++ movado/cartridges/app_custom_movado/cartridge/client/default/js/product/base.js
@@ -1119,6 +1119,121 @@ function validateOptions($el) {
     }
 }
 
+function clydeAddProductToCart() {
+        var addToCartUrl;
+        var pid;
+        var pidsObj;
+        var setPids;
+        var giftPid;
+
+        $('body').trigger('product:beforeAddToCart', this);
+
+        if ($('.set-items').length && $(this).hasClass('add-to-cart-global')) {
+            setPids = [];
+
+            $('.product-detail').each(function () {
+                if (!$(this).hasClass('product-set-detail')) {
+                    setPids.push({
+                        pid: $(this).find('.product-id').text(),
+                        qty: $(this).find('.quantity-select').val(),
+                        options: getOptions($(this))
+                    });
+                }
+            });
+            pidsObj = JSON.stringify(setPids);
+        }
+
+        if ($(this).closest('.product-detail') && $(this).closest('.product-detail').data('isplp') == true) {
+            pid = $(this).data('pid');
+            if ($('.gift-allowed-checkbox').is(":checked")) {
+                giftPid = $('.gift-allowed-checkbox').val();
+            }
+        } else {
+            pid = getPidValue($(this));
+            if ($('.gift-allowed-checkbox').is(":checked")) {
+                giftPid = $('.gift-allowed-checkbox').val();
+            }
+        }
+
+        var $productContainer = $(this).closest('.product-detail');
+        if (!$productContainer.length) {
+            $productContainer = $(this).closest('.quick-view-dialog').find('.product-detail');
+        }
+
+        addToCartUrl = getAddToCartUrl();
+
+        var form = {
+            pid: pid,
+            pidsObj: pidsObj,
+            childProducts: getChildProducts(),
+            quantity: getQuantitySelected($(this)),
+            giftPid: giftPid ? giftPid : ''
+        };
+
+        /**
+         * Custom Start: Add to cart form for MVMT
+         */
+        if ($('.pdp-mvmt')) {
+            form = {
+                pid: pid,
+                pidsObj: pidsObj,
+                childProducts: getChildProducts(),
+                quantity: 1,
+                giftPid: giftPid ? giftPid : ''
+            };
+        }
+        /**
+         *  Custom End
+         */
+
+        /**
+         * Custom Start: Clyde Integration
+         */
+        if (window.Resources && window.Resources.IS_CLYDE_ENABLED) {
+            form = clydeWidget.getSelectedClydeContract(form);
+        }
+        /**
+         * Custom end:
+         */
+        $productContainer.find('input[type="text"], textarea').filter('[required]')
+        .each(function() {
+            if($(this).val() && $(this).closest("form.submitted").length) {
+                Object.assign(form, {
+                    [$(this).data('name')]: $(this).val()
+                });
+            }
+        });
+
+        if (!$('.bundle-item').length) {
+            form.options = getOptions($productContainer);
+        }
+        form.currentPage = $('.page[data-action]').data('action') || '';
+        $(this).trigger('updateAddToCartFormData', form);
+        if (addToCartUrl) {
+            $.ajax({
+                url: addToCartUrl,
+                method: 'POST',
+                data: form,
+                success: function (data) {
+                    updateCartPage(data);
+                    handlePostCartAdd(data);
+                    $('body').trigger('product:afterAddToCart', data);
+                    $.spinner().stop();
+                    //Custom Start: [MSS-1451] Listrak SendSCA on AddToCart
+                    if (window.Resources.LISTRAK_ENABLED) {
+                        var ltkSendSCA = require('listrak_custom/ltkSendSCA');
+                        ltkSendSCA.renderSCA(data.SCACart, data.listrakCountryCode);
+                    }
+                    //Custom End
+                    $(window).resize(); // This is used to fix zoom feature after add to cart
+                },
+                error: function () {
+                    $.spinner().stop();
+                }
+            });
+        }
+}
+
 var updateCartPage = function(data) {
   $('.cart-section-wrapper').html(data.cartPageHtml);
   if (Resources.AFFIRM_PAYMENT_METHOD_STATUS) {
@@ -1220,119 +1335,31 @@ module.exports = {
 
     addToCart: function () {
         $(document).off('click.addToCart').on('click.addToCart', 'button.add-to-cart, button.add-to-cart-global', function (e) {
-            var addToCartUrl;
-            var pid;
-            var pidsObj;
-            var setPids;
-            var giftPid;
-
-            $('body').trigger('product:beforeAddToCart', this);
-
-            if ($('.set-items').length && $(this).hasClass('add-to-cart-global')) {
-                setPids = [];
-
-                $('.product-detail').each(function () {
-                    if (!$(this).hasClass('product-set-detail')) {
-                        setPids.push({
-                            pid: $(this).find('.product-id').text(),
-                            qty: $(this).find('.quantity-select').val(),
-                            options: getOptions($(this))
-                        });
-                    }
-                });
-                pidsObj = JSON.stringify(setPids);
-            }
-
-            if ($(this).closest('.product-detail') && $(this).closest('.product-detail').data('isplp') == true) {
-                pid = $(this).data('pid');
-                if ($('.gift-allowed-checkbox').is(":checked")) {
-                    giftPid = $('.gift-allowed-checkbox').val();
+            var clydeWidgets = ClydeSitePreferences.CLYDE_WIDGET_ENABLED;
+            if (clydeWidgets) {
+                var selectedContract = Clyde.getSelectedContract();
+                var clydeSettings = Clyde.getSettings();
+                if (clydeSettings.productPage == true) {
+                     if (selectedContract) {
+                         clydeAddProductToCart();
+                     } else {
+                         var product = Clyde.getActiveProduct();
+                         var hasContracts = product && product.contracts ? product.contracts.length > 0 : false;
+                         if (hasContracts && clydeSettings.modal == true) {
+                             Clyde.showModal(null, clydeAddProductToCart);
+                         } else {
+                             clydeAddProductToCart();
+                         }
+                     }
+                } else if (clydeSettings.modal == true) {
+                     Clyde.showModal(null, clydeAddProductToCart);
+                } else {
+                     clydeAddProductToCart()
                 }
             } else {
-            	pid = getPidValue($(this));
-                if ($('.gift-allowed-checkbox').is(":checked")) {
-                    giftPid = $('.gift-allowed-checkbox').val();
-                }
+                 clydeAddProductToCart();
             }
-
-            var $productContainer = $(this).closest('.product-detail');
-            if (!$productContainer.length) {
-                $productContainer = $(this).closest('.quick-view-dialog').find('.product-detail');
-            }
-
-            addToCartUrl = getAddToCartUrl();
-
-            var form = {
-                pid: pid,
-                pidsObj: pidsObj,
-                childProducts: getChildProducts(),
-                quantity: getQuantitySelected($(this)),
-                giftPid: giftPid ? giftPid : ''
-            };
-
-            /**
-             * Custom Start: Add to cart form for MVMT
-             */
-            if ($('.pdp-mvmt')) {
-                form = {
-                    pid: pid,
-                    pidsObj: pidsObj,
-                    childProducts: getChildProducts(),
-                    quantity: 1,
-                    giftPid: giftPid ? giftPid : ''
-                };
-            }
-            /**
-             *  Custom End
-             */
-
-            /**
-             * Custom Start: Clyde Integration
-             */
-            if (window.Resources && window.Resources.IS_CLYDE_ENABLED) {
-                form = clydeWidget.getSelectedClydeContract(form);
-            }
-            /**
-             * Custom end:
-             */
-            $productContainer.find('input[type="text"], textarea').filter('[required]')
-            .each(function() {
-                if($(this).val() && $(this).closest("form.submitted").length) {
-                    Object.assign(form, {
-                        [$(this).data('name')]: $(this).val()
-                    });
-                }
-            });
-
-            if (!$('.bundle-item').length) {
-                form.options = getOptions($productContainer);
-            }
-            form.currentPage = $('.page[data-action]').data('action') || '';
-            $(this).trigger('updateAddToCartFormData', form);
-            if (addToCartUrl) {
-                $.ajax({
-                    url: addToCartUrl,
-                    method: 'POST',
-                    data: form,
-                    success: function (data) {
-                        updateCartPage(data);
-                        handlePostCartAdd(data);
-                        $('body').trigger('product:afterAddToCart', data);
-                        $.spinner().stop();
-                        //Custom Start: [MSS-1451] Listrak SendSCA on AddToCart
-                        if (window.Resources.LISTRAK_ENABLED) {
-                            var ltkSendSCA = require('listrak_custom/ltkSendSCA');
-                            ltkSendSCA.renderSCA(data.SCACart, data.listrakCountryCode);
-                        }
-                        //Custom End
-                        $(window).resize(); // This is used to fix zoom feature after add to cart
-                    },
-                    error: function () {
-                        $.spinner().stop();
-                    }
-                });
-            }
-        });
+         });
     },
     selectBonusProduct: function () {
         $(document).on('click', '.select-bonus-product', function () {
