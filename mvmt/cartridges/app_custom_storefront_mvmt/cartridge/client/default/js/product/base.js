1a43418767b1eee84405246fd222ea4946d27410
Ali Haider <ali.haider@innovadeltech.com>
2023-05-10 18:06:55 +0500

    MSS-2231 issue fixed

--- mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/base.js
+++ mvmt/cartridges/app_custom_storefront_mvmt/cartridge/client/default/js/product/base.js
@@ -221,7 +221,7 @@ function openMiniCart() {
 function updateCartIcons() {
     var $cartItems = $('.cart-quantity-items').data('quantity-id');
     var $cartIcon = $('.cart-icon');
-    var $cartIcount = $('.add-to-cart');
+    var $cartIcount = $('.cart-counter');
     if ($cartItems !== undefined && $cartItems !== 0) {
         $cartIcon.addClass('fill-cart-icon');
         $cartIcount.addClass('fill-cart-count');
