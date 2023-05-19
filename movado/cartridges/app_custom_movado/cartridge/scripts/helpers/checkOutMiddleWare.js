6ddd69ef059ffeeae79eb3ba6117e5fa3ccf678d
Muhammad Shahbaz <muhammad.shehbaz@innovadeltech.com>
2023-05-19 21:15:09 +0500

    MSS-2273 - All Brands - Prevent checkout with specific Product Options

---
+++ movado/cartridges/app_custom_movado/cartridge/scripts/helpers/checkOutMiddleWare.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function (req, res, next) {
+    var BasketMgr = require('dw/order/BasketMgr').getCurrentBasket();
+    // if  basket has a USD set rate
+    //set flag
+
+    //basket has Engrave or clyde
+    //return error
+}
