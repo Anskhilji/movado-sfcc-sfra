35775cd4f5ac48edee1a20ca317bf251b0003c2d
muhammad_yasir_bashir <muhammad.yasir@innovadeltech.com>
2023-06-01 12:59:42 +0500

    MSS-2254-recommendation-bug-fix

--- movado/cartridges/app_custom_movado/cartridge/models/product/productTile.js
+++ movado/cartridges/app_custom_movado/cartridge/models/product/productTile.js
@@ -33,7 +33,7 @@ function getProductSearchHit(apiProduct) {
             var tempHit = searchHits.next();
             if (tempHit.firstRepresentedProductID === apiProduct.ID) {
                 hit = tempHit;
-            } else if (!empty(apiProduct) && !empty(apiProduct.variants) && apiProduct.variants.length > 0 && tempHit.hitType == 'slicing_group' || tempHit.hitType == Constants.VARIATION_GROUP) {
+            } else if (!empty(apiProduct) && !empty(apiProduct.variants) && apiProduct.variants.length > 0 && tempHit.hitType == Constants.SLICING_GROUP || tempHit.hitType == Constants.VARIATION_GROUP) {
                 allVariantProducts = apiProduct.variants.toArray();
                 variantProduct = allVariantProducts.filter(function (data) { return data.ID === tempHit.firstRepresentedProductID });
                 hit = variantProduct ? tempHit : null;
