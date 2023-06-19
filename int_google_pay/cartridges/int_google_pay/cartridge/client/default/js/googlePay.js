e2771673f71a33bd63f00e4d01eac76e13771d83
Anskhan <anskhilji900@gmail.com>
2023-06-19 16:04:35 +0500

    MSS-2110 fixed review comments

--- int_google_pay/cartridges/int_google_pay/cartridge/client/default/js/googlePay.js
+++ int_google_pay/cartridges/int_google_pay/cartridge/client/default/js/googlePay.js
@@ -391,9 +391,9 @@ function addGooglePayButton() {
         var $googlePayButton = $('#google-pay-container-mini-cart > .gpay-button-fill-new-style');
         if ($googlePayButton.length === 0) {
             button = paymentsClient.createButton(buttonConfigs);
-            var googlePayMiniCart = document.getElementById('google-pay-container-mini-cart');
-            googlePayMiniCart.innerHTML = "";
-            googlePayMiniCart.appendChild(button);
+            var $googlePayMiniCart = document.getElementById('google-pay-container-mini-cart');
+            $googlePayMiniCart.innerHTML = "";
+            $googlePayMiniCart.appendChild(button);
         }
     }
 }
@@ -507,6 +507,7 @@ function onGooglePaymentButtonClicked() {
  */
 function processPayment(paymentData) {
     // show returned data in developer console for debugging
+    console.log(paymentData);
     return new Promise(function (resolve, reject) {
         var $selector = isGlobalMiniCart ? $('#google-pay-container-mini-cart') : $('#google-pay-container');
         setTimeout(function () {
