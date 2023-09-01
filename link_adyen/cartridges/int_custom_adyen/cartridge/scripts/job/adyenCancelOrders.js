d5940f9e2271662c2d2f6fb7161c0914a94a297d
muhammad_yasir_bashir <muhammad.yasir@innovadeltech.com>
2023-09-01 20:23:35 +0500

    MSS-2350-failed-orders-in-sfcc

--- link_adyen/cartridges/int_custom_adyen/cartridge/scripts/job/adyenCancelOrders.js
+++ link_adyen/cartridges/int_custom_adyen/cartridge/scripts/job/adyenCancelOrders.js
@@ -16,7 +16,7 @@ function processAdyenCancelledOrders(param) {
     var hooksHelper = require('*/cartridge/scripts/helpers/hooks');
 
     var searchQuery = CustomObjectMgr.queryCustomObjects('adyenNotification', "custom.eventCode = 'CANCELLATION' AND custom.success = 'false'", null);
-    var ERROR = 'ERROR'; 
+
     Logger.info('Process notifications start with count {0}', searchQuery.count);
 
     var customObject,
@@ -24,49 +24,89 @@ function processAdyenCancelledOrders(param) {
         order,
         notify;
 
-    while (searchQuery.hasNext()) {
-        customObject = searchQuery.next();
-        Transaction.wrap(function () {
-            resultHandler = objectsHandler.handle(customObject);
-        });
-
-        order = resultHandler.Order;
-        
-        var orderTotal = order.getTotalGrossPrice().value;
-        var amount = order.getTotalGrossPrice().value;
-        var isJob = true;
-        var sendMail = true;
-
-        var refundedAmount = 0;
-        var alreadyRefundedAmountList = orderStatusHelper.convertSapAttributesToList(order.custom.sapAlreadyRefundedAmount);
-        if (alreadyRefundedAmountList) {
-            for (var i = 0; i < alreadyRefundedAmountList.length; i++) {
-                refundedAmount = parseFloat(refundedAmount) + parseFloat(alreadyRefundedAmountList[i]);
+    try {
+        while (searchQuery.hasNext()) {
+            customObject = searchQuery.next();
+            Transaction.wrap(function () {
+                resultHandler = objectsHandler.handle(customObject);
+            });
+    
+            order = resultHandler.Order;
+    
+            var orderTotal = order.getTotalGrossPrice().value;
+            var amount = order.getTotalGrossPrice().value;
+            var isJob = true;
+            var sendMail = true;
+    
+            var refundedAmount = 0;
+            var alreadyRefundedAmountList = orderStatusHelper.convertSapAttributesToList(order.custom.sapAlreadyRefundedAmount);
+            if (alreadyRefundedAmountList) {
+                for (var i = 0; i < alreadyRefundedAmountList.length; i++) {
+                    refundedAmount = parseFloat(refundedAmount) + parseFloat(alreadyRefundedAmountList[i]);
+                }
             }
-        }
+    
+            if (resultHandler.status || resultHandler.status === 'SUCCESS') {
+                var paymentMethod = order.paymentInstruments[0].paymentMethod;
+    
+                if (paymentMethod && (paymentMethod == 'CREDIT_CARD' || paymentMethod == 'DW_APPLE_PAY' || paymentMethod == 'GOOGLE_PAY' || paymentMethod == 'Adyen') && amount == orderTotal && refundedAmount == 0) {
+                    var cancelResponse = hooksHelper('app.payment.adyen.cancelOrRefund', 'cancelOrRefund', order, amount, isJob, sendMail,
+                        require('*/cartridge/scripts/hooks/payment/adyenCancelSVC').cancelOrRefund);
+                    return cancelResponse;
+                }  else if (paymentMethod && (paymentMethod == 'CREDIT_CARD' || paymentMethod == 'DW_APPLE_PAY') && amount < orderTotal && transactionType.toLowerCase() == 'void') {
+                    var adjustResponse = hooksHelper('app.payment.adyen.adjustAuthorisation', 'adjustAuthorisation', order, amount, sendMail,
+                        require('*/cartridge/scripts/hooks/payment/adyenAdjustAuthorisationSVC').adjustAuthorisation);
+                    return adjustResponse;
+                }
 
-        if (resultHandler.status || resultHandler.status === 'SUCCESS') {
-            var paymentMethod = order.paymentInstruments[0].paymentMethod;
-
-            if (paymentMethod && (paymentMethod == 'CREDIT_CARD' || paymentMethod == 'DW_APPLE_PAY' || paymentMethod == 'GOOGLE_PAY' || paymentMethod == 'Adyen') && amount == orderTotal && refundedAmount == 0) {
-                var cancelResponse = hooksHelper('app.payment.adyen.cancelOrRefund', 'cancelOrRefund', order, amount, isJob, sendMail,
-                    require('*/cartridge/scripts/hooks/payment/adyenCancelSVC').cancelOrRefund);
-                return cancelResponse;
-            }  else if (paymentMethod && (paymentMethod == 'CREDIT_CARD' || paymentMethod == 'DW_APPLE_PAY') && amount < orderTotal && transactionType.toLowerCase() == 'void') {
-                var adjustResponse = hooksHelper('app.payment.adyen.adjustAuthorisation', 'adjustAuthorisation', order, amount, sendMail,
-                    require('*/cartridge/scripts/hooks/payment/adyenAdjustAuthorisationSVC').adjustAuthorisation);
-                return adjustResponse;
+                removeAdyenCustomObjects();
             }
+    
+    
         }
-
-
+    } catch (ex) {
+        Logger.error('(adyenCancelOrders) -> processAdyenCancelledOrders: Error occured while calling cancel or refund hook and error is:{0} at line {1} in file {2}', ex.toString(), ex.lineNumber, ex.fileName);
     }
+
     Logger.info('Process notifications finished with count {0}', searchQuery.count);
     searchQuery.close();
 
     return new Status(Status.OK);
 }
 
+/**
+ * remove adyen custom objects
+ */
+function removeAdyenCustomObjects() {
+    
+    var	deleteCustomObjects = require('int_adyen_overlay/cartridge/scripts/deleteCustomObjects');
+
+    var searchQuery = CustomObjectMgr.queryCustomObjects('adyenNotification', "custom.eventCode = 'CANCELLATION' AND custom.success = 'false'", null);
+    
+    Logger.info('Removing Processed Custom Objects start with count {0}', searchQuery.count);
+
+    var customObject,
+        orderID;
+
+    try {
+        while (searchQuery.hasNext()) {
+            customObject = searchQuery.next();
+            orderID = customObject.custom.orderId.split('-', 1)[0];
+            Transaction.wrap(function () {
+                deleteCustomObjects.handle(orderID);
+            });
+        }
+    } catch (ex) {
+        Logger.error('(adyenCancelOrders) -> removeAdyenCustomObjects: Error occured while removing adyen custom objects and error is:{0} at line {1} in file {2}', ex.toString(), ex.lineNumber, ex.fileName);
+    }
+
+    Logger.info('Removing Processed Custom Objects finished with count {0}', searchQuery.count);
+    searchQuery.close();
+
+    return new Status(Status.OK);
+}
+
 module.exports = {
-    processAdyenCancelledOrders: processAdyenCancelledOrders
+    processAdyenCancelledOrders: processAdyenCancelledOrders,
+    removeAdyenCustomObjects: removeAdyenCustomObjects
 };
