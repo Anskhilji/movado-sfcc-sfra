4cc5f5ef295475bcf04a07a8357be0f7a05e30f8
bilal-chaudhary <bilal.javed@innovadeltech.com>
2023-09-14 14:13:39 +0500

    MSS-1884 Refactor code

--- concord/cartridges/app_custom_concord_redesign/cartridge/client/default/js/productDetail.js
+++
@@ -1,7 +0,0 @@
-var processInclude = require('base/util');
-processInclude(require('movado/productDetail'));
-
-$(document).ready(function () {
-    processInclude(require('movad_redesign/product/zoom'));
-    processInclude(require('./product/productDetailCustom'));
-}); 
