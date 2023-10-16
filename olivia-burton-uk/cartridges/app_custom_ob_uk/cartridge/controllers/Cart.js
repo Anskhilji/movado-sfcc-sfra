8823e47b2096f6bbdf746a9a1563855394c76155
Muhammad Shahbaz <muhammad.shehbaz@innovadeltech.com>
2023-10-16 22:36:26 +0500

    MSS-2196 - Upsells in Cart

--- olivia-burton-uk/cartridges/app_custom_ob_uk/cartridge/controllers/Cart.js
+++ olivia-burton-uk/cartridges/app_custom_ob_uk/cartridge/controllers/Cart.js
@@ -10,6 +10,8 @@ server.extend(module.superModule);
 //Added custom code for personalization text for Engraving and Embossing
 server.append('AddProduct', function (req, res, next) {
     var BasketMgr = require('dw/order/BasketMgr');
+    var renderTemplateHelper = require('*/cartridge/scripts/renderTemplateHelper');
+    var CartModel = require('*/cartridge/models/cart');
     var viewData = res.getViewData();
     var addCartGtmArray;
     if (!viewData.error) {
@@ -19,11 +21,14 @@ server.append('AddProduct', function (req, res, next) {
         var orientation = req.form.orientation;
         var font = req.form.font;
         var currentBasket = BasketMgr.getCurrentOrNewBasket();
+        var basketModel = new CartModel(currentBasket);
         var personalizationType = req.form.personalizationType;
         
         if (embossedMessage || engravedMessage || personalizationType) {
             customProductOptionsHelper.updateOptionLineItem(currentBasket, viewData.pliUUID, embossedMessage, engravedMessage, orientation, font, personalizationType);
         }
+
+        viewData.cartPageHtml =  renderTemplateHelper.getRenderedHtml(basketModel, '/cart/cartSection');
     }
     next();
 });
