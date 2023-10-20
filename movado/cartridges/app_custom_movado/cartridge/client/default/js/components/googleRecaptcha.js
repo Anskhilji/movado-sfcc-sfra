f7b2d9fc5c10ad32285113ede3dd8cb097dd394e
bilal-chaudhary <bilal.javed@innovadeltech.com>
2023-10-20 17:37:13 +0500

    MSS-2425 Improve the logic.

--- movado/cartridges/app_custom_movado/cartridge/client/default/js/components/googleRecaptcha.js
+++ movado/cartridges/app_custom_movado/cartridge/client/default/js/components/googleRecaptcha.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var $loginForm = $('form.login .g-recaptcha');
+
 window.onSubmitCaptcha = function(token) {
     $(document).ready(function () {
         var $submitForm = $('.form-submit');
@@ -9,24 +11,21 @@ window.onSubmitCaptcha = function(token) {
     });
 }
 
-if ($('form.login .g-recaptcha').length == 1) {
-    
-
-    $('#login-form-email, #login-form-password').on('keydown', function (event) {
-        if (event.which === 13) {
-            event.preventDefault();
-            $('form.login .g-recaptcha').click();
-            
-        }
-    });
-}
-
-
 window.onsubmitCaptchaLogin = function (token) {
     $(document).ready(function () {
         var $submitFormButton = $('form.login');
         var $gCaptchaInput = $('.g-recaptcha-token');
         $gCaptchaInput.val(token);
         $submitFormButton.submit();
+    });
+}
+
+if ($loginForm.length == 1) {
+    $('#login-form-email, #login-form-password').on('keydown', function (event) {
+        if (event.which === 13) {
+            event.preventDefault();
+            $loginForm.click();
+            
+        }
     });
 }
